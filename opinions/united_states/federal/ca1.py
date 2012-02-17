1917cdfa01c89e285feebd5a98051007db895640
Mike Lissner <mlissner@users.noreply.github.com>
2012-02-17 13:28:50 -0800

    Adds the initial set of files. Some fleshing out down, readme should be in place (though mostly empty).

---
+++ opinions/united_states/federal/ca1.py
@@ -0,0 +1,16 @@
+class Site(object):
+    def __init__(self):
+        # contains xpath expressions for all meta data
+        urls = ('http://www.ca1.uscourts.gov/opinions/opinionrss.php',)
+        download_links =
+        case_names =
+        case_dates =
+        docket_numbers =
+        neutral_citations =
+        precedential_status =
+
+    def parse(html):
+
+
+    def download():
+        # methods for downloading the correct documents
