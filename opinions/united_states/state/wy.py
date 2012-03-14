e73c72418869e852d52db1e17575fd04261d0f1e
mlissner <mlissner@michaeljaylissner.com>
2012-03-13 23:51:52 -0700

    Adds support for wy.

---
+++ opinions/united_states/state/wy.py
@@ -0,0 +1,37 @@
+from juriscraper.GenericSite import GenericSite
+import re
+import time
+from datetime import date
+from lxml import html
+from lib.string_utils import clean_string
+
+
+class Site(GenericSite):
+    def __init__(self):
+        super(Site, self).__init__()
+        self.url = 'http://www.courts.state.wy.us/Opinions.aspx'
+        self.court_id = self.__module__
+
+    def _get_case_names(self):
+        appellants = self.html.xpath('//table/tr[@class="searchDataGridRow"]/td[3]/text()')
+        appellees = self.html.xpath('//table/tr[@class="searchDataGridRow"]/td[4]/text()')
+        return [' v. '.join(t) for t in zip(appellants, appellees)]
+
+    def _get_download_urls(self):
+        return [href for href in
+                self.html.xpath('//table/tr[@class="searchDataGridRow"]/td[6]/a/@href')]
+
+    def _get_case_dates(self):
+        dates = []
+        for date_string in self.html.xpath('//table/tr[@class="searchDataGridRow"]/td[2]/span/text()'):
+            dates.append(date.fromtimestamp(time.mktime(time.strptime(clean_string(date_string), '%B %d, %Y'))))
+        return dates
+
+    def _get_docket_numbers(self):
+        return [e for e in self.html.xpath('//table/tr[@class="searchDataGridRow"]/td[5]/text()')]
+
+    def _get_neutral_citations(self):
+        return [e for e in self.html.xpath('//table/tr[@class="searchDataGridRow"]/td[1]/text()')]
+
+    def _get_precedential_statuses(self):
+        return ["Published"] * len(self.case_names)
