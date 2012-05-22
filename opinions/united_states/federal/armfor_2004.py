48723e9f7972d99490d97ff285918b3ea351d3fd
Brian Carver <brianwc@ocf.berkeley.edu>
2012-05-22 10:12:36 -0700

    armfor backscraper that accounts for problems on 2004 page.

---
+++ opinions/united_states/federal/armfor_2004.py
@@ -0,0 +1,47 @@
+from juriscraper.GenericSite import GenericSite
+import time
+from datetime import date
+from lxml import html
+
+class Site(GenericSite):
+    def __init__(self):
+        super(Site, self).__init__()
+        self.url = 'http://www.armfor.uscourts.gov/newcaaf/opinions/2004Term.htm'
+        self.court_id = self.__module__
+
+    def _get_case_names(self):
+        return [t for t in self.html.xpath('//table//tr/td[1]/font/text()')]
+        
+    def _get_download_urls(self):
+        download_urls = []
+        for t in self.html.xpath('//table//table//tr[position() > 1]/td[2]/div/font/a[2]/@href'):
+            download_urls.append(t)
+        return download_urls
+
+    # The 2004 page contains June and July spelled out instead of abbreviated.
+    # There are also linebreaks and whitespace in the middle of the dates.
+    def _get_case_dates(self):
+        dates = []
+        for e in self.html.xpath('//table/tr[3]/td[2]/blockquote/table/tbody/tr[position() > 1]/td[3]'):
+            s = html.tostring(e, method='text', encoding='unicode').strip(
+                                ).replace('June', 'Jun').replace('July', 'Jul')
+            dates.append(date.fromtimestamp(time.mktime(time.strptime(
+                    s.replace('\r\n', '').replace('  ', ''), '%b %d, %Y'))))
+        return dates
+        
+    def _get_docket_numbers(self):
+        docket_numbers = []
+        for t in self.html.xpath('//table/tr[3]/td[2]/blockquote/table/tbody/tr/td[2]/div/font/a[1]/text()'):
+            docket_numbers.append(t)    
+        return docket_numbers
+
+    def _get_precedential_statuses(self):
+        return ['Published'] * len(self.case_names)
+
+    def _get_neutral_citations(self):
+        neutral_citations = []
+        for e in self.html.xpath('//table/tr[3]/td[2]/blockquote/table/tbody/tr[position() > 1]/td[4]'):
+            s = html.tostring(e, method='text', encoding='unicode')            
+            neutral_citations.append(s)
+        return neutral_citations
+    
