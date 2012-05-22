ebb31b77eba6884c7d7568c59dc858195a3b6996
Brian Carver <brianwc@ocf.berkeley.edu>
2012-05-22 10:11:52 -0700

    armfor backscraper that accounts for problems on 2005 page.

---
+++ opinions/united_states/federal/armfor_2005.py
@@ -0,0 +1,49 @@
+from juriscraper.GenericSite import GenericSite
+import time
+from datetime import date
+from lxml import html
+
+class Site(GenericSite):
+    def __init__(self):
+        super(Site, self).__init__()
+        self.url = 'http://www.armfor.uscourts.gov/newcaaf/opinions/2005Term.htm'
+        self.court_id = self.__module__
+
+    def _get_case_names(self):
+        return [t for t in self.html.xpath('//table//tr/td[1]/font/text()')]
+        
+    def _get_download_urls(self):
+        # Must account for the presence of both .pdf and .htm links
+        # Only part of the page has both link types, so a single xpath won't work.
+        # So we get all the a tags and then append only those with .pdf
+        download_urls = []
+        for t in self.html.xpath('//table//tr/td[2]/font/a/@href'):
+            if '.pdf' in t:
+                download_urls.append(t)
+            else:
+                continue
+        return download_urls
+
+    def _get_case_dates(self):
+        dates = []
+        for e in self.html.xpath('//table/tr[3]/td[2]/blockquote/table/tbody/tr[position() > 1]/td[3]'):
+            s = html.tostring(e, method='text', encoding='unicode').strip()
+            dates.append(date.fromtimestamp(time.mktime(time.strptime(s, '%b %d, %Y'))))        
+        return dates
+        
+    def _get_docket_numbers(self):
+        docket_numbers = []
+        for e in self.html.xpath('//table//table//tr[position() > 1]/td[2]'):
+            s = html.tostring(e, method='text', encoding='unicode')
+            docket_numbers.append(s.strip()[:-5])
+        return docket_numbers
+
+    def _get_precedential_statuses(self):
+        return ['Published'] * len(self.case_names)
+
+    def _get_neutral_citations(self):
+        neutral_citations = []
+        for e in self.html.xpath('//table/tr[3]/td[2]/blockquote/table/tbody/tr[position() > 1]/td[4]'):
+            s = html.tostring(e, method='text', encoding='unicode')
+            neutral_citations.append(s)
+        return neutral_citations
