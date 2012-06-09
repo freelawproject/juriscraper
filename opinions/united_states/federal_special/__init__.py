799eb1a817c5da31cb83ef227596fce20888ca57
Brian Carver <brianwc@ocf.berkeley.edu>
2012-06-09 15:55:19 -0700

    Add entries to __init__.py for four uscfc scrapers.

--- opinions/united_states/federal_special/__init__.py
+++ opinions/united_states/federal_special/__init__.py
@@ -2,4 +2,8 @@ __all__ = ['armfor',
            'cavc'
            'cavc_full_court'
            'cit',
-           'tax', ]
+           'tax',
+           'uscfc',
+           'uscfc_u',
+           'uscfc_vaccine',
+           'uscfc_vaccine_u', ]
