9dcdded23d7f4131355e966256a27f3319c6d48e
Brian Carver <brianwc@ocf.berkeley.edu>
2013-01-31 06:12:04 -0800

    Adds backscraper for cavc 2012.

---
+++ opinions/united_states_backscrapers/federal_special/cavc_2012.py
@@ -0,0 +1,10 @@
+# Scraper for the United States Court of Appeals for Veterans Claims
+# CourtID: cavc
+# Court Short Name: Vet.App.
+from juriscraper.opinions.united_states.federal_special import cavc
+
+class Site(cavc.Site):
+    def __init__(self):
+        super(Site, self).__init__()
+        self.url = 'http://www.uscourts.cavc.gov/orders_and_opinions/Opinions2012.html'
+        self.court_id = self.__module__
