529fd90a509d34403f0f5d3e4457ae444de25179
mlissner <mlissner@michaeljaylissner.com>
2013-03-07 18:13:21 -0800

    Removes outdated versions of ca7.

--- opinions/united_states/federal_appellate/ca7_u.py
+++
@@ -1,7 +0,0 @@
-import ca7_p
-
-class Site(ca7_p.Site):
-    def __init__(self):
-        super(Site, self).__init__()
-        self.url = "http://www.ca7.uscourts.gov/fdocs/docs.fwx?yr=&num=&Submit=Past+Week&dtype=Nonprecedential+Disposition&scrid=Select+a+Case"
-        self.court_id = self.__module__
