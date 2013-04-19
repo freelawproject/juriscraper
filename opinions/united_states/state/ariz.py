df160418f7a4fb43bebca1af48e177e70f35d108
mlissner <mlissner@michaeljaylissner.com>
2013-04-19 12:39:05 -0700

    Adds missing ariz file.

---
+++ opinions/united_states/state/ariz.py
@@ -0,0 +1,39 @@
+# Author: Michael Lissner
+# Date created: 2013-04-05
+
+import time
+from datetime import date
+
+from juriscraper.GenericSite import GenericSite
+from juriscraper.lib.string_utils import titlecase
+
+
+class Site(GenericSite):
+    def __init__(self):
+        super(Site, self).__init__()
+        self.court_id = self.__module__
+        self.url = 'http://www.azcourts.gov/opinions/SearchOpinions.aspx?court=999'
+
+    def _get_download_urls(self):
+        path = '//a[contains(@id , "hypCaseNum")]/@href'
+        return [t for t in self.html.xpath(path)]
+
+    def _get_case_names(self):
+        path = '//span[contains(@id , "lblTitle")]//text()'
+        return [titlecase(t.upper()) for t in self.html.xpath(path)]
+
+    def _get_case_dates(self):
+        path = '//span[contains(@id , "FilingDate")]//text()'
+        return [date.fromtimestamp(time.mktime(time.strptime(date_string, '%m/%d/%Y')))
+                    for date_string in self.html.xpath(path)]
+
+    def _get_precedential_statuses(self):
+        return ['Published'] * len(self.case_names)
+
+    def _get_docket_document_numbers(self):
+        path = '//a[contains(@id , "hypCaseNum")]//text()'
+        return [t for t in self.html.xpath(path)]
+
+    def _get_judges(self):
+        path = '//tr[contains(@id , "trSummary")]/td/text()[1]'
+        return [t for t in self.html.xpath(path) if t.strip()]
