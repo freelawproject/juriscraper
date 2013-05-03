83a184cbc6735a83aff4ad0a59167b5b9ddc14bf
mlissner <mlissner@michaeljaylissner.com>
2013-05-03 10:58:00 -0700

    Fixes ca8 due to new redirect and integration of results into full webpage. Also makes query go against month page, not the today page.

--- opinions/united_states/federal_appellate/ca8.py
+++ opinions/united_states/federal_appellate/ca8.py
@@ -6,23 +6,24 @@ from datetime import date
 class Site(GenericSite):
     def __init__(self):
         super(Site, self).__init__()
-        self.url = 'http://www.ca8.uscourts.gov/cgi-bin/new/today2.pl'
+        today = date.today()
+        self.url = 'http://media.ca8.uscourts.gov/cgi-bin/opnByMM.pl?theMM=%s&theYY=%s&A1=Get+Opinions' % (today.month, today.year)
         self.court_id = self.__module__
 
     def _get_case_names(self):
         case_names = []
         case_name_regex = re.compile('(\d{2}/\d{2}/\d{4})(.*)')
-        for text in self.html.xpath('//a[contains(@href, ".pdf")]/following-sibling::b/text()'):
+        for text in self.html.xpath('//a[contains(@href, "opndir")]/following-sibling::b/text()'):
             case_names.append(case_name_regex.search(text).group(2))
         return case_names
 
     def _get_download_urls(self):
-        return [e for e in self.html.xpath('//a[contains(@href, ".pdf")]/@href')]
+        return [e for e in self.html.xpath('//a[contains(@href, "opndir")]/@href')]
 
     def _get_case_dates(self):
         case_dates = []
         case_date_regex = re.compile('(\d{2}/\d{2}/\d{4})(.*)')
-        for text in self.html.xpath('//a[contains(@href, ".pdf")]/following-sibling::b/text()'):
+        for text in self.html.xpath('//a[contains(@href, "opndir")]/following-sibling::b/text()'):
             date_string = case_date_regex.search(text).group(1)
             case_dates.append(date.fromtimestamp(time.mktime(time.strptime(date_string, '%m/%d/%Y'))))
         return case_dates
@@ -30,14 +31,14 @@ class Site(GenericSite):
     def _get_docket_numbers(self):
         docket_numbers = []
         docket_number_regex = re.compile('(\d{2})(\d{4})(u|p)', re.IGNORECASE)
-        for docket_number in self.html.xpath('//a[contains(@href, ".pdf")]/text()'):
+        for docket_number in self.html.xpath('//a[contains(@href, "opndir")]/text()'):
             regex_results = docket_number_regex.search(docket_number)
             docket_numbers.append('%s-%s' % (regex_results.group(1), regex_results.group(2)))
         return docket_numbers
 
     def _get_precedential_statuses(self):
         statuses = []
-        for docket_number in self.html.xpath('//a[contains(@href, ".pdf")]/text()'):
+        for docket_number in self.html.xpath('//a[contains(@href, "opndir")]/text()'):
             docket_number = docket_number.split('.')[0]
             if 'p' in docket_number.lower():
                 statuses.append('Published')
