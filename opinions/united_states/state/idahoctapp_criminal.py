fea63f5c3116518c252c25709dcc0d1167975a21
mlissner <mlissner@michaeljaylissner.com>
2013-06-04 16:19:56 -0700

    1. Adds functioning code for Idaho Supreme, and appeals courts. 2. Moves MI backscraper

--- opinions/united_states/state/idahoctapp_criminal.py
+++ opinions/united_states/state/idahoctapp_criminal.py
@@ -4,5 +4,5 @@ import idaho_civil
 class Site(idaho_civil.Site):
     def __init__(self):
         super(Site, self).__init__()
-        self.url = 'http://www.isc.idaho.gov/appeals-court/sccriminal'
+        self.url = 'http://www.isc.idaho.gov/appeals-court/coacriminal'
         self.court_id = self.__module__
--- opinions/united_states/state/mich.py
+++ opinions/united_states/state/mich.py
@@ -72,3 +72,9 @@ class Site(GenericSite):
             except IndexError:
                 nums.append('')
         return nums
+
+    def _download_backwards(self, page):
+        if page <= 21711:
+            self.url = "http://courts.mi.gov/opinions_orders/opinions_orders/Pages/default.aspx?SearchType=4&Status_Advanced=sct&FirstDate_Advanced=7%2f1%2f1996&LastDate_Advanced=3%2f1%2f2013&PageIndex=" + str(page)
+
+        self.html = self._download()
