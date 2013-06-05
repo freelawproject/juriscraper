a24e730065fe81bf64cebf56c272631287f73d82
mlissner <mlissner@michaeljaylissner.com>
2013-06-05 08:31:49 -0700

    Fixes the precedential statuses for idahoctapp_u

--- opinions/united_states_backscrapers/state/idahoctapp_u.py
+++ opinions/united_states_backscrapers/state/idahoctapp_u.py
@@ -6,3 +6,6 @@ class Site(idahoctapp_civil.Site):
         super(Site, self).__init__()
         self.url = 'http://www.isc.idaho.gov/opinions/caunpub.htm'
         self.court_id = self.__module__
+
+    def _get_precedential_statuses(self):
+        return ["Unpublished"] * len(self.case_names)
