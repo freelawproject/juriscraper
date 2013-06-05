0d323dba6c69ba510a0a9952a47eb045a53033bd
mlissner <mlissner@michaeljaylissner.com>
2013-06-05 10:36:10 -0700

    Adds six nm court scrapers.

---
+++ opinions/united_states/state/colo.py
@@ -0,0 +1,22 @@
+'''
+Current Situation:
+ - Colorado has a PDF on their site with links to the other cases. No meta data is provided in a computer-readable
+   format.
+ - The bar association has content going back to 1996. According to the Clerk of the court, they get this from the
+   court website and put it up manually.
+ - West, Lexis, and CaseMaker all do the work manually, and charge for the results (Lexis charges up to $7.00 per
+   query).
+ - The Bar Association posts the Opinions but in a nearly unusable format.
+
+Possible Solutions:
+ - Volunteer submits the meta data for the court once/week.
+ - Pay somebody to pull the information weekly.
+ - Apply pressure on the court to do better.
+
+Contact log:
+ - 2013-06-05: Spoke to Chris Ryan, the clerk of the court. He's unable and unwilling to consider posting the
+               content in a useful manner.
+ - 2013-06-05: Left a message for Heather Clark at the Bar Association.
+'''
+
+
