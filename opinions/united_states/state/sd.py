537a5d963a665c5ffa30f5820729b63ff97eb03c
Brian Carver <brianwc@ocf.berkeley.edu>
2013-08-08 22:04:11 -0700

    Fixes broken South Dakota Supreme Court scraper. They changed site.

--- opinions/united_states/state/sd.py
+++ opinions/united_states/state/sd.py
@@ -1,5 +1,6 @@
 # Author: Michael Lissner
 # Date created: 2013-06-11
+# Revised: 2013-08-06 by Brian Carver
 
 import re
 from datetime import date
@@ -13,14 +14,15 @@ class Site(GenericSite):
     def __init__(self):
         super(Site, self).__init__()
         self.court_id = self.__module__
-        self.url = 'http://www.sdjudicial.com/sc/scopinions.aspx'
+        self.url = 'http://www.sdjudicial.com/Supreme_Court/opinions.aspx'
 
     def _get_download_urls(self):
-        path = '//tr[contains(@id, "ctl00xmainCopyxWGOpinions_r")]//a[2]/@href'
-        return list(self.html.xpath(path))
+        path ="//table[@id = 'ContentPlaceHolder1_PageContent_gvOpinions']//a/@href[starts-with(.,'http')]"
+        return [t for t in self.html.xpath(path)]
 
     def _get_case_names(self):
-        path = '//tr[contains(@id, "ctl00xmainCopyxWGOpinions_r")]/td[2]/text()'
+#        path = '//div//div[3]/div/div[2]/div[8]/div/div[3]/div//tr[position() >1]/td[2]/a/text()'
+        path ="//table[@id = 'ContentPlaceHolder1_PageContent_gvOpinions']//tr[position() > 1]/td/a/text()"
         case_names = []
         for s in self.html.xpath(path):
             case_name = re.search('(.*)(\d{4} S\.?D\.? \d{1,4})', s, re.MULTILINE).group(1)
@@ -28,23 +30,15 @@ class Site(GenericSite):
         return case_names
 
     def _get_case_dates(self):
-        path = '//tr[contains(@id, "ctl00xmainCopyxWGOpinions_r")]/td[1]/@uv'
+        path = "//table[@id = 'ContentPlaceHolder1_PageContent_gvOpinions']//tr[position() >1]/td[1]/text()"
         return [datetime.strptime(date_string, '%m/%d/%Y').date()
                 for date_string in self.html.xpath(path)]
 
     def _get_precedential_statuses(self):
         return ['Published'] * len(self.case_names)
 
-    def _get_docket_numbers(self):
-        path = '//tr[contains(@id, "ctl00xmainCopyxWGOpinions_r")]//a[2]/@href'
-        docket_numbers = []
-        for url in self.html.xpath(path):
-            docket_number = re.search('(\d+).pdf', url).group(1)
-            docket_numbers.append(docket_number)
-        return docket_numbers
-
     def _get_neutral_citations(self):
-        path = '//tr[contains(@id, "ctl00xmainCopyxWGOpinions_r")]/td[2]/text()'
+        path = "//table[@id = 'ContentPlaceHolder1_PageContent_gvOpinions']//tr[position() > 1]/td/a/text()"
         neutral_cites = []
         for s in self.html.xpath(path):
             neutral_cite = re.search('(.*)(\d{4} S\.?D\.? \d{1,4})', s, re.MULTILINE).group(2)
