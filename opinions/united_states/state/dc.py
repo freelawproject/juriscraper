4d1d46973706099fd6d74d9540e602c427d59ea2
mlissner <mlissner@michaeljaylissner.com>
2013-08-10 16:09:12 -0700

    Tunes up DC: 1. Removes unneeded titlecasing. 2. Simplifies and generalizes xpath expressions. 3. Adds dc to the __init__ file.

--- opinions/united_states/state/dc.py
+++ opinions/united_states/state/dc.py
@@ -1,13 +1,12 @@
 # Author: Krist Jin
+# Reviewer: Michael Lissner
 # Date created:2013-08-03
 
 import time
 from datetime import date
 from datetime import datetime
-from lxml import html
 
 from juriscraper.GenericSite import GenericSite
-from juriscraper.lib.string_utils import titlecase
 
 
 class Site(GenericSite):
@@ -17,22 +16,22 @@ class Site(GenericSite):
         self.url = 'http://www.cadc.uscourts.gov/internet/opinions.nsf/OpinionsByMonday'
 
     def _get_download_urls(self):
-        path = '/html/body/div[2]/div/div[2]/div[2]/div[2]/div/opinion/div/span/a/@href'
-        return [t for t in self.html.xpath(path)]
+        path = '//opinion/div/span/a/@href'
+        return list(self.html.xpath(path))
 
     def _get_case_names(self):
-        path = '/html/body/div[2]/div/div[2]/div[2]/div[2]/div/opinion/div/span[@class="column-two"]//text()'
-        return [titlecase(t.upper()) for t in self.html.xpath(path)]
+        path = '//opinion/div/span[@class="column-two"]//text()'
+        return list(self.html.xpath(path))
 
     def _get_case_dates(self):
-        path = '/html/body/div[2]/div/div[2]/div[2]/div[2]/div/opinion/div/span[@class="column-two myDemphasize"]//text()'
+        path = '//opinion/div/span[@class="column-two myDemphasize"]//text()'
         return [date.fromtimestamp(time.mktime(time.strptime(date_string, '%m/%d/%Y')))
-                    for date_string in self.html.xpath(path)]
+                for date_string in self.html.xpath(path)]
 
     def _get_precedential_statuses(self):
         return ["Published"] * len(self.case_names)
 
     def _get_docket_numbers(self):
-        path = '/html/body/div[2]/div/div[2]/div[2]/div[2]/div/opinion/div/span/a//text()'
-        return [t for t in self.html.xpath(path)]
+        path = '//opinion/div/span/a//text()'
+        return list(self.html.xpath(path))
 
