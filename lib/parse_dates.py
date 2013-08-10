327d70861bbc01468fb67a84853d6b808d1a391e
mlissner <mlissner@michaeljaylissner.com>
2013-08-09 17:36:45 -0700

    1. Adds the contributor agreement, and the clause in the readme requesting that it be completed. 2. Updates nev and checks in a broken form of okla (not yet ready) 3. Update the date parser and string utilities (and their tests) as part of the lawbox import.

--- lib/parse_dates.py
+++ lib/parse_dates.py
@@ -1,10 +1,48 @@
 # -*- coding: utf-8 -*-
 
-from dateutil.parser import _timelex, parser
-from datetime import datetime
+from dateutil.parser import _timelex, parser, parserinfo
 
-p = parser()
+# We import the entire datetime library because otherwise we run into conflicts in our isinstance statements.
+import datetime
+
+MISSPELLINGS = {
+    'Febraury': 'February',
+    'Feburay': 'February',
+    'Sepetmber': 'September',
+    'Sepember': 'September',
+    'Term': '1',
+}
+
+
+class BetterInfo(parserinfo):
+    """Removes tokens to provide better support for splitting out multiple dates.
+
+    By default, the JUMP variable is:
+
+        JUMP = [" ", ".", ",", ";", "-", "/", "'",
+                "at", "on", "and", "ad", "m", "t", "of",
+                "st", "nd", "rd", "th"]
+
+    This assumes that a single date is being sent to timesplit, and that that date might contain tokens like "and", ";",
+    or "on". But when you're sending multiple dates, you are more likely to have something like this:
+
+        'February 5, 1980; March 14, 1980 and May 28, 1980.
+
+    This uses the semicolon and the word "and" to separate dates, so we need to allow them for splitting. This class
+    makes that possible by removing them from the JUMP variable.
+    """
+    # m from a.m/p.m, t from ISO T separator
+    JUMP = [" ", ".", ",", "-", "/", "'",
+            "ad", "m", "t",
+            "st", "nd", "rd", "th"]
+
+    def __init__(self):
+        super(BetterInfo, self).__init__()
+
+
+p = parser(info=BetterInfo())
 info = p.info
+
 
 def timetoken(token):
     try:
@@ -32,32 +70,48 @@ def timesplit(input_string):
         yield " ".join(batch)
 
 
-def parse_dates(s, debug=False, sane_start=1750, sane_end=2050):
-    '''Parse dates out of a string
+def parse_dates(s, debug=False, sane_start=datetime.datetime(1750, 1, 1), sane_end=datetime.datetime(2050, 1, 1)):
+    """Parse dates out of a string
 
-    Based on http://stackoverflow.com/questions/7028689/, this method is
-    a wrapper for the above two functions. It simply takes a string, splits it
-    accordingly, and then finds dates within it.
+    Based on http://stackoverflow.com/questions/7028689/, this method is a wrapper for the above two functions. It
+    simply takes a string, splits it accordingly and then finds dates within it.
 
-    Since there are many false positives, it tries to remove them while catching
-    any errors. To do this, dates in the year 1900 or on Christmas are never
-    returned.
+    Since this parser will use the DEFAULT value to fill in missing pieces of "partial" dates, we use a year way in the
+    past and Christmas to eliminate false positives. So if the parser finds the value "June 9th," it'll convert this to
+    June 9th, 1600, and it'll get punted out. If it finds just a year, that'll get converted to Christmas, and likewise
+    be punted out. Note that this means dates in the year 1600 or on Christmas are never returned.
 
     returns a list of dates
-    '''
-    # Default is set to Christmas, 1900.
-    DEFAULT = datetime(1900, 12, 25)
+    """
+    if isinstance(sane_start, datetime.date):
+        # Convert to datetime.datetime
+        sane_start = datetime.datetime.combine(sane_start, datetime.time())
+    if isinstance(sane_end, datetime.date):
+        sane_end = datetime.datetime.combine(sane_end, datetime.time())
+
+    # Ditch unicode (_timelex() flips out on unicode if the system has cStringIO installed -- the default)
+    if isinstance(s, unicode):
+        s = s.encode('ascii', 'ignore')
+
+    # Fix misspellings
+    for i, j in MISSPELLINGS.iteritems():
+        s = s.replace(i, j)
+
+
+    # Default is set to Christmas, 1600.
+    DEFAULT = datetime.datetime(1600, 12, 25)
     dates = []
     for item in timesplit(s):
         #print "Found:", item
         try:
-            date = p.parse(item, default=DEFAULT)
-            if date.year != DEFAULT.year and \
-                    (date.month != DEFAULT.month and date.day != DEFAULT.day):
+            d = p.parse(item, default=DEFAULT)
+            hit_default_year = (d.year == DEFAULT.year)
+            hit_default_day_and_month = (d.month == DEFAULT.month and d.day == DEFAULT.day)
+            if not any([hit_default_year, hit_default_day_and_month]):
                 if debug:
-                    print "Item %s parsed as: %s" % (item, date)
-                if sane_start < date.year < sane_end:
-                    dates.append(date)
+                    print "Item %s parsed as: %s" % (item, d)
+                if sane_start < d < sane_end:
+                    dates.append(d)
         except OverflowError:
             pass
         except ValueError:
--- tests/tests.py
+++ tests/tests.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+import datetime
 import glob
 import logging
 import os
@@ -8,10 +9,35 @@ import sys
 import unittest
 
 from juriscraper.lib.importer import build_module_list
+from juriscraper.lib.parse_dates import parse_dates
 from juriscraper.lib.string_utils import clean_string
 from juriscraper.lib.string_utils import force_unicode
 from juriscraper.lib.string_utils import harmonize
 from juriscraper.lib.string_utils import titlecase
+
+
+class DateParserTest(unittest.TestCase):
+    def test_various_date_extractions(self):
+        test_pairs = (
+            # Dates separated by semicolons and JUMP words
+            ('February 5, 1980; March 14, 1980 and May 28, 1980.',
+             [datetime.datetime(1980, 2, 5, 0, 0), datetime.datetime(1980, 3, 14, 0, 0),
+              datetime.datetime(1980, 5, 28, 0, 0)]),
+            # Misspelled month value.
+            ('Febraury 17, 1945',
+             [datetime.datetime(1945, 2, 17, 0, 0)]),
+            ('Sepetmber 19 1924',
+             [datetime.datetime(1924, 9, 19)]),
+            # Using 'Term' as an indicator.
+            ('November Term 2004.',
+             [datetime.datetime(2004, 11, 01)]),
+            (u'April 26, 1961.[†]',
+             [datetime.datetime(1961, 4, 26)]),
+
+        )
+        for pair in test_pairs:
+            dates = parse_dates(pair[0])
+            self.assertEqual(dates, pair[1])
 
 
 class ScraperExampleTest(unittest.TestCase):
@@ -136,14 +162,17 @@ class StringUtilTest(unittest.TestCase):
                        u'Test v. United States'],
                       ['The United States v. Lissner',
                        u'United States v. Lissner'],
-                      ['USA v White', # tests no period in v.
+                      ['USA v White',  # tests no period in v.
                        u'United States v. White'],
-                      ['USA vs White', # tests no period in vs.
+                      ['USA vs White',  # tests no period in vs.
                        u'United States v. White'],
-                      ['/USA vs White', # tests leading slash.
+                      ['/USA vs White',  # tests leading slash.
                        u'United States v. White'],
-                      ['12–1438-cr', # tests unicode input
-                       u'12–1438-cr']]
+                      ['12–1438-cr',  # tests unicode input
+                       u'12–1438-cr'],
+                      ['Carver v. US',  # Tests the output from a titlecased word containing US to ensure it gets
+                                        # harmonized.
+                       u'Carver v. United States']]
         for pair in test_pairs:
             self.assertEqual(harmonize(clean_string(pair[0])), pair[1])
 
@@ -208,7 +237,9 @@ class StringUtilTest(unittest.TestCase):
                       ['International Union of Painter v. J&r Flooring, Inc',
                        u'International Union of Painter v. J&R Flooring, Inc'],
                       ['DOROTHY L. BIERY, and JERRAMY and ERIN PANKRATZ v. THE UNITED STATES 07-693L And',
-                       u'Dorothy L. Biery, and Jerramy and Erin Pankratz v. the United States 07-693l And']]
+                       u'Dorothy L. Biery, and Jerramy and Erin Pankratz v. the United States 07-693l And'],
+                      ['CARVER v. US',
+                       u'Carver v. US']]
         for pair in test_pairs:
             self.assertEqual(titlecase(force_unicode(pair[0])),
                              pair[1])
