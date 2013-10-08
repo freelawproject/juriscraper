be178c478b85831c9f374934454b1fa6ea181bd9
mlissner <mlissner@michaeljaylissner.com>
2013-10-07 18:20:07 -0700

    Tweaks tenn courts for a corner case (gotta catch 'em all).

--- opinions/united_states/state/tenn.py
+++ opinions/united_states/state/tenn.py
@@ -16,29 +16,29 @@ class Site(GenericSite):
         self.court_id = self.__module__
 
     def _get_download_urls(self):
-        return [t for t in self.html.xpath("//table//tr/td/a/@href")]
+        return [t for t in self.html.xpath("//table//tr[not(descendant::a[contains(@href, 'pendingcase')])]/td/a/@href")]
 
     def _get_case_names(self):
-        return [t for t in self.html.xpath("//table//tr/td/a/text()")]
+        return [t for t in self.html.xpath("//table//tr[not(descendant::a[contains(@href, 'pendingcase')])]/td/a/text()")]
 
     def _get_lower_courts(self):
-        return [t.strip() for t in self.html.xpath("//table//tr/td[2]/text()")]
+        return [t.strip() for t in self.html.xpath("//table//tr[not(descendant::a[contains(@href, 'pendingcase')])]/td[2]/text()")]
 
     def _get_case_dates(self):
         dates = []
-        for s in self.html.xpath('//table//tr/td[4]/span/text()'):
+        for s in self.html.xpath("//table//tr[not(descendant::a[contains(@href, 'pendingcase')])]/td[4]/span/text()"):
             dates.append(date.fromtimestamp(time.mktime(time.strptime(s, '%m/%d/%y'))))
         return dates
 
     def _get_docket_numbers(self):
-        return [t for t in self.html.xpath("//table//tr/td/div[./@class[contains(.,'number')]]/text()")]
+        return [t for t in self.html.xpath("//table//tr[not(descendant::a[contains(@href, 'pendingcase')])]/td/div[./@class[contains(.,'number')]]/text()")]
 
     # Here we are using the 'judges' field to list the Authoring Judge.
     # It would be better for GenericSite to have an 'author' field and to put
     # these there.
     def _get_judges(self):
         judges = []
-        for t in self.html.xpath('//table//tr/td/div/text()'):
+        for t in self.html.xpath("//table//tr[not(descendant::a[contains(@href, 'pendingcase')])]/td/div/text()"):
             if 'Authoring' in t:
                 # We strip the text 'Authoring Judge: '
                 judges.append(t[17:])
@@ -48,7 +48,7 @@ class Site(GenericSite):
 
     def _get_lower_court_judges(self):
         trial_judges = []
-        for t in self.html.xpath('//table//tr/td/div/text()'):
+        for t in self.html.xpath("//table//tr[not(descendant::a[contains(@href, 'pendingcase')])]/td/div/text()"):
             if 'Trial' in t:
                 # We strip the text 'Trial Court Judge: '
                 trial_judges.append(t[19:])
@@ -58,7 +58,7 @@ class Site(GenericSite):
 
     def _get_summaries(self):
         summaries = []
-        for e in self.html.xpath('//table//tr/td//div/p'):
+        for e in self.html.xpath("//table//tr[not(descendant::a[contains(@href, 'pendingcase')])]/td//div/p"):
             s = html.tostring(e, method='text', encoding='unicode')
             summaries.append(s)
         return summaries
