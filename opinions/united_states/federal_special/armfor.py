48ab24ef254ffa8bb32b50f536a3257d3200d8a0
mlissner <mlissner@michaeljaylissner.com>
2013-10-09 14:25:05 -0700

    Fixes armfor.

--- opinions/united_states/federal_special/armfor.py
+++ opinions/united_states/federal_special/armfor.py
@@ -7,6 +7,7 @@ import time
 from datetime import date
 from lxml import html
 
+
 class Site(GenericSite):
     def __init__(self):
         super(Site, self).__init__()
@@ -16,19 +17,18 @@ class Site(GenericSite):
         else:
             url_year = today.year - 1
         self.url = (
-            'http://www.armfor.uscourts.gov/newcaaf/opinions/%sSepTerm.htm'
-                % url_year )
+            'http://www.armfor.uscourts.gov/newcaaf/opinions/%sSepTerm.htm' % url_year)
         self.court_id = self.__module__
 
     def _get_case_names(self):
-        return [t for t in self.html.xpath('//table//tr/td[1]/font/text()')]
+        return [t for t in self.html.xpath('//table//tr[descendant::a]/td[1]/font/text()')]
 
     def _get_download_urls(self):
-        return [t for t in self.html.xpath('//table//tr/td[2]/font/a/@href')]
+        return [t for t in self.html.xpath('//table//tr[descendant::a]/td[2]/font/a/@href')]
 
     def _get_case_dates(self):
         dates = []
-        for s in self.html.xpath('//table//tr/td[3]/font/text()'):
+        for s in self.html.xpath('//table//tr[descendant::a]/td[3]/font/text()'):
             s = s.strip()
             dates.append(date.fromtimestamp(time.mktime(time.strptime(
                 s, '%b %d, %Y'))))
@@ -36,7 +36,7 @@ class Site(GenericSite):
 
     def _get_docket_numbers(self):
         docket_numbers = []
-        for e in self.html.xpath('//table//table//tr[position() > 1]/td[2]'):
+        for e in self.html.xpath('//table//table//tr[descendant::a][position() > 1]/td[2]'):
             s = html.tostring(e, method='text', encoding='unicode')
             docket_numbers.append(s.strip()[:-5])
         return docket_numbers
