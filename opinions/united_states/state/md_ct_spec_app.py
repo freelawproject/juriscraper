1259c8e1abdecf61516a42a4ae7bc87bd23e7e8e
Andrei Chelaru <chelaru.andrei@gmail.com>
2014-06-27 19:41:07 +0000

    md_ct_spec_app.py created online with Bitbucket

---
+++ opinions/united_states/state/md_ct_spec_app.py
@@ -0,0 +1,21 @@
+"""
+Scraper for Maryland Court of Special Appeals
+CourtID: md
+Court Short Name: MD
+Author: Andrei Chelaru
+Date created: 06/27/2014
+"""
+
+
+from juriscraper.opinions.united_states.state import md
+
+import time
+from datetime import date
+
+
+class Site(md.Site):
+    def __init__(self):
+        super(Site, self).__init__()
+        self.url = 'http://www.mdcourts.gov/cgi-bin/indexlist.pl?court=cosa' \
+                    '&year={current_year}&order=bydate&submit=Submit'.format(current_year=date.today().year)
+        self.court_id = self.__module__
