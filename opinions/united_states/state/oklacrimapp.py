61e15cf457b0cfd461153c2593e65e430d7b31e1
Andrei Chelaru <chelaru.andrei@gmail.com>
2014-07-05 11:29:00 +0000

    oklacrimapp.py created online with Bitbucket

---
+++ opinions/united_states/state/oklacrimapp.py
@@ -0,0 +1,22 @@
+# Scraper for Oklahoma Court of Criminal Appeals
+#CourtID: oklacrimapp
+#Court Short Name: OK
+#Author: Andrei Chelaru
+#Reviewer:
+#Date: 2014-07-05
+
+from datetime import date
+import time
+import re
+
+from juriscraper.opinions.united_states.state import okla
+
+
+class Site(okla.Site):
+    def __init__(self):
+        super(Site, self).__init__()
+        self.court_id = self.__module__
+        d = date.today()
+        self.url = 'http://www.oscn.net/applications/oscn/Index.asp?ftdb=STOKCSCR&year={year}&level=1'.format(
+            year=d.year
+        )
