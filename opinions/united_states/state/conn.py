be9de94bdd2207ca4374322dda96a9e0f56713f9
asadullah baig <asadullahbeg@outlook.com>
2014-07-16 23:35:48 +0000

    conn.py edited online with Bitbucket

--- opinions/united_states/state/conn.py
+++ opinions/united_states/state/conn.py
@@ -14,6 +14,7 @@ from datetime import date
 from lxml import html
 import time
 
+
 class Site(OpinionSite):
     def __init__(self):
         super(Site, self).__init__()
@@ -22,35 +23,36 @@ class Site(OpinionSite):
         self.court_id = self.__module__
 
     def _get_case_names(self):
-        names=[]
-        casenames=self.html.xpath('//*[@id="AutoNumber1"]/tr[2]/td/table/tr/td//ul//text()')
-        for x in range(0,len(casenames)):
+        names = []
+        casenames = self.html.xpath('//*[@id="AutoNumber1"]/tr[2]/td/table/tr/td//ul//text()')
+        for x in range(0, len(casenames)):
             if "-" in casenames[x]:
-                 names.append(clean_string(casenames[x]))
+                names.append(clean_string(casenames[x]))
         return names
 
     def _get_download_urls(self):
         return self._get_data_by_grouping_name('href')
 
     def _get_case_dates(self):
-        dates=[]
+        dates = []
         for title in self.html.xpath('//table[@id="AutoNumber1"]/tr[2]/td/table/tr/td/p//text()'):
-            for x in range(0,len(title.getparent().xpath("following::ul[1]//a/@href[contains(., 'pdf')]"))):
-                dates.append( date.fromtimestamp(
-                      time.mktime(time.strptime(self._get_last_entry(clean_string(title)),'%m/%d/%y'))))
+            for x in range(0, len(title.getparent().xpath("following::ul[1]//a/@href[contains(., 'pdf')]"))):
+                dates.append(date.fromtimestamp(
+                             time.mktime(time.strptime(self._get_last_entry(clean_string(title)),'%m/%d/%y'))))
         return dates
     
     def _get_docket_numbers(self):
         return self._get_data_by_grouping_name('docket_numbers')
 
     def _get_precedential_statuses(self):
-        publish=[]
-        for x in range(0,len(self.case_dates)):
+        publish = []
+        for x in range(0, len(self.case_dates)):
             if self.crawl_date < self.case_dates[x]:
                 publish.append('Not Published')
             else:
                 publish.append('Published')
         return publish
+        
     @staticmethod
     def _get_last_entry(text):
         t1=text.split()
@@ -59,15 +61,15 @@ class Site(OpinionSite):
     def _get_data_by_grouping_name(self, group_name):
         meta_data = []
         if group_name is 'href':
-            links= self.html.xpath('//@href')
-            for i in range(0,len(links)):
-                if( '.pdf' in links[i]):
+            links = self.html.xpath('//@href')
+            for i in range(0, len(links)):
+                if '.pdf' in links[i]:
                     meta_data.append(links[i])
             return meta_data
         if group_name is 'docket_numbers':
-            docket_numbers=self.html.xpath('//tr[2]//ul//a//text()')
+            docket_numbers = self.html.xpath('//tr[2]//ul//a//text()')
             for d in docket_numbers:
-                if re.search(r"([AC]|[SC]\d{5,5})",d):
-                   meta_data.append(d)
+                if re.search(r"([AC]|[SC]\d{5})", d):
+                    meta_data.append(d)
             return meta_data
     
