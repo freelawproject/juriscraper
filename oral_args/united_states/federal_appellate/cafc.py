040193ee525ffeaed2b6eb13bff1f7550277ef0a
mlissner <mlissner@michaeljaylissner.com>
2014-07-20 14:01:57 -0400

    Tweaks for cafc.

--- oral_args/united_states/federal_appellate/cafc.py
+++ oral_args/united_states/federal_appellate/cafc.py
@@ -2,13 +2,11 @@
 CourtID: cafc
 Court Short Name: cafc
 Author: Andrei Chelaru
-Reviewer:
+Reviewer: mlr
 Date created: 18 July 2014
 """
 
 from datetime import datetime, date
-import random
-import re
 
 from juriscraper.OralArgumentSite import OralArgumentSite
 
@@ -19,7 +17,7 @@ class Site(OralArgumentSite):
         self.court_id = self.__module__
         d = date.today()
         self.url = 'http://www.cafc.uscourts.gov/oral-argument-recordings/{date}/all'.format(
-            date=d.strftime('%Y-%m-%d')
+            date=d.strftime('%Y-%m')
         )
 
     def _get_download_urls(self):
@@ -28,27 +26,12 @@ class Site(OralArgumentSite):
 
     def _get_case_names(self):
         path = "id('searchResults')//tr[position() > 2]/td[3]/text()"
-        return map(self._return_case_name, self.html.xpath(path))
-
-    @staticmethod
-    def _return_case_name(e):
-        e = ' '.join(e.split())
-        return e
+        return [' '.join(s.split()) for s in self.html.xpath(path)]
 
     def _get_case_dates(self):
         path = "id('searchResults')//tr[position() > 2]/td[1]/text()"
-        return map(self._return_case_date, self.html.xpath(path))
-
-    @staticmethod
-    def _return_case_date(e):
-        e = ''.join(e.split())
-        return datetime.strptime(e, '%Y-%m-%d').date()
+        return [datetime.strptime(s.strip(), '%Y-%m-%d').date() for s in self.html.xpath(path)]
 
     def _get_docket_numbers(self):
         path = "id('searchResults')//tr[position() > 2]/td[2]/text()"
-        return map(self._return_docket_number, self.html.xpath(path))
-
-    @staticmethod
-    def _return_docket_number(e):
-        e = ''.join(e.split())
-        return e
+        return [s.strip() for s in self.html.xpath(path)]
