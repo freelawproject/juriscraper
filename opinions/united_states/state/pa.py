b2c8fa83fd5acd4b6b14e37c8cf76e5785e0f0ea
Mike Lissner <mlissner@users.noreply.github.com>
2014-08-01 17:03:43 -0400

    Tweaks for Pa.

--- opinions/united_states/state/pa.py
+++ opinions/united_states/state/pa.py
@@ -18,21 +18,23 @@ class Site(OpinionSite):
         self.court_id = self.__module__
         self.regex = re.compile("(.*)(?:[,-]?\s+Nos?\.)(.*)", re.MULTILINE)
         self.url = 'http://www.pacourts.us/assets/rss/SupremeOpinionsRss.ashx'
+        self.base = "//item[not(contains(title/text(), 'Judgment List'))][not(contains(title/text(), 'Reargument Table'))]"
 
     def _get_case_names(self):
-        path = "//item[not(contains(title/text(), 'Judgment List'))]/title/text()"
+        path = "{base}/title/text()".format(base=self.base)
         return map(self._return_case_name, self.html.xpath(path))
 
     def _return_case_name(self, s):
+        print s
         match = self.regex.search(s)
         return match.group(1)
 
     def _get_download_urls(self):
-        path = "//item[not(contains(title/text(), 'Judgment List'))]//@href"
+        path = "{base}//@href".format(base=self.base)
         return [item for item in self.html.xpath(path)]
 
     def _get_case_dates(self):
-        path = "//item[not(contains(title/text(), 'Judgment List'))]/pubdate/text()"
+        path = "{base}/pubdate/text()".format(base=self.base)
         # Isolate '29Jul2014' from a string like 'Tue, 29 Jul 2014 04:00:00 GMT'
         return [datetime.strptime(''.join(s.split(' ')[1:4]), '%d%b%Y').date()
                 for s in self.html.xpath(path)]
@@ -41,7 +43,7 @@ class Site(OpinionSite):
         return ['Published'] * len(self.case_names)
 
     def _get_docket_numbers(self):
-        path = "//item[not(contains(title/text(), 'Judgment List'))]/title/text()"
+        path = "{base}/title/text()".format(base=self.base)
         return map(self._return_docket_number, self.html.xpath(path))
 
     def _return_docket_number(self, e):
@@ -49,5 +51,5 @@ class Site(OpinionSite):
         return match.group(2)
 
     def _get_judges(self):
-        path = "//item[not(contains(title/text(), 'Judgment List'))]/creator/text()"
+        path = "{base}/creator/text()".format(base=self.base)
         return list(self.html.xpath(path))
