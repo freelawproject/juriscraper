ae9d2f8b0916e3c97eaa2ee0e72c1eee7c3ff97d
Mike Lissner <mlissner@users.noreply.github.com>
2014-08-05 16:50:39 -0400

    Fixes CIT.

--- opinions/united_states/federal_special/cit.py
+++ opinions/united_states/federal_special/cit.py
@@ -14,21 +14,30 @@ class Site(OpinionSite):
         super(Site, self).__init__()
         self.url = 'http://www.cit.uscourts.gov/SlipOpinions/index.html'
         self.court_id = self.__module__
+        self.base = '//tr[../tr/th[contains(., "Caption")]]'
 
     def _get_download_urls(self):
-        return [t for t in self.html.xpath('//table[6]//tr/td[1]/a/@href')]
+        return [t for t in self.html.xpath('{base}/td[1]/a/@href'.format(base=self.base))]
 
     def _get_neutral_citations(self):
-        return [t for t in self.html.xpath('//table[6]//tr/td[1]/a/text()')]
+        neutral_citations = []
+        for t in self.html.xpath('{base}/td[1]/a/text()'.format(base=self.base)):
+            year, item_number = t.split('-')
+            neutral_citations.append('20{year} CIT {number}'.format(year=year, number=item_number))
+        return neutral_citations
 
     def _get_case_names(self):
         # Exclude confidential rows by ensuring there is a sibling row that
         # contains an anchor (which confidential cases do not)
-        return [s for s in self.html.xpath('//table[6]//tr[position() > 1]/td[2][../td/a]/text()[1]')]
+        case_names = []
+        for e in self.html.xpath('{base}/td[2][../td/a]'.format(base=self.base)):
+            text_nodes = e.xpath('.//text()')
+            case_names.append(text_nodes[0])
+        return case_names
 
     def _get_precedential_statuses(self):
         statuses = []
-        for e in self.html.xpath('//table[6]//tr[position() > 1]/td[2][../td/a]'):
+        for e in self.html.xpath('{base}/td[2][../td/a]'.format(base=self.base)):
             s = html.tostring(e, method='text', encoding='unicode').lower().strip()
             if "errata" in s:
                 statuses.append('Errata')
@@ -42,11 +51,10 @@ class Site(OpinionSite):
         # original release date instead.
         dates = []
         date_formats = ['%m/%d/%Y', '%m/%d/%y']
-        for date_string in self.html.xpath('//table[6]//tr/td[3][../td/a]/text()'):
+        for date_string in self.html.xpath('{base}/td[3][../td/a]//text()'.format(base=self.base)):
             for date_format in date_formats:
                 try:
-                    d = date.fromtimestamp(time.mktime(
-                                       time.strptime(date_string, date_format)))
+                    d = date.fromtimestamp(time.mktime(time.strptime(date_string, date_format)))
                     dates.append(d)
                     break
                 except ValueError:
@@ -56,17 +64,16 @@ class Site(OpinionSite):
 
     def _get_docket_numbers(self):
         docket_numbers = []
-        for e in self.html.xpath('//table[6]//tr[position() > 1]/td[4][../td/a]'):
-            docket_numbers.append(html.tostring(
-                                e, method='text', encoding='unicode').strip())
+        for e in self.html.xpath('{base}/td[4][../td/a]'.format(base=self.base)):
+            docket_numbers.append(html.tostring(e, method='text', encoding='unicode').strip())
         return docket_numbers
 
     def _get_judges(self):
         judges = []
-        for e in self.html.xpath('//table[6]//tr[position() > 1]/td[5][../td/a]'):
-            s = html.tostring (e, method='text', encoding='unicode')
+        for e in self.html.xpath('{base}/td[5][../td/a]'.format(base=self.base)):
+            s = html.tostring(e, method='text', encoding='unicode')
             judges.append(s)
         return judges
 
     def _get_nature_of_suit(self):
-        return [t for t in self.html.xpath('//table[6]//tr/td[6][../td/a]/text()')]
+        return [t for t in self.html.xpath('{base}/td[6][../td/a]/text()'.format(base=self.base))]
