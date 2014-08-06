9ab708a55d154ec7e82d7ba061ce55d83308e579
Mike Lissner <mlissner@users.noreply.github.com>
2014-08-06 12:06:58 -0400

    Updates cavc for new site.

--- opinions/united_states/federal_special/cavc.py
+++ opinions/united_states/federal_special/cavc.py
@@ -1,73 +1,74 @@
 """Scraper for the United States Court of Appeals for Veterans Claims
 CourtID: cavc
 Court Short Name: Vet.App.
+History:
+ - 2012-06-07: Created by Brian Carver
+ - 2014-08-06: Updated by mlr.
 """
 
 from juriscraper.OpinionSite import OpinionSite
 import time
 import datetime
 from datetime import date
-from lxml import html
 
 
 class Site(OpinionSite):
     def __init__(self):
         super(Site, self).__init__()
-        self.url = ('http://www.uscourts.cavc.gov/opinions.php')
+        self.url = 'http://www.uscourts.cavc.gov/opinions.php'
         self.court_id = self.__module__
 
     def _get_docket_numbers(self):
         docket_numbers = []
-        for e in self.html.xpath('//div/ul/li/ul/li/table/tr/td/a'):
+        for e in self.html.xpath("//*[@id='oasteps_boxes']//td[2]/a"):
             s = ', '.join([t.strip() for t in e.xpath('text()') if t.strip()])
             docket_numbers.append(s)
         return docket_numbers
 
     def _get_download_urls(self):
-        return [txt for txt in self.html.xpath('//div/ul/li/ul/li/table/tr/td/a/@href')]
+        return list(self.html.xpath("//*[@id='oasteps_boxes']//td[2]//@href"))
 
     def _get_case_dates(self):
         dates = []
-        for txt in self.html.xpath('//div/ul/li/ul/li/table/tr/td[3]/text()'):
-            if txt == '20JUN008':
-                dates.append(date.fromtimestamp(time.mktime(time.strptime('20JUN08', '%d%b%y'))))
-            else:
-                dates.append(date.fromtimestamp(time.mktime(time.strptime(txt, '%d%b%y'))))
+        for txt in self.html.xpath("//*[@id='oasteps_boxes']//td[3]//text()"):
+            dates.append(date.fromtimestamp(time.mktime(time.strptime(txt, '%d%b%y'))))
         return dates
 
-    # http://en.wikipedia.org/wiki/United_States_Secretary_of_Veterans_Affairs
-    # provided the names to append here for different opinion dates.
-    # Covering all possibilities here.
     def _get_case_names(self):
         case_names = []
         dates = self._get_case_dates()
         appellants = []
-        for e in self.html.xpath('//div/ul/li/ul/li/table/tr/td[1]'):
+        for e in self.html.xpath("//*[@id='oasteps_boxes']//td[1]"):
             app = ', '.join([t.strip() for t in e.xpath('text()') if t.strip()])
             appellants.append(app)
-        for txt, dat in zip(appellants, dates):
-            if dat > datetime.date(2009, 1, 20):
-                case_names.append(txt + ' v. Shinseki')
-            elif dat > datetime.date(2007, 12, 20):
-                case_names.append(txt + ' v. Peake')
-            elif dat > datetime.date(2007, 10, 1):
-                case_names.append(txt + ' v. Mansfield')
-            elif dat > datetime.date(2005, 1, 26):
-                case_names.append(txt + ' v. Nicholson')
-            elif dat > datetime.date(2001, 1, 23):
-                case_names.append(txt + ' v. Principi')
-            elif dat > datetime.date(2000, 7, 25):
-                case_names.append(txt + ' v. Gober')
-            elif dat > datetime.date(1998, 1, 2):
-                case_names.append(txt + ' v. West')
-            elif dat > datetime.date(1997, 7, 1):
-                case_names.append(txt + ' v. Gober')
-            elif dat > datetime.date(1993, 1, 22):
-                case_names.append(txt + ' v. Brown')
-            elif dat > datetime.date(1992, 9, 26):
-                case_names.append(txt + ' v. Principi')
+        for t, d in zip(appellants, dates):
+            # See: http://en.wikipedia.org/wiki/United_States_Secretary_of_Veterans_Affairs
+            if d > datetime.date(2014, 7, 30):
+                case_names.append(t + 'v. McDonald')
+            elif d > datetime.date(2014, 5, 30):
+                case_names.append(t + ' v. Gibson')
+            elif d > datetime.date(2009, 1, 20):
+                case_names.append(t + ' v. Shinseki')
+            elif d > datetime.date(2007, 12, 20):
+                case_names.append(t + ' v. Peake')
+            elif d > datetime.date(2007, 10, 1):
+                case_names.append(t + ' v. Mansfield')
+            elif d > datetime.date(2005, 1, 26):
+                case_names.append(t + ' v. Nicholson')
+            elif d > datetime.date(2001, 1, 23):
+                case_names.append(t + ' v. Principi')
+            elif d > datetime.date(2000, 7, 25):
+                case_names.append(t + ' v. Gober')
+            elif d > datetime.date(1998, 1, 2):
+                case_names.append(t + ' v. West')
+            elif d > datetime.date(1997, 7, 1):
+                case_names.append(t + ' v. Gober')
+            elif d > datetime.date(1993, 1, 22):
+                case_names.append(t + ' v. Brown')
+            elif d > datetime.date(1992, 9, 26):
+                case_names.append(t + ' v. Principi')
             else:
-                case_names.append(txt + ' v. Derwinski')
+                case_names.append(t + ' v. Derwinski')
         return case_names
 
     def _get_precedential_statuses(self):
