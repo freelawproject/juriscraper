ef991b0d2977795a1a3d77a2a553cbb4128ff23d
Mike Lissner <mlissner@users.noreply.github.com>
2014-08-06 13:02:21 -0400

    Updates wash for new site, including new backscraper.

--- opinions/united_states/state/washctapp_u.py
+++ opinions/united_states/state/washctapp_u.py
@@ -1,22 +1,13 @@
-import wash
-from datetime import datetime
+import washctapp_p
 
 
-
-class Site(wash.Site):
+class Site(washctapp_p.Site):
     def __init__(self):
         super(Site, self).__init__()
         self.court_id = self.__module__
-        self.parameters = {'courtLevel': 'C',
-                           'pubStatus': 'UNP',
-                           'beginDate': '01/01/2012',
-                           'endDate': '01/01/2050',
-                           'SType': 'Phrase',
-                           'SValue': ''}
-
-    def _get_case_names(self):
-        path = "//table[@class = 'listTable']/tr/td[4]/text()"
-        return list(self.html.xpath(path))
+        self.courtLevel = 'C'
+        self.pubStatus = 'UNP'
+        self._set_parameters()
 
     def _get_precedential_statuses(self):
         return ["Unpublished"] * len(self.case_names)
