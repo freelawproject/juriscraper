75eb07e98a2e754eea6655a41592c8ea11469649
Mike Lissner <mlissner@users.noreply.github.com>
2014-08-06 13:09:05 -0400

    Updates wash for new site, including new backscraper.

--- opinions/united_states/state/washctapp_p.py
+++ opinions/united_states/state/washctapp_p.py
@@ -5,6 +5,7 @@ class Site(wash.Site):
     def __init__(self):
         super(Site, self).__init__()
         self.court_id = self.__module__
+        self.courtLevel = 'C'
         self.pubStatus = 'PUB'
         self._set_parameters()
 
