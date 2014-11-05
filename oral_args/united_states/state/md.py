0fdc54801e2523a4795b5059e5fc02a340e2f8bf
Michael Lissner <mlissner@michaeljaylissner.com>
2014-11-05 11:41:26 -0800

    Disables MD and makes a note as to why.
    We can enable this in the future when we have funding for videos. :(

--- oral_args/united_states/state/md.py
+++ oral_args/united_states/state/md.py
@@ -1,4 +1,21 @@
 """Scraper for Maryland Supreme Court Oral Argument Audio
+
+This scraper has an interesting history. It was briefly running on the live
+site, but we realized shortly after starting it that the scraper was
+downloading video, not audio!
+
+Seeing that we weren't ready for video, we disabled this scraper and deleted
+any traces of it on the server.
+
+One interesting lesson though was that the OA system didn't crumble or budge
+when this was running. The video converted to mp3 just fine (each item took a
+few hours) and we began hosting it like nothing was different. Go figure.
+
+Your humble editor,
+
+Mike
+
+
 CourtID: md
 Court Short Name: Md.
 Author: Brian W. Carver
