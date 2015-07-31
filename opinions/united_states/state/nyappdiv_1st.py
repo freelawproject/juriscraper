30666847b5267bc9efda870406cd65642afe532d
Michael Lissner <mlissner@michaeljaylissner.com>
2015-07-30 21:06:30 -0700

    Fixes terrible code in ny and provides more tests for same.

--- opinions/united_states/state/nyappdiv_1st.py
+++ opinions/united_states/state/nyappdiv_1st.py
@@ -5,10 +5,10 @@
 #Reviewer: mlr
 #Date: 2014-07-04
 
-import re
 import time
 from datetime import date
 
+import re
 from juriscraper.OpinionSite import OpinionSite
 
 
@@ -26,19 +26,19 @@ class Site(OpinionSite):
         self.court_id = self.__module__
 
     def _get_case_names(self):
-        path = '''//tr[td[4]]//a[contains(./@href, '3d')]/text()'''
+        path = '//tr[td[4]]//a[contains(./@href, "3d")]/text()'
         return self.html.xpath(path)
 
     def _get_download_urls(self):
-        path = '''//tr[td[4]]//a/@href[contains(., '3d')]'''
+        path = '//tr[td[4]]//a/@href[contains(., "3d")]'
         return self.html.xpath(path)
 
     def _get_case_dates(self):
-        path = '''//caption//text()'''
+        path = '//caption//text()'
         dates = self.html.xpath(path)
         case_dates = []
         for index, case_date in enumerate(dates):
-            path_2 = "count(//table[./caption][{c}]//tr[.//@href])".format(c=index + 1)
+            path_2 = "count((//table[./caption])[{c}]/tr[.//@href])".format(c=index + 1)
             d = date.fromtimestamp(time.mktime(time.strptime(re.sub('Cases Decided ', '', case_date), '%B %d, %Y')))
             case_dates.extend([d] * int(self.html.xpath(path_2)))
         return case_dates
@@ -47,11 +47,11 @@ class Site(OpinionSite):
         return ['Published'] * len(self.case_names)
 
     def _get_docket_numbers(self):
-        path = '''//tr[contains(./td[1]/a/@href, '3d')]/td[3]'''
+        path = '//tr[contains(./td[1]/a/@href, "3d")]/td[3]'
         return map(self._add_str_to_list_where_empty_element, self.html.xpath(path))
 
     def _get_judges(self):
-        path = '''//tr[contains(./td[1]/a/@href, '3d')]/td[2]'''
+        path = '//tr[contains(./td[1]/a/@href, "3d")]/td[2]'
         return map(self._add_str_to_list_where_empty_element, self.html.xpath(path))
 
     @staticmethod
