3c705c15eb99a9f0d41d156547894644607898b7
Andrei <chelaru.andrei@gmail.com>
2015-10-01 17:48:11 +0300

    ca11 backscraepr

--- opinions/united_states/federal_appellate/ca11_u.py
+++ opinions/united_states/federal_appellate/ca11_u.py
@@ -5,3 +5,16 @@ class Site(ca11_p.Site):
         super(Site, self).__init__()
         self.url = 'http://media.ca11.uscourts.gov/opinions/unpub/logname.php'
         self.court_id = self.__module__
+        self.back_scrape_iterable = xrange(20, 22000, 20)
+
+    def _download_backwards(self, n):
+        self.url = 'http://media.ca11.uscourts.gov/opinions/unpub/logname.php?begin={}&num={}&numBegin=1'.format(
+            n,
+            n/20 - 1
+        )
+
+        self.html = self._download()
+        if self.html is not None:
+            # Setting status is important because it prevents the download
+            # function from being run a second time by the parse method.
+            self.status = 200
