655c68e6ed8939e5921893624267fb4c12fe64ca
Michael Lissner <mlissner@michaeljaylissner.com>
2015-10-13 16:52:54 -0700

    Makes ca11 work in the face of terrible data.

--- opinions/united_states/federal_appellate/ca11_p.py
+++ opinions/united_states/federal_appellate/ca11_p.py
@@ -4,9 +4,10 @@
 # 2013-01-28  | InsanityException due to the court adding busted share links.
 # 2014-07-02  | New website required rewrite.
 
-from juriscraper.OpinionSite import OpinionSite
 import time
 from datetime import date
+
+from juriscraper.OpinionSite import OpinionSite
 from juriscraper.lib.string_utils import clean_string
 
 
@@ -18,19 +19,19 @@ class Site(OpinionSite):
         self.back_scrape_iterable = xrange(20, 10000, 20)
 
     def _get_case_names(self):
-        return [e for e in self.html.xpath('//tr/td[1]//text()')]
+        return [e for e in self.html.xpath('//tr[./td[1]/a//text()]/td[1]//text()')]
 
     def _get_download_urls(self):
-        return [e for e in self.html.xpath('//tr/td[1]/a/@href')]
+        return [e for e in self.html.xpath('//tr[./td[1]/a//text()]/td[1]/a/@href')]
 
     def _get_case_dates(self):
         dates = []
-        for date_string in self.html.xpath('//tr/td[5]//text()'):
+        for date_string in self.html.xpath('//tr[./td[1]/a//text()]/td[5]//text()'):
             dates.append(date.fromtimestamp(time.mktime(time.strptime(clean_string(date_string), '%m-%d-%Y'))))
         return dates
 
     def _get_docket_numbers(self):
-        return [e for e in self.html.xpath('//tr/td[2]//text()')]
+        return [e for e in self.html.xpath('//tr[./td[1]/a//text()]/td[2]//text()')]
 
     def _get_precedential_statuses(self):
         if 'unpub' in self.url:
@@ -39,7 +40,7 @@ class Site(OpinionSite):
             return ['Published'] * len(self.case_names)
 
     def _get_nature_of_suit(self):
-        return [e for e in self.html.xpath('//tr/td[4]//text()')]
+        return [e for e in self.html.xpath('//tr[./td[1]/a//text()]/td[4]//text()')]
 
     def _download_backwards(self, n):
         self.url = 'http://media.ca11.uscourts.gov/opinions/pub/logname.php?begin={}&num={}&numBegin=1'.format(
