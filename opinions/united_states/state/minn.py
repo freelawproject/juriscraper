0f34c304bd361a16c423c2c565f09247a7af38c7
Philip Ardery <arderyp@gmail.com>
2016-04-29 18:58:57 -0700

    Minnesota court portals changed the url ar which the opinions are available, and also changed the result output/formatting.  Old example files are removed, as they are no longer relevant with the new formatting.  Single example files for each of the 3 minn scrapers has been updated to reflect new format.  The logic is all greatly simplified, but there is a new challenge in that the docket number is no longer explicitly published.  So, instead of parsing it from an explicit string, we now have to infer it from the given opinion's url.  See the comment in the code for more detail.

--- opinions/united_states/state/minn.py
+++ opinions/united_states/state/minn.py
@@ -5,88 +5,51 @@
 #Reviewer: mlr
 #Date: 2014-07-03
 
-from datetime import date
-import time
-
 from juriscraper.OpinionSite import OpinionSite
-from juriscraper.lib.date_utils import quarter, is_first_month_in_quarter
-from lxml import html
-import re
-from requests.exceptions import HTTPError
+from juriscraper.lib.string_utils import convert_date_string
 
 
 class Site(OpinionSite):
+    court_abbreviation = 'supct'
+
     def __init__(self, *args, **kwargs):
         super(Site, self).__init__(*args, **kwargs)
         self.court_id = self.__module__
-        d = date.today()
-        self.url = 'http://mn.gov/lawlib/archive/sct{short_year}q{quarter}.html'.format(
-            short_year=d.strftime("%y"),
-            quarter=quarter(d.month)
-        )
-        # self.url = 'http://mn.gov/lawlib/archive/sct14q3.html'
-
-    def _download(self, request_dict={}):
-        """Overrides the download function so that we can catch 404 errors
-        silently. This is necessary because these web pages simply do not exist
-        for several days at the beginning of each quarter.
-        """
-        try:
-            return super(Site, self)._download()
-        except HTTPError, e:
-            is_first_days_of_the_quarter = (
-                date.today().day <= 15 and
-                is_first_month_in_quarter(date.today().month)
-            )
-            got_404 = (e.response.status_code == 404)
-            if got_404 and is_first_days_of_the_quarter:
-                # Do nothing; abort the crawler
-                self.status = 200
-                # We need the body tag here so that xpath works elsewhere.
-                html_tree = html.fromstring('<html><body></body></html>')
-                return html_tree
-            else:
-                raise e
+        self.url = 'http://mn.gov/law-library/search/index.jsp?v%3Asources=mn-law-library-opinions&query=+%28url%3A%2Farchive%2F' + \
+                   self.court_abbreviation + '%29+&sortby=date'
 
     def _get_case_names(self):
-        path = '''//ul//li[not(contains(text(), 'ORDER') or
-                               contains(text(), 'NO OPINIONS'))]/text()'''
-
-        return list(self.html.xpath(path))
+        return [name.strip() for name in self.html.xpath("//a[@class='searchresult_title']/text()")]
 
     def _get_download_urls(self):
-        path = '''//ul//li[not(contains(text(), 'ORDER') or
-                               contains(text(), 'NO OPINIONS'))]//@href'''
-        return list(self.html.xpath(path))
+        return [url.strip() for url in self.html.xpath("//span[@class='searchresult_url']/text()")]
 
     def _get_case_dates(self):
-        path = '''//ul//h4/text()'''
-        dates = self.html.xpath(path)
-        last_date_index = len(dates) - 1
-        case_dates = []
-        for index, date_element in enumerate(dates):
-            if index < last_date_index:
-                path_2 = ("//h4[{c}]/following-sibling::li/text()[count("
-                          "  .|//h4[{n}]/preceding-sibling::li/text())="
-                          "count("
-                          "    //h4[{n}]/preceding-sibling::li/text()"
-                          ") and not("
-                          "    contains(., 'ORDER') or"
-                          "    contains(., 'NO OPINIONS')"
-                          ")]".format(c=index + 1,
-                                      n=index + 2))
-            else:
-                path_2 = ("//h4[{c}]/following-sibling::li/text()[not("
-                          "  contains(., 'ORDER') or"
-                          "  contains(., 'NO OPINIONS'))]").format(c=index + 1)
-            d = date.fromtimestamp(time.mktime(time.strptime(re.sub(' ', '', str(date_element)), '%B%d,%Y')))
-            case_dates.extend([d] * len(self.html.xpath(path_2)))
-        return case_dates
+        dates = []
+        for date in self.html.xpath("//div[@class='searchresult_date']/text()"):
+            date = date.strip()
+            if date:
+                date = ' '.join(date.strip().split())
+                dates.append(convert_date_string(date))
+        return dates
 
     def _get_precedential_statuses(self):
         return ['Published'] * len(self.case_names)
 
     def _get_docket_numbers(self):
-        path = '''//ul//li[not(contains(text(), 'ORDER') or
-                               contains(text(), 'NO OPINIONS'))]/a/text()'''
-        return list(self.html.xpath(path))
+        """At the time of coding, the only reliable way to get docket numbers
+        is from the resource url.  Sometimes they are included in the case names
+        but many cases are too long, cut off, and do not include them.  We also
+        have to guess at their standard numbering convention, assuming A##-####,
+        which is the correct format the majority of the time, but sometimes they
+        seem to drop a zero in the case of A##-0###.
+        """
+        dockets = []
+        for url in self._get_download_urls():
+            file_name = url.split('/')[-1]
+            docket = file_name.split('-')[0].upper()[2:]
+            dockets.append('%s-%s' % (docket[:3], docket[3:]))
+        return dockets
+
+    def _get_summaries(self):
+        return [summary.strip() for summary in self.html.xpath("//div[@class='searchresult_snippet']/text()")]
