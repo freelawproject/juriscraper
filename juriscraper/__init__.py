7c9fc6b0d534bc7389cfbe58ea420a02e87ae91f
Michael Lissner <mlissner@michaeljaylissner.com>
2016-06-03 14:17:56 -0700

    Release 1.1.7

--- juriscraper/__init__.py
+++ juriscraper/__init__.py
@@ -3,7 +3,7 @@ __all__ = [
     'oral_args',
 ]
 
-__version__ = "1.1.6.2"
+__version__ = "1.1.7"
 
 __title__ = "juriscraper"
 __description__ = "An API to scrape American court websites for metadata."
