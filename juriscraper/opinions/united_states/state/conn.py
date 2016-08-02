6e2dd17f22154078570b477c814a34126d10d1a5
Philip Ardery <arderyp@gmail.com>
2016-08-02 06:46:41 -0700

    Fixing conn scraper.  Site introduced new formatting inconsistency in how it presents it's dates.  Fixed scraper to handle that.  Added new test file as well. (#141)

--- juriscraper/opinions/united_states/state/conn.py
+++ juriscraper/opinions/united_states/state/conn.py
@@ -9,13 +9,15 @@ History:
  - 2014-08-08, mlr: updated to fix InsanityError on case_dates
  - 2014-09-18, mlr: updated XPath to fix InsanityError on docket_numbers
  - 2015-06-17, mlr: made it more lenient about date formatting
+ - 2016-07-21, arderyp: fixed to handle altered site format
 """
 
-from datetime import date, datetime
 import re
+from datetime import date
 
 from juriscraper.OpinionSite import OpinionSite
 from juriscraper.lib.string_utils import clean_string
+from juriscraper.lib.string_utils import convert_date_string
 
 
 class Site(OpinionSite):
@@ -29,9 +31,9 @@ class Site(OpinionSite):
     def _get_case_names(self):
         case_names = []
         path = '//*[@id="AutoNumber1"]/tr[2]/td/table/tr/td//ul//text()'
-        for s in self.html.xpath(path):
-            if re.search(u'[–-]', s):
-                case_names.append(clean_string(s))
+        for text in self.html.xpath(path):
+            if re.search(u'[–-]', text):
+                case_names.append(clean_string(text))
         return case_names
 
     def _get_download_urls(self):
@@ -39,23 +41,24 @@ class Site(OpinionSite):
 
     def _get_case_dates(self):
         dates = []
-        for title in self.html.xpath('//table[@id="AutoNumber1"]/tr[2]/td/table/tr/td//b//text()'):
-            count = len(title.getparent().xpath("following::ul[1]//a/@href[contains(., 'pdf')]"))
-            date_string = title.split()[-1].strip(':')
-            for format in ['%m/%d/%y', '%m/%d/%Y', None]:
-                try:
-                    dates.extend([datetime.strptime(date_string, format).date()] * count)
-                    break
-                except ValueError:
-                    continue
+        target_cell = self.html.xpath('//table[@id="AutoNumber1"]/tr[2]/td/table/tr/td')[0]
+        for text in target_cell.xpath('//b//text() | //strong//text()'):
+            text_clean = clean_string(text)
+            if text_clean:
+                last_substring = text_clean.split()[-1]
+                if last_substring.endswith(':') and last_substring.count('/') == 2:
+                    date = convert_date_string(last_substring.rstrip(':'))
+                    count = len(text.getparent().xpath("following::ul[1]//a/@href[contains(., 'pdf')]"))
+                    dates.extend([date] * count)
         return dates
 
     def _get_docket_numbers(self):
-        docket_numbers = []
-        for d in self.html.xpath("//a[contains(./@href, '.pdf')]//text()"):
-            if re.search(r"(A?S?C\d{3,5})", d):
-                docket_numbers.append(d)
-        return docket_numbers
+        dockets = []
+        for text in self.html.xpath("//a[contains(./@href, '.pdf')]//text()"):
+            if re.search(r"(A?S?C\d{3,5})", text):
+                docket = text.strip('Dissent').strip('Concurrence').strip()
+                dockets.append(docket)
+        return dockets
 
     def _get_precedential_statuses(self):
         return ['Published'] * len(self.case_dates)
