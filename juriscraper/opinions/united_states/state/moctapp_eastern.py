8ea4033ab80811428297634626163f0a4c5ad2e6
Philip Ardery <arderyp@gmail.com>
2016-08-02 07:24:55 -0700

    Fixing mo scraper.  Restructured main mo scraper to use more standard business logic and be more intuitive.  Simplified children scrapers.  One of the crucial html id tags was changed, which was causing these scrapers to break.  I've updated the example files to reflect the new id. (#143)

--- juriscraper/opinions/united_states/state/moctapp_eastern.py
+++ juriscraper/opinions/united_states/state/moctapp_eastern.py
@@ -1,13 +1,13 @@
-from datetime import date
 import mo
 
 
 class Site(mo.Site):
     def __init__(self, *args, **kwargs):
         super(Site, self).__init__(*args, **kwargs)
-        today = date.today()
-        self.url = 'https://www.courts.mo.gov/page.jsp?id=12086&dist=Opinions Eastern&date=all&year=%s#all' % today.year
         self.court_id = self.__module__
 
+    def url_slug(self):
+        return 'Eastern'
+
     def _get_divisions(self):
-        return ['Eastern Dist.'] * len(self.case_names)
+        return ['Eastern Dist.'] * len(self.cases)
