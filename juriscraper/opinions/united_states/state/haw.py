a6aa2a9c5f404f30210740232dc6253fa555c8da
Philip Ardery <arderyp@gmail.com>
2016-08-15 14:10:04 -0700

    Fixing haw/hawapp.  The site url changed.  I simplified hawapp to be a simple extension of haw.  Improved backscraper to loop over all result pages.  I deviated away from the very long and complex lxml/xpath paths used in the previous implementation of haw/hawapp because I found them to be complex and difficult to interpret. (#151)

--- juriscraper/opinions/united_states/state/haw.py
+++ juriscraper/opinions/united_states/state/haw.py
@@ -1,52 +1,72 @@
 # Author: Michael Lissner
 # Date created: 2013-05-23
 
-# import re
-import time
 from datetime import date
-from lxml import html
 
 from juriscraper.OpinionSite import OpinionSite
+from juriscraper.lib.string_utils import convert_date_string
 
 
 class Site(OpinionSite):
     def __init__(self, *args, **kwargs):
         super(Site, self).__init__(*args, **kwargs)
-        self.url = 'http://www.courts.state.hi.us/opinions_and_orders/opinions/%s/index.html' % date.today().year
-        self.back_scrape_iterable = range(2010, 2013)
+        year = date.today().year
+        self.url = 'http://www.courts.state.hi.us/opinions_and_orders/opinions?yr=%s' % year
+        self.back_scrape_iterable = range(2010, year + 1)
         self.court_id = self.__module__
+        self.target_id = 'S.Ct'
+        self.cases = []
+
+    def _download(self, request_dict={}):
+        html = super(Site, self)._download(request_dict)
+        self.extract_cases_from_table(html)
+        return html
+
+    def extract_cases_from_table(self, html):
+        path_rows = '//div[@id="table-content"]/table/tbody/tr'
+        path_target = './td[2]/div/span/text()'
+        for row in html.xpath(path_rows):
+            court = row.xpath(path_target)
+            if court and court[0].strip() == self.target_id:
+                case_text = row.xpath('./td[4]/p/text()')[0]
+                court_text = row.xpath('./td[5]/text()')
+                self.cases.append({
+                    'url': row.xpath('./td[3]/a/@href')[0],
+                    'date': row.xpath('./td[1]/text()')[0],
+                    'name': case_text.split(' (')[0],
+                    'docket': row.xpath('./td[3]/a/text()')[0],
+                    'court': court_text[0] if court_text else False,
+                })
 
     def _get_download_urls(self):
-        path = '//table[@class = "opinionsdata"]/tr/td[3][../td/text() = "S.Ct"][not(contains(../td[3]/a/text(), "Bar"))]/a[1]/@href'
-        return list(self.html.xpath(path))
+        return [case['url'] for case in self.cases]
 
     def _get_case_names(self):
-        # There is often a missing link in a case, thus we need an extra check here.
-        path = '//table[@class = "opinionsdata"]/tr/td[4][../td/a][../td/text() = "S.Ct"][not(contains(../td[3]/a/text(), "Bar"))]/p[1]/text()[1]'
-        case_names = []
-        for s in self.html.xpath(path):
-            case_names.append(s.split('(')[0])
-        return case_names
+        return [case['name'] for case in self.cases]
 
     def _get_case_dates(self):
-        path = '//table[@class = "opinionsdata"]/tr/td[1][../td/a][../td/text() = "S.Ct"][not(contains(../td[3]/a/text(), "Bar"))]/text()'
-        return [date.fromtimestamp(time.mktime(time.strptime(date_string, '%m/%d/%Y')))
-                for date_string in self.html.xpath(path)]
+        return [convert_date_string(case['date']) for case in self.cases]
 
     def _get_precedential_statuses(self):
-        return ['Published'] * len(self.case_names)
+        return ['Published'] * len(self.cases)
 
     def _get_docket_numbers(self):
-        path = '//table[@class = "opinionsdata"]/tr/td[3][../td/a][../td/text() = "S.Ct"][not(contains(../td[3]/a/text(), "Bar"))]/a[1]/text()'
-        return list(self.html.xpath(path))
+        return [case['docket'] for case in self.cases]
 
     def _get_lower_courts(self):
-        path = '//table[@class = "opinionsdata"]/tr/td[5][../td/a][../td/text() = "S.Ct"][not(contains(../td[3]/a/text(), "Bar"))]'
-        lower_courts = []
-        for e in self.html.xpath(path):
-            lower_courts.append(html.tostring(e, method='text', encoding='unicode'))
-        return lower_courts
+        return [case['court'] for case in self.cases]
 
     def _download_backwards(self, year):
-        self.url = 'http://www.courts.state.hi.us/opinions_and_orders/opinions/%s/index.html' % year
+        if int(year) < 2010:
+            raise Exception('the haw portal does not include pre-2010 cases')
+
+        self.url = 'http://www.courts.state.hi.us/opinions_and_orders/opinions?yr=%s' % year
         self.html = self._download()
+
+        # Loop over all pages
+        pages = self.html.xpath('//a[@class="page-numbers"]/text()')
+        if pages:
+            last_page = int(pages[-1])
+            for page in range(2, last_page + 1):
+                self.url = 'http://www.courts.state.hi.us/opinions_and_orders/opinions/page/%d?yr=%s' % (page, year)
+                self.html = self._download()
