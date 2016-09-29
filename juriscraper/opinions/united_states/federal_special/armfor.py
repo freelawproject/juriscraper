22f445e894296c25fbfdb59441aab83074f6dabc
Philip Ardery <arderyp@gmail.com>
2016-09-28 21:23:10 -0700

    Fixing armfor.  They are using non-standard ways of building their Term opinions urls.  Thus, we can't predict them with a simple algorithm. So, I've adjusted to scraper to work from the Term listing landing page.  When running live, it will only scraper the most recent url in the list, which covers the past year's worth of opinions.  I've enabled a backscraper that will direct the scraper to walk over all of the links, except for the 1997 and 1998 links, as those opinion listings present a different format, and also exclude case dates.  After deploying this commit, we may want to run the backscraper to pick up anything we've missed over time. I've updated the example file to use the landing page HTML, instead of a specific term page's html.

--- juriscraper/opinions/united_states/federal_special/armfor.py
+++ juriscraper/opinions/united_states/federal_special/armfor.py
@@ -2,48 +2,84 @@
 CourtID: armfor
 Court Short Name: C.A.A.F."""
 
-from datetime import date, datetime
+import certifi
+import requests
+from lxml.html import html5parser, fromstring, tostring
 
 from juriscraper.OpinionSite import OpinionSite
-from lxml import html
+from juriscraper.lib.string_utils import convert_date_string
 
 
+# This court has some funky html that varies
+# across the old term listings. Before committing
+# changes, make sure you haven't broken the backscraper
+# by running: python juriscraper/sample_caller.py -c opinions.united_states.federal_special.armfor --backscrape
 class Site(OpinionSite):
     def __init__(self, *args, **kwargs):
         super(Site, self).__init__(*args, **kwargs)
-        today = date.today()
-        if today.month > 8 and today.day > 4:
-            url_year = today.year
-        else:
-            url_year = today.year - 1
-        self.url = 'http://www.armfor.uscourts.gov/newcaaf/opinions/%sSepTerm.htm' % url_year
         self.court_id = self.__module__
+        self.session = requests.session()
+        self.back_scrape_iterable = ['']
+        self.url = "http://www.armfor.uscourts.gov/newcaaf/opinions.htm"
+        self.row_base_path = '//table[@border="1"]//tr[descendant::a]'
+        self.path_to_landing_page_links = '//blockquote/ul/li[1]/font/a[1]/@href'
+
+    def _download(self, request_dict={}):
+        landing_page_html = super(Site, self)._download(request_dict)
+        urls = landing_page_html.xpath(self.path_to_landing_page_links)
+        return [self.html_from_url(url, request_dict) for url in urls]
 
     def _get_case_names(self):
-        return [t for t in self.html.xpath('//table//tr[descendant::a]/td[1]/font/text()')]
+        names = []
+        path = '%s/td[1]' % self.row_base_path
+        for html_tree in self.html:
+            names.extend([cell.text_content().strip() for cell in html_tree.xpath(path)])
+        return names
 
     def _get_download_urls(self):
-        return [t for t in self.html.xpath('//table//tr[descendant::a]/td[2]/font/a/@href')]
+        urls = []
+        path = '%s/td[2]//a[last()]/@href' % self.row_base_path
+        for html_tree in self.html:
+            urls.extend([url for url in html_tree.xpath(path)])
+        return urls
 
     def _get_case_dates(self):
         dates = []
-        date_formats = ('%b %d, %Y', '%B %d, %Y')
-        for s in self.html.xpath('//table//tr[descendant::a]/td[3]/font/text()'):
-            s = s.strip()
-            for date_format in date_formats:
-                try:
-                    dates.append(datetime.strptime(s, date_format).date())
-                    break
-                except ValueError:
-                    continue
+        path = '%s/td[3]' % self.row_base_path
+        for html_tree in self.html:
+            for cell in html_tree.xpath(path):
+                dates.append(convert_date_string(cell.text_content()))
         return dates
 
     def _get_docket_numbers(self):
-        docket_numbers = []
-        for e in self.html.xpath('//table//table//tr[descendant::a and position() > 1]/td[2]'):
-            s = html.tostring(e, method='text', encoding='unicode')
-            docket_numbers.append(s.strip()[:-5])
-        return docket_numbers
+        dockets = []
+        path = '%s//td[2]' % self.row_base_path
+        for html_tree in self.html:
+            for cell in html_tree.xpath(path):
+                docket_raw = cell.text_content().strip()
+                dockets.append(docket_raw.rstrip('(PDF)'))
+        return dockets
 
     def _get_precedential_statuses(self):
         return ['Published'] * len(self.case_names)
+
+    def _download_backwards(self, _):
+        # We skip the 1998 and 1999 entries because they
+        # are formatted differently and don't contain dates
+        limitation = 'not(contains(@href, "1998Term.htm")) and not(contains(@href, "1997Term.htm"))'
+        self.path_to_landing_page_links = '//blockquote/ul/li/font/a[1][%s]/@href' % limitation
+        self.html = self._download()
+
+    def html_from_url(self, url, request_dict={}):
+        request = self.session.get(
+            url,
+            headers={'User-Agent': 'Juriscraper'},
+            verify=certifi.where(),
+            **request_dict
+        )
+        request.raise_for_status()
+        if request.encoding == 'ISO-8859-1':
+            request.encoding = 'cp1252'
+        text = self._clean_text(request.text)
+        parsed = html5parser.document_fromstring(text.encode('utf-8'))
+        return fromstring(tostring(parsed))
