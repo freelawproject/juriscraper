b5fe8a5b4c1ccefae04df27cd6df8edc2c47d8b7
Philip Ardery <arderyp@gmail.com>
2017-03-20 19:25:48 -0700

    Fixing ca4 oral_args. Resource moved from http://www.ca4.uscourts.gov/OAarchive/OAlist.asp to http://www.ca4.uscourts.gov/oral-argument/listen-to-oral-arguments

--- juriscraper/oral_args/united_states/federal_appellate/ca4.py
+++ juriscraper/oral_args/united_states/federal_appellate/ca4.py
@@ -5,39 +5,30 @@
 # Reviewer: mlr
 # Date created: 18 July 2014
 
-
-from datetime import datetime
-
 from juriscraper.OralArgumentSite import OralArgumentSite
+from juriscraper.lib.string_utils import convert_date_string
 
 
 class Site(OralArgumentSite):
     def __init__(self, *args, **kwargs):
         super(Site, self).__init__(*args, **kwargs)
         self.court_id = self.__module__
-        self.url = 'http://www.ca4.uscourts.gov/OAarchive/OAlist.asp'
+        self.url = 'http://www.ca4.uscourts.gov/oral-argument/listen-to-oral-arguments'
 
     def _get_download_urls(self):
-        path = '//tr/td[2]/a/@href'
-        return list(self.html.xpath(path))
+        return [href for href in self.html.xpath('//tr/td[2]//a/@href')]
 
     def _get_case_names(self):
-        path = '//tr/td[3]/text()'
-        return list(self.html.xpath(path))
+        return self.text_from_cell(3)
 
     def _get_case_dates(self):
-        path = '//tr/td[1]/text()'
-        return map(self._return_case_date, self.html.xpath(path))
-
-    @staticmethod
-    def _return_case_date(e):
-        e = ''.join(e.split())
-        return datetime.strptime(e, '%m/%d/%Y').date()
+        return [convert_date_string(date) for date in self.text_from_cell(1)]
 
     def _get_judges(self):
-        path = '//tr/td[4]/text()'
-        return list(self.html.xpath(path))
+        return self.text_from_cell(4)
 
     def _get_docket_numbers(self):
-        path = '//tr/td[2]/a/text()'
-        return list(self.html.xpath(path))
+        return self.text_from_cell(2)
+
+    def text_from_cell(self, cell_number):
+        return [cell.text_content().strip() for cell in self.html.xpath('//tr/td[%d]' % cell_number)]
