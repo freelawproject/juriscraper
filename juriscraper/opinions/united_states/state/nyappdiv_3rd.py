13359f94c4797d348f8142be4cb32ee5b08fbdd0
Philip Ardery <arderyp@protonmail.com>
2017-05-17 21:21:14 -0700

    Changing nyappdiv_3rd and nyappdiv_4th to extend nyappdiv_1st, the same way nyappdiv_2nd does.  Now all 4 scrapers are pulling data from the same source.  This is easier, makes for more minimal code, and also revitalizes nyappdiv_3rd, which has been failing since January 2017.  It seems that the 3rd division is no longer publishing opinions to the old url we were using.

--- juriscraper/opinions/united_states/state/nyappdiv_3rd.py
+++ juriscraper/opinions/united_states/state/nyappdiv_3rd.py
@@ -1,99 +1,13 @@
-# -*- coding: utf-8 -*-
 # Scraper for New York Appellate Divisions 3rd Dept.
 # CourtID: nyappdiv_3rd
 # Court Short Name: NY
-# History:
-#   2014-07-04: Created by Andrei Chelaru
-#   2014-07-05: Reviewed by mlr
-#   2014-12-15: Updated to fix regex and insanity errors.
-#   2016-02-17: Updated by arderyp, regex was breaking due to new page section.
-#   2016-03-08: Updated by arderyp, Added regex back to handle human typos found
-#               on older pages, and added fallback loose checking to handle
-#               differently formatted disciplinary/admissions docket numbers
-#   2016-08-03: Updated by arderyp to handle junk/repetitive anchor tags
 
-import re
-import six
-
-from datetime import date
-from dateutil.relativedelta import relativedelta, TH
-
-from juriscraper.OpinionSite import OpinionSite
-from juriscraper.lib.string_utils import clean_if_py3
+from juriscraper.opinions.united_states.state import nyappdiv_1st
 
 
-class Site(OpinionSite):
+class Site(nyappdiv_1st.Site):
     def __init__(self, *args, **kwargs):
         super(Site, self).__init__(*args, **kwargs)
         self.court_id = self.__module__
-        # Last thursday, this court publishes weekly
-        self.crawl_date = date.today() + relativedelta(weekday=TH(-1))
-        self.url = 'http://decisions.courts.state.ny.us/ad3/CalendarPages/nc{month_nr}{day_nr}{year}.htm'.format(
-            month_nr=self.crawl_date.strftime("%m"),
-            day_nr=self.crawl_date.strftime("%d"),
-            year=self.crawl_date.year
-        )
-        self.LINK_PATH = "//td[2]//a[contains(./@href, 'Decisions')]"
-        self.LINK_HREF_PATH = '%s/@href' % self.LINK_PATH
-        self.LINK_TEXT_PATTERN_MEMORANDUM = (r'(?P<docket_number>^\d+((/\d+)+)?)(/)?'
-                                             r'(?P<case_name>.+)')
-        self.LINK_TEXT_PATTERN_ATTORNEY = (r'(?P<docket_number>^\w{1}-\d+-\d+)'
-                                          r'(?P<case_name>.+)')
-
-    def _get_case_names(self):
-        case_names = []
-        for link in self.html.xpath(self.LINK_PATH):
-            text = link.text_content().strip()
-            if text:
-                docket, name = self._get_docket_and_name_from_text(text)
-                if name:
-                    case_names.append(name)
-        return case_names
-
-    def _get_download_urls(self):
-        urls = []
-        for link in self.html.xpath(self.LINK_PATH):
-            if link.text_content().strip():
-                urls.append(link.xpath('@href')[0])
-        return urls
-
-    def _get_case_dates(self):
-        return [self.crawl_date] * len(self._get_case_names())
-
-    def _get_precedential_statuses(self):
-        return ['Published'] * len(self.case_names)
-
-    def _get_docket_numbers(self):
-        docket_numbers = []
-        for link in self.html.xpath(self.LINK_PATH):
-            text = link.text_content().strip()
-            if text:
-                docket, name = self._get_docket_and_name_from_text(text)
-                if docket:
-                    docket_numbers.append(docket)
-        return docket_numbers
-
-    def _get_docket_and_name_from_text(self, text):
-        text = self._sanitize_docket_name_text(text)
-        if not text:
-            return False, False
-        if text.split()[0].count('-') == 2:
-            regex = self.LINK_TEXT_PATTERN_ATTORNEY
-        else:
-            regex = self.LINK_TEXT_PATTERN_MEMORANDUM
-        data = re.search(regex, text)
-        docket_raw = data.group('docket_number')
-        docket = ', '.join(docket_raw.split('/'))
-        name = ' '.join(data.group('case_name').split())
-
-        return docket, name
-
-    def _sanitize_docket_name_text(self, text):
-        text = clean_if_py3(text).strip()
-        first_word = text.split()[0]
-
-        # Replace en dash typo with proper hyphen so regex parses properly
-        en_dash = b'\xe2\x80\x93'.decode('utf-8')
-        first_word_sanitized = first_word.replace(en_dash, '-')
-
-        return text.replace(first_word, first_word_sanitized)
+        self.division = 3
+        self.url = self.build_url()
--- juriscraper/opinions/united_states/state/nyappdiv_4th.py
+++ juriscraper/opinions/united_states/state/nyappdiv_4th.py
@@ -1,111 +1,13 @@
 # Scraper for New York Appellate Divisions 4th Dept.
-#CourtID: nyappdiv_4th
-#Court Short Name: NY
-#Author: Andrei Chelaru
-#Reviewer: mlr
-#Date: 2014-07-04
+# CourtID: nyappdiv_4th
+# Court Short Name: NY
 
-from juriscraper.OpinionSite import OpinionSite
-from juriscraper.lib.string_utils import convert_date_string
+from juriscraper.opinions.united_states.state import nyappdiv_1st
 
 
-class Site(OpinionSite):
+class Site(nyappdiv_1st.Site):
     def __init__(self, *args, **kwargs):
         super(Site, self).__init__(*args, **kwargs)
         self.court_id = self.__module__
-        self.back_scrape_iterable = ['']
-        self.backscraping = False
-        self.path_to_year_links = '//div[@id="editBody"]/table/tr[2]/td/a[1]/@href'
-        self.path_to_date_links = '//ul[@class="nav nav-pills nav-stacked"]/li/a[1]/@href'
-        self.path_to_cases_link = '//a[contains(text(), "- by CASE NAME")]/@href[1]'
-        self.path_to_date = '//div[@class="page-header"]/h1/small'
-        self.path_to_row = '//div[@id="editBody"]//tr'
-        self.url = "http://www.nycourts.gov/courts/ad4/Clerk/Decisions/"
-        self.cases = []
-
-    def _download(self, request_dict={}):
-        """Traverse link tree to find pertinent resource pages.
-
-        This is a fun scraper, because we need to traverse multiple
-        landing pages in order to find the target resource pages,
-        since they are not posted on a regular schedule that would
-        allow up to algorithmically build target resource urls.
-        """
-        landing_page_html = super(Site, self)._download(request_dict)
-
-        # Test files should be direct target resource page html,
-        # not landing page. This prevents hitting the network on tests.
-        if self.method == 'LOCAL':
-            self.extract_case_data_from_html(landing_page_html)
-            return landing_page_html
-
-        # For live runs, traverse landing page tree
-        html_trees = []
-        for year_link in landing_page_html.xpath(self.path_to_year_links):
-            years_page_html = self._get_html_tree_by_url(year_link)
-            for date_link in years_page_html.xpath(self.path_to_date_links):
-                # Stop processing after first resource page if not running backscraper
-                if not self.backscraping and html_trees:
-                    return html_trees
-                # Crappy anchor html practices make below necessary
-                if not date_link.endswith('.html') and not date_link.endswith('/'):
-                    date_link += '/'
-                date_page_html = self._get_html_tree_by_url(date_link)
-                cases_link = date_page_html.xpath(self.path_to_cases_link)
-                if cases_link:
-                    cases_page_html = self._get_html_tree_by_url(cases_link[0])
-                    self.extract_case_data_from_html(cases_page_html)
-                    html_trees.append(cases_page_html)
-        return html_trees
-
-    def _download_backwards(self, _):
-        self.backscraping = True
-        # Adjust paths to work with legacy pages
-        self.path_to_date_links += ' | //div[@id="page_content"]/a/@href[1]'
-        self.path_to_row += ' | //div[@id="page_content"]//tr'
-        self.path_to_date += ' | //div[@id="page_content"]/p[1]'
-        self.html = self._download()
-
-    def extract_case_data_from_html(self, html):
-        date_element = html.xpath(self.path_to_date)[0]
-        date_string = date_element.text_content().strip().replace('Decisions - ', '')
-        date = convert_date_string(date_string)
-        for row in html.xpath(self.path_to_row):
-            first_cell = row.xpath('./td[1]')
-            # Skip blank rows that court sometimes includes
-            if first_cell:
-                name = first_cell[0].text_content().strip()
-                # Skip non case record that court sometimes includes at bottom of table
-                if self.is_valid_name(name):
-                    self.cases.append({
-                        'name': name,
-                        'url': row.xpath('./td[1]/a/@href')[0],
-                        'docket': row.xpath('./td[3]')[0].text_content().strip(),
-                        'date': date,
-                        'status': 'Published'
-                    })
-
-    def is_valid_name(self, name):
-        if not name:
-            return False
-        lower = name.lower()
-        if 'motions' in lower and 'crawfords' in lower:
-            return False
-        if lower == 'motions':
-            return False
-        return True
-
-    def _get_case_dates(self):
-        return [case['date'] for case in self.cases]
-
-    def _get_case_names(self):
-        return [case['name'] for case in self.cases]
-
-    def _get_download_urls(self):
-        return [case['url'] for case in self.cases]
-
-    def _get_precedential_statuses(self):
-        return [case['status'] for case in self.cases]
-
-    def _get_docket_numbers(self):
-        return [case['docket'] for case in self.cases]
+        self.division = 4
+        self.url = self.build_url()
