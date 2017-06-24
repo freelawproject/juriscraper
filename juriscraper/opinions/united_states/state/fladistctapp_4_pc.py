30d8a6e52b48759671832753c9e9ce6d3fe57160
Philip Ardery <arderyp@protonmail.com>
2017-06-24 10:34:42 -0700

    Breaking fladistctapp_4 into fladistctapp_4_pc and fladistctapp_4_written due to changes in website structure. Adding initial example files.  For now, I am leaving fladistctapp_4 in place, as is, incase @mike wants to kill this split approach for some reason.  If he doesn't, that scraper, and its example files, should be removed in the next release.

---
+++ juriscraper/opinions/united_states/state/fladistctapp_4_pc.py
@@ -0,0 +1,13 @@
+# Scraper for Florida 4th District Court of Appeal Per Curiam Opinions
+# CourtID: flaapp4pc
+# Court Short Name: flaapp4pc
+
+from juriscraper.opinions.united_states.state import fladistctapp_4_written
+
+
+class Site(fladistctapp_4_written.Site):
+    """Web Interface: http://www.4dca.org/opinions_auto/most_recent_pc.shtml"""
+    def __init__(self, *args, **kwargs):
+        super(Site, self).__init__(*args, **kwargs)
+        self.court_id = self.__module__
+        self.set_url('p')
