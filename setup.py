00f586682c4c7e82b779c5dbad3584606a730a39
Michael Lissner <mlissner@michaeljaylissner.com>
2017-07-10 15:11:17 -0700

    Version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 from pip.req import parse_requirements
 from setuptools import setup, find_packages
 
-VERSION = "1.5.12"
+VERSION = "1.5.13"
 AUTHOR = "Mike Lissner"
 EMAIL = "info@free.law"
 HERE = os.path.abspath(os.path.dirname(__file__))
