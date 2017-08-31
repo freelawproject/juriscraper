648c2c5ec09f5d28ce9182eac47187f099fef553
Philip Ardery <arderyp@protonmail.com>
2017-08-30 20:08:12 -0700

    Fixing DC. Changed resource url and site layout.

--- juriscraper/opinions/united_states/state/dc.py
+++ juriscraper/opinions/united_states/state/dc.py
@@ -13,8 +13,14 @@ class Site(OpinionSite):
     def __init__(self, *args, **kwargs):
         super(Site, self).__init__(*args, **kwargs)
         self.court_id = self.__module__
-        self.url = 'http://www.dccourts.gov/internet/opinionlocator.jsf'
-        self.base_path = '//table//tr[not(contains(td[2]/span/text(), "NO OPINIONS"))]'
+        self.url = 'https://www.dccourts.gov/court-of-appeals/opinions-memorandum-of-judgments'
+        qualifier_no_opinions = 'not(contains(td[2]/span/text(), "NO OPINIONS"))'
+        qualifier_has_pdf_link = 'contains(.//td[1]/a/@href, ".pdf")'
+        self.base_path = '//table//tr[%s and %s]' % (qualifier_no_opinions, qualifier_has_pdf_link)
+
+    def _get_docket_numbers(self):
+        path = '%s/td[1]/a' % self.base_path
+        return [cell.text_content().strip() for cell in self.html.xpath(path)]
 
     def _get_download_urls(self):
         path = '%s/td[1]/a/@href' % self.base_path
@@ -30,7 +36,3 @@ class Site(OpinionSite):
 
     def _get_precedential_statuses(self):
         return ["Published"] * len(self.case_names)
-
-    def _get_docket_numbers(self):
-        path = '%s/td[1]/a' % self.base_path
-        return [cell.text_content() for cell in self.html.xpath(path)]
