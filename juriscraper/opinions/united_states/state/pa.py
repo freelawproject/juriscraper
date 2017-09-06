0faeb48c05e341df5643c20a9a23eb1b2a2ed4f4
Philip Ardery <arderyp@protonmail.com>
2017-09-05 19:25:33 -0700

    Fixing pacommwct via pa.  Added new example and (generated) compare.json file for new use case.  In fixing this scraper, I discovered a bug in the scraper that was causing the docket numbers to be scraped inaccurately.  As a result, I had to regenrate the compare.json files for all pa and children-of-pa scraper example files.

--- juriscraper/opinions/united_states/state/pa.py
+++ juriscraper/opinions/united_states/state/pa.py
@@ -1,4 +1,6 @@
-#  Scraper for Pennsylvania Supreme Court
+# -*- coding: utf-8 -*-
+
+# Scraper for Pennsylvania Supreme Court
 # CourtID: pa
 # Court Short Name: pa
 # Author: Andrei Chelaru
@@ -26,31 +28,35 @@ class Site(OpinionSite):
         self.regex = re.compile(pattern)
 
     def _get_case_names(self):
-        path = "{base}/title/text()".format(base=self.base)
-        return list(map(self.return_case_name, self.html.xpath(path)))
+        path = "%s/title" % self.base
+        return [self.return_case_name(s) for s in self.return_path_text(path)]
 
     def _get_download_urls(self):
-        return [href for href in self.html.xpath('%s//@href' % self.base)]
+        path = '%s//@href' % self.base
+        return [href for href in self.html.xpath(path)]
 
     def _get_case_dates(self):
-        path = "{base}/pubdate/text()".format(base=self.base)
-        return [convert_date_string(s) for s in self.html.xpath(path)]
+        path = "%s/pubdate" % self.base
+        return [convert_date_string(s) for s in self.return_path_text(path)]
 
     def _get_precedential_statuses(self):
         return ['Published'] * len(self.case_names)
 
     def _get_docket_numbers(self):
-        return self.return_path_text('%s/title' % self.base)
+        path = "%s/title" % self.base
+        return [self.return_docket_number(s) for s in self.return_path_text(path)]
 
     def _get_judges(self):
         return self.return_path_text('%s/creator' % self.base)
 
-    def return_case_name(self, s):
-        match = self.regex.search(s.replace(r'\n', '\n'))
-        return match.group(1)
-
-    def return_docket_number(self, e):
-        return self.regex.search(e.replace(r'\n', '\n')).group(2)
-
     def return_path_text(self, path):
-        return [element.text_content() for element in self.html.xpath(path)]
+        return [self.sanitize_text(element.text_content()) for element in self.html.xpath(path)]
+
+    def return_case_name(self, text):
+        return self.regex.search(text).group(1)
+
+    def return_docket_number(self, text):
+        return self.regex.search(text).group(2)
+
+    def sanitize_text(self, text):
+        return text.replace(r'\n', '\n').replace(u'–', '-')
