112550f82fd22da8ea66c0e562386e42a6ab9e6a
Philip Ardery <arderyp@protonmail.com>
2017-11-29 20:34:50 -0800

    Reworking mass scraper to parse new HTML page, since old RSS resource is no longer active. The solution is less than ideal, and the court HAS indicated that a new RSS feed is in the works, but there is no telling when that will be done.  This is a good workaround in the mean time.  I blew away the old example files, since they are no longer relveant to the new scraper architecture.  If we do ever move back to using an RSS feed, and if that new feed forllows the same format as the old feed, we may want to reinstate those old example files. Fixes #196

--- juriscraper/opinions/united_states/state/mass.py
+++ juriscraper/opinions/united_states/state/mass.py
@@ -3,17 +3,22 @@ Scraper for Massachusetts Supreme Court
 CourtID: mass
 Court Short Name: MS
 Author: Andrei Chelaru
+Court Contact: SJCReporter@sjc.state.ma.us (617) 557-1030
 Reviewer: mlr
 History:
  - 2014-07-12: Created.
  - 2014-08-05, mlr: Updated regex.
  - 2014-09-18, mlr: Updated regex.
  - 2016-09-19, arderyp: Updated regex.
+ - 2017-11-29, arderyp: Moved from RSS source to HTML
+    parsing due to website redesign
 """
 
 import re
+import datetime
 
 from juriscraper.OpinionSite import OpinionSite
+from juriscraper.lib.string_utils import clean_string
 from juriscraper.lib.string_utils import convert_date_string
 
 
@@ -24,50 +29,65 @@ class Site(OpinionSite):
     """
     def __init__(self, *args, **kwargs):
         super(Site, self).__init__(*args, **kwargs)
-        self.url = 'http://www.mass.gov/courts/court-info/sjc/about/reporter-of-decisions/opinions.xml'
+        self.url = 'https://www.mass.gov/service-details/new-opinions'
         self.court_id = self.__module__
         self.court_identifier = 'SJC'
         # If you discover new examples that failed the
         # regex below, please add them to the test_mass
         # method in test_everything.py
-        self.regex = "(.*)\s+\((%s[-\s]+\d+(?:,?;?\s(%s\s)?\d+)*)\)" % (self.court_identifier, self.court_identifier)
-        self.date_group = 4
+        self.regex = '(.*)\s+\((.*)\)\s+\((.*)\)'
         self.set_local_variables()
 
     def set_local_variables(self):
+        """The base xpath below requires '<current year>)' in
+        the link text via the include_date definition. This
+        means we might miss some opinions, when the court
+        publishes line items without this pattern, but the
+        only alternative it to email Mass every time there
+        is an issue. Line items missing this pattern is not
+        normal, but doesn't look altogether uncommon.
+        Consequently, this approach is less than ideal, but
+        it works for the time being.
+        """
+        year = datetime.datetime.now().year
+        exclude_list = "not(contains(., 'List of Un'))"
+        include_date = "contains(., '%d)')" % year
+        include_court = "contains(., '%s ') or contains(., '%s-')" % (
+            self.court_identifier,
+            self.court_identifier,
+        )
+        self.base_path = "//a/text()[%s][%s][%s]" % (
+            exclude_list,
+            include_date,
+            include_court
+        )
         self.grouping_regex = re.compile(self.regex)
-        pattern_base_path = "//title[not(contains(., 'List of Un')) and contains(., '%s ') or contains(., '%s-')]"
-        pattern_court_path = "%s//text()[contains(., '(%s ') or contains(., '(%s-')]"
-        self.base_path = pattern_base_path % (self.court_identifier, self.court_identifier)
-        self.court_path = pattern_court_path % (self.base_path, self.court_identifier, self.court_identifier)
-        self.date_path = "%s/../../published/text()" % self.court_path
 
     def _get_case_names(self):
         names = []
-        for s in self.html.xpath(self.court_path):
+        for s in self.html.xpath(self.base_path):
+            s = clean_string(s)
             name_raw = self.grouping_regex.search(s).group(1)
-            names.append(name_raw.replace(';', ' / '))
+            name = name_raw.replace(';', ' / ')
+            names.append(name)
         return names
 
     def _get_download_urls(self):
-        path = self.base_path + "/following-sibling::link/@href"
+        path = '%s/parent::a/@href' % self.base_path
         return list(self.html.xpath(path))
 
     def _get_case_dates(self):
-        """We were previously extracting dates from the link title text
-        but massappct examples started popping up that did not include
-        a date in the title text. We are not sure why the court is even
-        including the date in the title text when their XML also includes
-        a <published> tag. For now we are relying on the date in the
-        <published> tag, as parsing this is easier than using complex
-        regular expressions and relying on the court to repond if/when
-        dates are missing in the titles.
-        """
-        return [convert_date_string(s.strip()) for s in self.html.xpath(self.date_path)]
+        dates = []
+        for s in self.html.xpath(self.base_path):
+            s = clean_string(s)
+            date_string = self.grouping_regex.search(s).group(3)
+            dates.append(convert_date_string(date_string))
+        return dates
 
     def _get_docket_numbers(self):
         dockets = []
-        for s in self.html.xpath(self.court_path):
+        for s in self.html.xpath(self.base_path):
+            s = clean_string(s)
             docket_raw = self.grouping_regex.search(s).group(2)
             docket = docket_raw.replace(';', ',').replace(self.court_identifier + ',', self.court_identifier)
             dockets.append(docket)
