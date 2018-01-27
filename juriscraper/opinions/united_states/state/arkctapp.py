5c26bb4b3235812a4ab47a677b092630b041754b
Philip Ardery <arderyp@protonmail.com>
2018-01-27 15:40:58 -0800

    Fixing ark and arkctapp to work with new site. Replacing example files.

--- juriscraper/opinions/united_states/state/arkctapp.py
+++ juriscraper/opinions/united_states/state/arkctapp.py
@@ -7,5 +7,6 @@ from juriscraper.opinions.united_states.state import ark
 class Site(ark.Site):
     def __init__(self, *args, **kwargs):
         super(Site, self).__init__(*args, **kwargs)
-        self.url = 'http://opinions.aoc.arkansas.gov/weblink8/Browse.aspx?startid=39308'
         self.court_id = self.__module__
+        self.url_id = 'courtofappeals'
+        self.url = self.get_url()
