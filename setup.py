935100a8150027ab27b0626c78c0897d25fcf409
Michael Lissner <mlissner@michaeljaylissner.com>
2018-04-16 15:34:21 -0700

    fix(pip junk): Makes it support pip >= 10
    Pip's API changed. My kingdom for a better way to package and install
    stuff in Python.

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
 import codecs
 import os
 
-from pip.req import parse_requirements
+try:  # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError:  # for pip <= 9.0.3
+    from pip.req import parse_requirements
+
 from setuptools import setup, find_packages
 
-VERSION = "1.15.11"
+VERSION = "1.15.11.1"
 AUTHOR = "Mike Lissner"
 EMAIL = "info@free.law"
 HERE = os.path.abspath(os.path.dirname(__file__))
