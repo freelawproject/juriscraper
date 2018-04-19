5bbcf954aa4910015dd08650131389a3b98dac1e
Philip Ardery <arderyp@protonmail.com>
2018-04-18 18:51:17 -0700

    Replacing vt_p and vt_u with new set of vt scrapers.  Their site changed a while ago, and we've been missing content ever since.  Bumping versoin minor.  After deploying this 1.15.13 release, @mike, please be sure to run back scrapers for vt, vt_civil, vt_criminal, vt_environmental, vt_family, and vt_probate (all vt scrapers).

---
+++ juriscraper/opinions/united_states/state/vt_environmental.py
@@ -0,0 +1,15 @@
+"""Scraper for the Vermont Environmental
+CourtID: vt
+Court Short Name: VT
+Court Contact: submit form here https://www.vermontjudiciary.org/website-feedback-form
+"""
+
+import vt
+
+
+class Site(vt.Site):
+    def get_backscrape_max(self):
+        return 72
+
+    def get_division_id(self):
+        return '3'
