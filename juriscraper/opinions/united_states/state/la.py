7601ae565144ecfa56efb48cdddb70802c10f7ef
Philip Ardery <arderyp@protonmail.com>
2018-06-28 20:21:18 -0700

    Fixing la.  Scraper was failing due to inconsistencies in xpath strings used throughout scraper. I also fixed the testing logic so that it no longer hits the network.  I also added new test cases for coverage.  Overall, this scraper is really gross and unnecessarily complicated.  It should probably be rewritten from scratch.  I did my best to clean it up and remove the most offensive repetition, but don't have time to do a full rewrite right now.

--- juriscraper/opinions/united_states/state/la.py
+++ juriscraper/opinions/united_states/state/la.py
@@ -19,9 +19,18 @@ class Site(OpinionSite):
         super(Site, self).__init__(*args, **kwargs)
         self.court_id = self.__module__
         self.url = 'http://www.lasc.org/news_releases/{year}/default.asp'.format(year=date.today().year)
+        self.path_case_paragraph = (
+            "//p[contains(., 'v.') or "
+            "contains(., 'IN RE') or "
+            "contains(., 'IN THE') or "
+            "contains(., 'vs.')]"
+        )
+        self.path_case_link = '%s//a' % self.path_case_paragraph
 
     def _download(self, request_dict={}):
         html_l = OpinionSite._download(self)
+        if self.method == 'LOCAL':
+            return [html_l]
         html_trees = []
         path = "//td[contains(./text(),'Opinion') or contains(./text(), 'PER CURIAM')]/preceding-sibling::td[1]//@href"
         for url in html_l.xpath(path)[:2]:
@@ -62,8 +71,7 @@ class Site(OpinionSite):
             judges.extend(self._add_judges(html_tree))
         return judges
 
-    @staticmethod
-    def _add_judges(html_tree):
+    def _add_judges(self, html_tree):
         judges = []
         for e in html_tree.xpath("//strong[contains(., 'J.') or contains(., 'CURIAM')]"):
             text = html.tostring(e, method='text', encoding='unicode')
@@ -76,20 +84,19 @@ class Site(OpinionSite):
                     preceding_judge = 'CURIAM'
                 else:
                     preceding_judge = judge
-                judges.extend([judge] * len(list(html_tree.xpath(
-                    "//p[contains(., 'v.') or contains(., 'IN RE') or contains(., 'IN THE') or contains(., 'vs.')]"
-                    "[preceding::strong[1][contains(., '{pre_jud}') or text()=':']]//a".format(
-                        pre_jud=preceding_judge)))))
-        nr_of_links = len(html_tree.xpath("//a[contains(., 'v.') or contains(., 'IN RE') or "
-                                          "contains(., 'IN THE') or contains(., 'vs.')]"))
+                judge_path_format = "%s[preceding::strong[1][contains(., '%s') or text()=':']]//a"
+                judge_path = judge_path_format % (self.path_case_paragraph, preceding_judge)
+                judge_count = len(html_tree.xpath(judge_path))
+                judges.extend([judge] * judge_count)
+        nr_of_links = self._get_link_count_from_html(html_tree)
+
         nr_of_judges = len(judges)
         if nr_of_judges < nr_of_links:
             for i in range(nr_of_links - nr_of_judges):
                 judges.insert(0, '')
         return judges
 
-    @staticmethod
-    def _add_dates(html_tree):
+    def _add_dates(self, html_tree):
         dates = []
         text = ' '.join(list(html_tree.xpath("//text()[contains(normalize-space(), 'day of')]")))
         text = ' '.join(text.split())
@@ -97,22 +104,16 @@ class Site(OpinionSite):
             match = re.search(r'\D*((\d{1,2})\w{2} day of (\w+),\s*(\d{4})).*', text)
             date_text = '{day}{month}{year}'.format(day=match.group(2), month=match.group(3), year=match.group(4))
             case_date = date.fromtimestamp(time.mktime(time.strptime(date_text, '%d%B%Y')))
-            dates.extend([case_date] * len(html_tree.xpath("//p[contains(., 'v.') or "
-                                                           "contains(., 'IN RE') or "
-                                                           "contains(., 'IN THE') or "
-                                                           "contains(., 'vs.')]//a")))
+            dates.extend([case_date] * self._get_link_count_from_html(html_tree))
         return dates
 
-    @staticmethod
-    def _add_download_urls(html_tree):
-        return list(html_tree.xpath("//p[contains(., 'v.') or contains(., 'IN RE') or "
-                                    "contains(., 'IN THE') or contains(., 'vs.')]//a/@href"))
+    def _add_download_urls(self, html_tree):
+        path = "%s/@href" % self.path_case_link
+        return html_tree.xpath(path)
 
-    @staticmethod
-    def _add_case_names(html_tree):
+    def _add_case_names(self, html_tree):
         case_names = []
-        for element in html_tree.xpath("//p[contains(., 'v.') or contains(., 'IN RE') or "
-                                       "contains(., 'IN THE') or contains(., 'vs.')]//a"):
+        for element in self._get_links_from_html(html_tree):
             text = ' '.join(list(element.xpath(".//text()")))
             text = ' '.join(text.split())
             if text:
@@ -130,19 +131,25 @@ class Site(OpinionSite):
 
         return case_names
 
-    @staticmethod
-    def _add_docket_numbers(html_tree):
+    def _add_docket_numbers(self, html_tree):
+        regex = r'(\d+ ?-.*- ?\d+)\s*(.*)'
         docket_numbers = []
-        for element in html_tree.xpath("//p[contains(., 'v.') or contains(., 'IN RE') or contains(., 'IN THE') or contains(., 'vs.')]//a"):
+        for element in self._get_links_from_html(html_tree):
             text = ' '.join(list(element.xpath(".//text()")))
             text = ' '.join(text.split())
             if text:
                 try:
-                    docket_numbers.append(re.search(r'(\d+ ?-.*- ?\d+)\s*(.*)', text).group(1))
+                    docket_numbers.append(re.search(regex, text).group(1))
                 except AttributeError:
                     name_element = element.xpath("./parent::p[1]//text()")
                     text2 = ' '.join(list(name_element))
                     text2 = ' '.join(text2.split())
                     if text2:
-                        docket_numbers.append(re.search(r'(\d+ ?-.*- ?\d+)\s*(.*)', text2).group(1))
+                        docket_numbers.append(re.search(regex, text2).group(1))
         return docket_numbers
+
+    def _get_link_count_from_html(self, html_tree):
+        return len(self._get_links_from_html(html_tree))
+
+    def _get_links_from_html(self, html_tree):
+        return html_tree.xpath(self.path_case_link)
