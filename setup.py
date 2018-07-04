370d7a64e02f3c1b163b20b98f483546a35b5682
Philip Ardery <arderyp@protonmail.com>
2018-07-03 20:14:46 -0700

    Fixing nh. Adding new example file set for coverage. Bumping version minor.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except ImportError:  # for pip <= 9.0.3
 
 from setuptools import setup, find_packages
 
-VERSION = "1.18.10"
+VERSION = "1.18.11"
 AUTHOR = "Mike Lissner"
 EMAIL = "info@free.law"
 HERE = os.path.abspath(os.path.dirname(__file__))
