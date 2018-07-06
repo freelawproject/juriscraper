d361911d682b5d6d2ffd8a9122544e4389aded23
Michael Lissner <mlissner@michaeljaylissner.com>
2018-07-06 16:36:33 -0700

    Version bump

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except ImportError:  # for pip <= 9.0.3
 
 from setuptools import setup, find_packages
 
-VERSION = "1.19.4"
+VERSION = "1.19.5"
 AUTHOR = "Mike Lissner"
 EMAIL = "info@free.law"
 HERE = os.path.abspath(os.path.dirname(__file__))
