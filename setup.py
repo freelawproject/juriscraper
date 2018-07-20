9d363a7bba0f4762d5c02c29476093bf298cee99
Michael Lissner <mlissner@michaeljaylissner.com>
2018-07-20 15:59:43 -0700

    Version bump
    Needed to fix issue with Pypi. Code is identical to 1.20.1.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except ImportError:  # for pip <= 9.0.3
 
 from setuptools import setup, find_packages
 
-VERSION = "1.20.1"
+VERSION = "1.20.2"
 AUTHOR = "Mike Lissner"
 EMAIL = "info@free.law"
 HERE = os.path.abspath(os.path.dirname(__file__))
