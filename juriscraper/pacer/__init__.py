4edd9e7688d5c7c24a6bae76e276d7d4a1c4fbf2
John Hawkinson <jhawk@mit.edu>
2018-07-30 14:42:18 -0400

    pacer/__init__.py: add CaseQuery

--- juriscraper/pacer/__init__.py
+++ juriscraper/pacer/__init__.py
@@ -1,5 +1,6 @@
 from .appellate_docket import AppellateDocketReport
 from .attachment_page import AttachmentPage
+from .case_query import CaseQuery
 from .docket_history_report import DocketHistoryReport
 from .docket_report import DocketReport
 from .free_documents import FreeOpinionReport
@@ -11,6 +12,7 @@ from .rss_feeds import PacerRssFeed
 __all__ = [
     AppellateDocketReport,
     AttachmentPage,
+    CaseQuery,
     DocketHistoryReport,
     DocketReport,
     FreeOpinionReport,
