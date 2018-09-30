9ad816483590f86211733847e0e4f76d03b339bf
Philip Ardery <arderyp@protonmail.com>
2018-09-29 19:47:11 -0700

    Fixing nh.  Its getting a bit complex, given their various quarky ways of writing bad/non-standard HTML. Added new test files for coverage. Bumping version minor.

--- juriscraper/opinions/united_states/state/nh.py
+++ juriscraper/opinions/united_states/state/nh.py
@@ -18,6 +18,7 @@ import re
 from datetime import date
 
 from juriscraper.OpinionSite import OpinionSite
+from juriscraper.lib.exceptions import InsanityException
 from juriscraper.lib.string_utils import convert_date_string
 
 
@@ -29,20 +30,11 @@ class Site(OpinionSite):
         self.court_id = self.__module__
         self.link_path = 'id("content")/div//ul//li//a[position()=1]'
         self.link_text_regex = re.compile('(\d{4}-\d+(?!.*\d{4}-\d+))(?:,|\.|\s?) (.*)')
+        self.docket_name_pairs = False
 
     def _get_case_names(self):
-        case_names = []
-        for link in self.html.xpath(self.link_path):
-            # Text of some links includes info for multiple cases split by <br>
-            # so we'll iterate over each, extract the name, clean it up, then
-            # glue the names together to form one long name. (Example: Feb2016)
-            link_names = []
-            for text in link.xpath('text()'):
-                name_raw = self.link_text_regex.search(text).group(2)
-                if name_raw:
-                    link_names.append(' '.join(name_raw.split()))
-            case_names.append(' and '.join(link_names))
-        return case_names
+        self.docket_name_pairs = self._get_anchor_docket_name_pairs()
+        return [pair['name'] for pair in self.docket_name_pairs]
 
     def _get_download_urls(self):
         path = "id('content')/div//ul//li//a[position()=1]"
@@ -71,15 +63,55 @@ class Site(OpinionSite):
         return ['Published'] * len(self.case_names)
 
     def _get_docket_numbers(self):
-        docket_numbers = []
-        for link in self.html.xpath(self.link_path):
-            # Text of some links includes info for multiple cases split by <br> so we'll
-            # iterate over each, extract the docket number, then glue the numbers together
-            # to create a single "xx,yy" docket number. (Example: Feb2016)
-            case_dockets = []
-            for text in link.xpath('text()'):
-                docket = self.link_text_regex.search(text).group(1)
-                if docket:
-                    case_dockets.append(docket)
-            docket_numbers.append(', '.join(case_dockets))
-        return docket_numbers
+        return [pair['docket'] for pair in self.docket_name_pairs]
+
+    def _get_anchor_docket_name_pairs(self):
+        """The court has some ugly HTML practices that we need to handle.
+
+        Most anchor links include single line strings with a single docket
+        number and a single case name.  However, there are two other formats
+        we've seen and must work around.
+
+        (CASE 1)
+        The anchor has multiple lines broken with <br> tag(s), and each
+        line contains "<docket> <name>". In this case we need to combine
+        the docket numbers and name strings respectively.
+        [EXAMPLE: February 18, 2016 in nh_example_2.html]
+
+        (CASE 2)
+        The anchor has multiple lines broken with <br> tag(s), and the
+        second line is a continuation of a long case name started on the first
+        line.  So, the second line does not lead with a docket number, thus
+        this line's string should be glued onto the <name> substring extracted
+        from the previous line.
+        [EXAMPLE: September 18, 2018 in nh_example_6.html]
+        """
+        pairs = []
+        for anchor in self.html.xpath(self.link_path):
+            i = 0
+            dockets = []
+            name_substrings = []
+            for text in anchor.xpath('text()'):
+                text = text.strip()
+                match = self.link_text_regex.search(text)
+                try:
+                    docket = match.group(1)
+                    dockets.append(docket)
+                    name = match.group(2)
+                    name = ' '.join(name.split())
+                    name_substrings.append(name)
+                    i += 1
+                except AttributeError:
+                    if i == 0:
+                        # docket and name (root) should be contained in first substring
+                        error = 'Unexpected anchor root string format: %s' % text
+                        raise InsanityException(error)
+                    # no docket in the substring, its a trailing name substring
+                    # that they broke over multiple lines, so glue it to the
+                    # previous name substring
+                    name_substrings[i-1] += ' %s' % text
+            pairs.append({
+                'docket': ', '.join(dockets),
+                'name': ' and '.join(name_substrings),
+            })
+        return pairs
