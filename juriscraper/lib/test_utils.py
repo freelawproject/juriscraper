9a1abe849e6ee8caee6436e2698d9ace79ea4aff
Joseph Rawson <joseph.rawson.works@gmail.com>
2019-01-02 21:08:04 -0600

    rework slowness warnings

--- juriscraper/lib/test_utils.py
+++ juriscraper/lib/test_utils.py
@@ -1,14 +1,25 @@
 # -*- coding: utf-8 -*-
 import os
 import sys
+import warnings
 
 from requests.exceptions import ConnectionError
 from requests.models import Request, Response
 
 from .exceptions import SlownessException
 
-IS_TRAVIS = 'TRAVIS' in os.environ
-SKIP_SLOWNESS = "JURISCRAPER_SKIP_SLOWNESS_ERRORS" in os.environ or IS_TRAVIS
+WARN_SLOW_SRAPERS = 'TRAVIS' in os.environ
+# FIXME: when testing, should we only find the first warning
+# or provide a warning for each slow scraper?
+warnings.simplefilter("always")
+
+
+class SlownessWarning(UserWarning):
+    pass
+
+
+class TooSlowWarning(SlownessWarning):
+    pass
 
 
 class MockRequest(Request):
@@ -49,17 +60,22 @@ class MockRequest(Request):
 def warn_or_crash_slow_parser(duration, warn_duration=1, max_duration=15):
     msg = ''
     if duration > max_duration:
-        if sys.gettrace() is None and not SKIP_SLOWNESS:
+        if sys.gettrace() is None and not WARN_SLOW_SRAPERS:
             # Only do this if we're not debugging. Debuggers make things slower
             # and breakpoints make things stop.
             raise SlownessException(
-                "This scraper took {duration}s to test, which is more than the "
-                "allowed speed of {max_duration}s. Please speed it up for "
+                "This scraper took {duration}s to test, which is more than "
+                "the allowed speed of {max_duration}s. Please speed it up for "
                 "tests to pass.".format(duration=duration,
                                         max_duration=max_duration)
             )
+        elif WARN_SLOW_SRAPERS:
+            msg = ' - WARNING: SLOW SCRAPER'
+            wmsg = "WARNING: VERY SLOW SCRAPER (potential Error)"
+            warnings.warn(wmsg, TooSlowWarning)
     elif duration > warn_duration:
         msg = ' - WARNING: SLOW SCRAPER'
+        warnings.warn('WARNING: SLOW SCRAPER', SlownessWarning)
     else:
         msg = ''
     return msg
