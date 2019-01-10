ff637c29393de66de635d6ef1bec277ed189d8ee
Philip Ardery <arderyp@protonmail.com>
2019-01-09 19:55:33 -0800

    Fixing fla. The court updated its site, and its new site is refreshingly easy to parse.  Updated primary example and compare files to use new site's source.  Removes subsiquent example/compare files since they are no longer relevant.

--- juriscraper/opinions/united_states/state/fla.py
+++ juriscraper/opinions/united_states/state/fla.py
@@ -1,15 +1,10 @@
 # Scraper for Florida Supreme Court
 # CourtID: fla
 # Court Short Name: fla
-# Author: Andrei Chelaru
-# Reviewer: mlr
-# Date created: 21 July 2014
 
-import re
 from datetime import datetime
 
 from juriscraper.OpinionSite import OpinionSite
-from juriscraper.lib.exceptions import InsanityException
 from juriscraper.lib.string_utils import convert_date_string
 
 
@@ -17,10 +12,14 @@ class Site(OpinionSite):
     def __init__(self, *args, **kwargs):
         super(Site, self).__init__(*args, **kwargs)
         self.court_id = self.__module__
-        self.url = 'http://www.floridasupremecourt.org/decisions/opinions.shtml'
-        self.regex = re.compile("(S?C\d+-\d+)(.*)")
-        self.regex_date = re.compile(r'(?:.*\s)?(\w+\s\d+,\s\d{4})(?:.*)?')
-        self.path_anchor_qualifier = 'not(contains(., "Notice"))][not(contains(., "Rehearing Order"))'
+        today = datetime.today().strftime('%m/%d/%Y')
+        this_year = datetime.today().year
+        last_year = this_year - 1
+        today_last_year = today.replace(str(this_year), str(last_year))
+        # Get 50 most recent opinions from past year--even though you can
+        # put whatever number you want as limit, 50 seems to be max
+        self.url = 'https://www.floridasupremecourt.org/search/?searchtype=opinions&limit=50&startdate=%s&enddate=%s' % (today_last_year, today)
+        self.path_cell = '//div[@class="search-results"]//tr/td[%d]'
         self.cases = []
 
     def _download(self, request_dict={}):
@@ -28,55 +27,20 @@ class Site(OpinionSite):
         self._extract_cases_from_html(html)
         return html
 
-    def extract_year_from_h1(self, html):
-        """For testability with example files from previous years,
-        we can't use the current year in the base_path search, and
-        instead need to extract the year from the pages <h1> tag.
-        This is also handy early in the calendar year if/when the
-        court is publishing new opinions for the end of the previous
-        year
-        """
-        year_string = html.xpath('//h1')[0].text_content().split()[3]
-        # Basic validation of year
-        if len(year_string) != 4 or not year_string.startswith('20'):
-            raise InsanityException('Extracted year "%s") appears to be invalid' % year_string)
-        # If running live scrape, year should always be this year or last year
-        if self.method != 'LOCAL':
-            this_year = datetime.today().year
-            if int(year_string) not in [this_year, this_year - 1]:
-                raise InsanityException('Year ("%s") too far in the future or past' % year_string)
-        return year_string
-
     def _extract_cases_from_html(self, html):
-        year = self.extract_year_from_h1(html)
-        path_dates = "//h2[contains(., '%s')]" % year
-        for h2 in html.xpath(path_dates):
-            text_date = h2.text_content().strip()
-            if not text_date or 'No opinions released' in text_date:
+        path = '//div[@class="search-results"]//tbody/tr'
+        for row in html.xpath(path):
+            cells = row.xpath('./td')
+            url = cells[4].xpath('./a/@href')
+            if not url:
+                # Skip rows without PDFs
                 continue
-            date_string = self.regex_date.search(text_date).group(1)
-            date = convert_date_string(date_string)
-            next_tag = h2.getnext().tag
-            if not self.cases and next_tag == 'p':
-                # Sometimes court puts most recent date's opinions in
-                # its own box at the top of the page, in a format that
-                # doesn't conform with the other date sections below
-                path = './/a[%s]' % self.path_anchor_qualifier
-                anchors = h2.getparent().xpath(path)
-            else:
-                path = './following::ul[1]//a[%s]' % self.path_anchor_qualifier
-                anchors = h2.xpath(path)
-            for anchor in anchors:
-                text_anchor = anchor.text_content()
-                match = self.regex.search(text_anchor)
-                if not match:
-                    continue
-                self.cases.append({
-                    'date': date,
-                    'docket': match.group(1),
-                    'name': match.group(2).strip(),
-                    'url': anchor.xpath('./@href')[0],
-                })
+            self.cases.append({
+                'url': url[0],
+                'name': cells[2].text_content(),
+                'docket': cells[1].text_content(),
+                'date': convert_date_string(cells[0].text_content()),
+            })
 
     def _get_case_names(self):
         return [case['name'] for case in self.cases]
@@ -91,4 +55,4 @@ class Site(OpinionSite):
         return [case['docket'] for case in self.cases]
 
     def _get_precedential_statuses(self):
-        return ['Published'] * len(self.case_names)
+        return ['Published'] * len(self.cases)
