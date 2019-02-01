343b4e819003d8d7805fb7c463857089a5ad451f
Philip Ardery <arderyp@protonmail.com>
2019-01-31 22:03:20 -0800

    fix(fladistctapp_3): handle empty rows, fix bug in testing logic, regenerated compare files, added new example file for empty row use case, bump version minor

--- juriscraper/opinions/united_states/state/fladistctapp_3.py
+++ juriscraper/opinions/united_states/state/fladistctapp_3.py
@@ -10,7 +10,6 @@ History:
  - 2015-07-28: Updated by m4h7
 """
 
-from lxml import html
 from datetime import date
 
 from juriscraper.lib.string_utils import titlecase, convert_date_string
@@ -21,35 +20,35 @@ class Site(OpinionSite):
     def __init__(self, *args, **kwargs):
         super(Site, self).__init__(*args, **kwargs)
         self.court_id = self.__module__
-        self.year = date.today().year
         self.url = 'http://www.3dca.flcourts.org/Opinions/ArchivedOpinions.shtml'
         self.base_path = "//h3/following::text()[.='OPINIONS']/following::table[1]//tr"
 
     def _download(self, request_dict={}):
         html_l = super(Site, self)._download(request_dict)
         if self.method == 'LOCAL':
-            return html_l
+            return [html_l]
         html_trees = []
         # this path reads the row for the last month in that year
-        path = "//th[contains(., '{year}')]/following::tr[1]/td[position()>1]/a[contains(., '/')]/@href".format(
-            year=self.year
-        )
-        # to get all the dates in that page the following path can be used:
-        # path = "//th/following::tr/td[position()>1]/a[contains(., '/')]/@href"
+        path_format = "//th[contains(., '{year}')]/following::tr[1]/td[position()>1]/a[contains(., '/')]/@href"
+        path = path_format.format(year=date.today().year)
         for url in html_l.xpath(path):
             html_tree = self._get_html_tree_by_url(url, request_dict)
             html_trees.append(html_tree)
         return html_trees
 
     def _get_case_names(self):
-        case_names = []
+        case_names = []            # return html_l
         for html_tree in self.html:
             case_names.extend(self._return_case_names(html_tree))
         return case_names
 
     def _return_case_names(self, html_tree):
-        path = "{base}/td[2]//text()[1]".format(base=self.base_path)
-        return [titlecase(s.lower()) for s in html_tree.xpath(path)]
+        path = "%s/td[2]" % self.base_path
+        cells = html_tree.xpath(path)
+        names = [c.text_content().strip() for c in cells]
+
+        # Return formatted text for non-empty cells
+        return [titlecase(n.lower()) for n in names if n]
 
     def _get_download_urls(self):
         download_urls = []
@@ -58,8 +57,8 @@ class Site(OpinionSite):
         return download_urls
 
     def _return_download_urls(self, html_tree):
-        path = "{base}/td[1]//a/@href".format(base=self.base_path)
-        return list(html_tree.xpath(path))
+        path = "%s/td[1]//a/@href" % self.base_path
+        return html_tree.xpath(path)
 
     def _get_case_dates(self):
         case_dates = []
@@ -68,13 +67,17 @@ class Site(OpinionSite):
         return case_dates
 
     def _return_dates(self, html_tree):
-        path = "//h3/text()"
-        dates = []
-        text = html_tree.xpath(path)[0]
+        path_date_string = "//h3/text()"
+        text = html_tree.xpath(path_date_string)[0]
         date_string = text.split()[0]
         case_date = convert_date_string(date_string)
-        dates.extend([case_date] * int(html_tree.xpath("count({base})".format(base=self.base_path))))
-        return dates
+
+        # Count rows with non-empty first cells
+        path_first_cell = "%s/td[1]" % self.base_path
+        first_cells = html_tree.xpath(path_first_cell)
+        opinion_count = sum(1 for r in first_cells if r.text_content().strip())
+
+        return [case_date] * opinion_count
 
     def _get_precedential_statuses(self):
         return ['Published'] * len(self.case_dates)
@@ -86,8 +89,5 @@ class Site(OpinionSite):
         return docket_numbers
 
     def _return_docket_numbers(self, html_tree):
-        docket_numbers = []
-        for e in html_tree.xpath("{base}/td[1]//a".format(base=self.base_path)):
-            s = html.tostring(e, method='text', encoding='unicode')
-            docket_numbers.append(s)
-        return docket_numbers
+        path = "%s/td[1]//a" % self.base_path
+        return [a.text_content().strip() for a in html_tree.xpath(path)]
