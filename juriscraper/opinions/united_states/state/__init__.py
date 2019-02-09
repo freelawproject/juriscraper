4e394ef24f829d44eb21d35609bd0409379c7477
Philip Ardery <arderyp@protonmail.com>
2019-02-09 12:52:50 -0800

    Commenting colo and coloctapp out of __init__.py because the test are slow (hit network) and are causing intermittent test suit failure.  The scrapers have also been broken in prod for months, so there's no point in testing these now.

--- juriscraper/opinions/united_states/state/__init__.py
+++ juriscraper/opinions/united_states/state/__init__.py
@@ -19,8 +19,8 @@ __all__ = [
     'calctapp_6th',
     'calctapp_app_div',
     'calctapp_u',
-    'colo',
-    'coloctapp',
+    #'colo',        # See scraper, works in dev but not production; its been months and no solution; slow tests fail
+    #'coloctapp',   # See scraper, works in dev but not production; its been months and no solution; slow tests fail
     'conn',
     'connappct',
     'dc',
