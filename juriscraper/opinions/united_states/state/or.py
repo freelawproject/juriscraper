a549fc2f9a1bf62be47911b7c51578f341f04922
Philip Ardery <arderyp@protonmail.com>
2019-02-25 20:22:11 -0800

    fix(or): site changed format; updated example file to reflect new format

--- juriscraper/opinions/united_states/state/or.py
+++ juriscraper/opinions/united_states/state/or.py
@@ -4,46 +4,50 @@ History:
 """
 
 from juriscraper.OpinionSite import OpinionSite
-import re
-import time
-from datetime import date
+from juriscraper.lib.string_utils import convert_date_string
 
 
 class Site(OpinionSite):
     def __init__(self, *args, **kwargs):
         super(Site, self).__init__(*args, **kwargs)
-        self.today = date.today()
-        self.url = 'http://www.publications.ojd.state.or.us/Pages/OpinionsSC{year}.aspx'.format(year=self.today.year)
-        # self.url = 'http://www.publications.ojd.state.or.us/Pages/OpinionsSC2014.aspx'
         self.court_id = self.__module__
+        self.url = 'https://www.courts.oregon.gov/publications/sc/Pages/default.aspx'
+        self.cases = []
+
+    def _download(self, request_dict={}):
+        html = super(Site, self)._download(request_dict=request_dict)
+        self.extract_cases(html)
+
+    def extract_cases(self, html):
+        for header in html.xpath('//h4//a/parent::h4'):
+            date_string = header.text_content().strip()
+            if not date_string:
+                continue
+            date = convert_date_string(date_string)
+            ul = header.xpath('./following-sibling::ul')[0]
+            for item in ul.xpath('.//li'):
+                text = item.text_content().strip()
+                url = item.xpath('.//a[1]/@href')[0]
+                docket = item.xpath('.//a[2]')[0].text_content().strip()
+                name = text.split(')', 1)[-1]
+                self.cases.append({
+                    'date': date,
+                    'name': name,
+                    'docket': docket,
+                    'url': url,
+                })
 
     def _get_case_names(self):
-        case_names = []
-        for name in self.html.xpath("/html/body//h2[contains(.,'SUPREME')]/following-sibling::ul[1]/li/a//text()"):
-            case_names.append(re.sub(r'\(?S\d+[\s)]\s?', '', name))
-        return case_names
+        return [case['name'] for case in self.cases]
 
     def _get_docket_numbers(self):
-        docket_numbers = []
-        for s in self.html.xpath("/html/body//h2[contains(.,'SUPREME')]/following-sibling::ul[1]/li/a//text()"):
-            docket_numbers.append(' & '.join(re.findall('S\d+', s)))
-        return docket_numbers
+        return [case['docket'] for case in self.cases]
 
     def _get_download_urls(self):
-        return [t for t in self.html.xpath("/html/body//h2[contains(.,'SUPREME')]/following-sibling::ul[1]/li/a/@href")]
+        return [case['url'] for case in self.cases]
 
     def _get_case_dates(self):
-        dates = []
-        for opinion in self.html.xpath('/html/body//h2[contains(.,"SUPREME")]'
-                                       '/following-sibling::ul[1]/li[a/@href]'):
-            for d in opinion.xpath('parent::ul/preceding-sibling::*[1]//'
-                                   'text()[normalize-space(.) != ""]'):
-                try:
-                    t = time.strptime(d, '%m/%d/%y')
-                except ValueError:
-                    t = time.strptime(d, '%m/%d/%Y')
-                dates.append(date.fromtimestamp(time.mktime(t)))
-        return dates
+        return [case['date'] for case in self.cases]
 
     def _get_precedential_statuses(self):
-        return ["Published"] * len(self.case_names)
+        return ["Published"] * len(self.cases)
