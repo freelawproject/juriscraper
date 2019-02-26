2dee7d45045c3a490f9e740aebd64a139ac58695
Philip Ardery <arderyp@protonmail.com>
2019-02-25 18:04:54 -0800

    fix(mont) They moved their opinnion/order listing to a different url with a different format

--- juriscraper/opinions/united_states/state/mont.py
+++ juriscraper/opinions/united_states/state/mont.py
@@ -1,94 +1,50 @@
 # Author: Michael Lissner
 # Date created: 2013-06-03
 
-import re
-import time
-from datetime import date
-from datetime import timedelta
-
 from juriscraper.OpinionSite import OpinionSite
-from juriscraper.lib.string_utils import titlecase
+from juriscraper.lib.string_utils import convert_date_string
+from juriscraper.lib.html_utils import get_table_column_text, get_table_column_links
 
 
 class Site(OpinionSite):
     def __init__(self, *args, **kwargs):
         super(Site, self).__init__(*args, **kwargs)
         self.court_id = self.__module__
-        today = date.today()
-        thirty_days_ago = date.today() - timedelta(days=30)
-        # Oddly, all the empty get params are needed. This site uses the Spring Framework, as gleaned in a stacktrace.
-        self.url = ('http://searchcourts.mt.gov/search/advanced?searchtype=advanced&txtContainsOne=&cboAndOrOne='
-                    '&txtContainsTwo=&cboAndOrTwo=&txtContainsThree=&cboDocumentType=Opinion%%2FOrder'
-                    '&txtCaseNumber=&txtCitation=&txtPartyNameOne=&txtPartyNameTwo=&cboCaseType=&cboOpinionBy='
-                    '&txtAttorneyNameOne=&txtAttorneyNameTwo=&txtTrialCourtJudge=&txtTrialCourtCaseNumber='
-                    '&cboFromYear=%s'
-                    '&cboFromMonth=%s'
-                    '&cboFromDay=%s'
-                    '&cboToYear=%s'
-                    '&cboToMonth=%s'
-                    '&cboToDay=%s' % (thirty_days_ago.year, thirty_days_ago.month, thirty_days_ago.day,
-                                      today.year, today.month, today.day))
-        self.back_scrape_iterable = range(1972, 2014)
+        self.url = 'https://courts.mt.gov/Portals/189/orders/orders/Recent_Orders.htm'
+        # Site has non-chained, bad certificate, need to
+        # ignore ssl verification for now for scraper to work
+        self.request['verify'] = False
 
     def _get_download_urls(self):
-        path = '//table[@id="anyid"]/tr[position() > 1]/td[1]/a/@href'
-        return list(self.html.xpath(path))
+        return get_table_column_links(self.html, 1)
 
     def _get_case_names(self):
-        path = '//table[@id="anyid"]/tr[position() > 1]/td[2]/text()'
-        return [titlecase(case_name) for case_name in self.html.xpath(path)]
+        return get_table_column_text(self.html, 4)
 
     def _get_case_dates(self):
-        path = '//table[@id="anyid"]/tr[position() > 1]/td[8]/text()'
-        return [date.fromtimestamp(time.mktime(time.strptime(date_string, '%Y/%m/%d')))
-                for date_string in self.html.xpath(path)]
+        return [convert_date_string(date_string) for date_string in get_table_column_text(self.html, 2)]
 
     def _get_precedential_statuses(self):
         return ["Published"] * len(self.case_names)
 
     def _get_docket_numbers(self):
-        docket_numbers = []
-        for s in self.html.xpath('//table[@id="anyid"]/tr[position() > 1]/td[3]/text()'):
-            if re.search('[A-Za-z]', s):
-                try:
-                    docket_numbers.append(s.split()[1])
-                except IndexError:
-                    docket_numbers.append(s)
-            else:
-                docket_numbers.append(s)
-        return docket_numbers
+        return get_table_column_text(self.html, 3)
+
+    def _get_summaries(self):
+        return get_table_column_text(self.html, 1)
 
     def _get_nature_of_suit(self):
-        path = '//table[@id="anyid"]/tr[position() > 1]/td[3]/text()'
         natures = []
-        for s in self.html.xpath(path):
-            if 'DA' in s:
-                nature = "Direct Appeal"
-            elif 'OP' in s:
-                nature = "Original Proceeding"
-            elif 'PR' in s:
+        for docket in self.docket_numbers:
+            if docket.startswith('DA'):
+                nature = 'Direct Appeal'
+            elif docket.startswith('OP'):
+                nature = 'Original Proceeding'
+            elif docket.startswith('PR'):
                 nature = 'Professional Regulation'
-            elif 'AF' in s:
+            elif docket.startswith('AF'):
                 nature = 'Administrative File'
             else:
                 nature = 'Unknown'
             natures.append(nature)
         return natures
-
-    def _get_neutral_citations(self):
-        path = '//table[@id="anyid"]/tr[position() > 1]/td[5]/text()'
-        return list(self.html.xpath(path))
-
-    def _download_backwards(self, year):
-        self.url = ('http://searchcourts.mt.gov/search/advanced?searchtype=advanced&txtContainsOne=&cboAndOrOne='
-                    '&txtContainsTwo=&cboAndOrTwo=&txtContainsThree=&cboDocumentType=Opinion%%2FOrder'
-                    '&txtCaseNumber=&txtCitation=&txtPartyNameOne=&txtPartyNameTwo=&cboCaseType=&cboOpinionBy='
-                    '&txtAttorneyNameOne=&txtAttorneyNameTwo=&txtTrialCourtJudge=&txtTrialCourtCaseNumber='
-                    '&cboFromYear=%s'
-                    '&cboFromMonth=%s'
-                    '&cboFromDay=%s'
-                    '&cboToYear=%s'
-                    '&cboToMonth=%s'
-                    '&cboToDay=%s' % (year, '01', '01',
-                                      year, '12', '31'))
-        self.html = self._download()
