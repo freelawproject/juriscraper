b50c06631a708c37dd35a14fc2cb3d45ee676920
Philip Ardery <arderyp@protonmail.com>
2019-02-26 18:00:11 -0800

    fix(sc): fixing bug in date parsing logic; added summary fields; updated test compare files to reflect fixed/extended data

--- juriscraper/opinions/united_states/state/sc.py
+++ juriscraper/opinions/united_states/state/sc.py
@@ -17,7 +17,7 @@ Contact information:
  - Web Developer (who can help with firewall issues): 803-734-0373, Winkie Clark
 """
 
-from datetime import date
+import datetime
 
 from juriscraper.OpinionSite import OpinionSite
 from juriscraper.lib.exceptions import InsanityException
@@ -28,46 +28,58 @@ class Site(OpinionSite):
     def __init__(self, *args, **kwargs):
         super(Site, self).__init__(*args, **kwargs)
         self.court_id = self.__module__
-        today = date.today()
+        today = datetime.date.today()
         self.url = 'http://www.sccourts.org/opinions/indexSCPub.cfm?year=%d&month=%d' % (today.year, today.month)
         self.cases = []
 
     def _process_html(self):
         path = '//a[@class="blueLink2"][contains(./@href, ".pdf") or contains(./@href, "?orderNo=")]'
         for link in self.html.xpath(path):
-            url = link.attrib['href']
-            text = link.text_content().strip()
-            parts = text.split('-', 1)
-            docket = parts[0].strip()
-            docket = '' if 'order' in docket.lower() else docket
-            name = parts[1]
-            parent = link.getparent()
-            parents_parent = parent.getparent()
-            attributes_parent = parent.attrib
-            attributes_parents_parent = parents_parent.attrib
-            container_id = 'maincontent'
-            if 'id' in attributes_parent and attributes_parent['id'] == container_id:
-                search_root = link
-            elif 'id' in attributes_parents_parent and attributes_parents_parent['id'] == container_id:
-                search_root = parent
-            else:
-                # Link should be within a <p> tag directly under <div id='maincontent'>, but
-                # occasionally the courts forgets the wrap it in a <p>, in which case it should
-                # be directly under the <div id='maincontent'>
-                raise InsanityException('Unrecognized placement of Opinion url on page: "%s"' % text)
-            date_element = search_root.xpath('./preceding-sibling::b')[0]
-            date_element_text = date_element.text_content().strip().lower()
-            if not date_element_text.endswith('opinions') and not date_element_text.endswith('orders'):
-                # text should be like "5-14-2014 - Opinions" or "5-21-2014 - Orders"
-                raise InsanityException('Unrecognized bold (date) element: "%s"' % date_element_text)
-            date_string = date_element_text.split()[0]
-
+            docket, name, url = self.extract_docket_name_url_from_link(link)
+            date, summary = self.extract_date_summary_from_link(link)
             self.cases.append({
-                'date': convert_date_string(date_string),
-                'name': name,
+                'date': date,
                 'docket': docket,
+                'name': name,
                 'url': url,
+                'summary': summary,
             })
+
+    def extract_docket_name_url_from_link(self, link):
+        url = link.attrib['href']
+        text = link.text_content().strip()
+        parts = text.split('-', 1)
+        name = parts[1]
+        docket = parts[0].strip()
+        docket = '' if 'order' in docket.lower() else docket
+        return docket, name, url
+
+    def extract_date_summary_from_link(self, link):
+        # Link should be within a <p> tag directly under <div id='maincontent'>, but
+        # occasionally the courts forgets the wrap it in a <p>, in which case it should
+        # be directly under the <div id='maincontent'>
+        container_id = 'maincontent'
+        parent = link.getparent()
+        parents_parent = parent.getparent()
+        if 'id' in parent.attrib and parent.attrib['id'] == container_id:
+            search_root = link
+        elif 'id' in parents_parent.attrib and parents_parent.attrib['id'] == container_id:
+            search_root = parent
+        else:
+            raise InsanityException('Unrecognized placement of Opinion url on page: "%s"' % link.text_content().strip())
+
+        # Find date from bolded header element above link (ex: "5-14-2014 - Opinions" or "5-21-2014 - Orders")
+        element_date = search_root.xpath('./preceding-sibling::b')[-1]
+        element_date_text = element_date.text_content().strip().lower()
+        if not element_date_text.endswith('opinions') and not element_date_text.endswith('orders'):
+            raise InsanityException('Unrecognized bold (date) element: "%s"' % element_date_text)
+        date_string = element_date_text.split()[0]
+
+        # Find summary from blockquote element below link
+        element_blockquote = search_root.xpath('./following-sibling::blockquote')[0]
+        summary = element_blockquote.text_content().strip()
+
+        return convert_date_string(date_string), summary
 
     def _get_download_urls(self):
         return [case['url'] for case in self.cases]
@@ -83,3 +95,6 @@ class Site(OpinionSite):
 
     def _get_precedential_statuses(self):
         return ['Published'] * len(self.cases)
+
+    def _get_summaries(self):
+        return [case['summary'] for case in self.cases]
