d59b0431fec6ac3544f559329d311bf9174c147b
Michael Lissner <mlissner@michaeljaylissner.com>
2019-07-28 23:53:53 -0700

    feat(pacer): First version of bankruptcy claims register
    Includes a parsing and querying methods, but punts on doing any sort of
    non-default parsing or querying. One day we can enhance it to add more
    features, but for the moment, defaults are all we care about.

--- juriscraper/pacer/__init__.py
+++ juriscraper/pacer/__init__.py
@@ -2,6 +2,7 @@ from .appellate_docket import AppellateDocketReport
 from .attachment_page import AttachmentPage
 from .case_query import CaseQuery
 from .case_query_advanced import CaseQueryAdvancedBankruptcy
+from .claims_register import ClaimsRegister
 from .docket_history_report import DocketHistoryReport
 from .docket_report import DocketReport
 from .free_documents import FreeOpinionReport
@@ -15,6 +16,7 @@ __all__ = [
     AttachmentPage,
     CaseQuery,
     CaseQueryAdvancedBankruptcy,
+    ClaimsRegister,
     DocketHistoryReport,
     DocketReport,
     FreeOpinionReport,
