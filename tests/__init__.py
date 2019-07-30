330518cf083216e6459b6ef1aabb2ee86a5703d8
Philip Ardery <arderyp@protonmail.com>
2019-07-29 18:13:00 -0700

    attempting to fix Circle testing, bumping version minor

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,9 +1,14 @@
 import os
-
+import unittest
 import juriscraper
+
 
 JURISCRAPER_ROOT = os.path.realpath(os.path.join(
     os.path.realpath(juriscraper.__file__), '..'))
 TESTS_ROOT = os.path.realpath(os.path.join(JURISCRAPER_ROOT, '../tests'))
 TESTS_ROOT_EXAMPLES = os.path.join(TESTS_ROOT, 'examples')
 TESTS_ROOT_EXAMPLES_PACER = os.path.join(TESTS_ROOT_EXAMPLES, 'pacer')
+
+
+def test_local():
+    return unittest.TestLoader().discover('./tests/local')
