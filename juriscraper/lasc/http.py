cba6f7a93d24dd1b61c23ba30101c618589cbb99
Michael Lissner <mlissner@michaeljaylissner.com>
2019-09-10 22:48:14 -0700

    refactor(lasc): Cleanup of http module
    This was a pretty big cleanup, so some notes.
    
    1. Just general cleanup. All functions get proper docstrings, instance
       variables eliminated where not needed, import statements cleaned up,
       whitespace & line length normalized, etc. A lot of this, some
       expected, but more than I'd hope for.
    
    2. I eliminated all hooks. I like the API they provide (it's neat
       to know that a hook will run after a request), but I can't stand the
       damage they do to the function that's called. Instead of having a
       simple API like `some_function(r)`, they wind up with a terrible one
       with args and kwargs. I think these would be worth it if we had a
       hook we *always* wanted to run after every request, but I don't think
       they're worth it for hooks that you only run after a single request.
       Just use a simple function for that! I think the code is now much
       more readable.
    
    3. I hid a number of internal-use-only functions that shouldn't be used
       by outside users of these objects.
    
    4. I eliminated the use of self.s. Since this extends Request.session,
       all you have to do is self.get, self.post, self.hooks, self.cookies,
       etc.
    
    This code is entirely untested, so I think it's back to @flooie for
    that, but I think this should be a solid improvement in style,
    readability, and general Pythonic-ness.

--- juriscraper/lasc/http.py
+++ juriscraper/lasc/http.py
@@ -1,179 +1,151 @@
-import requests, json
-requests.packages.urllib3.disable_warnings(requests.packages.urllib3.exceptions.InsecureRequestWarning)
+import requests
+
+from lxml.html import fromstring
+
 from ..lib.log_tools import make_default_logger
+
+requests.packages.urllib3.disable_warnings(
+    requests.packages.urllib3.exceptions.InsecureRequestWarning)
+
 logger = make_default_logger()
-from lxml.html import fromstring as f
 
 
 class LASCSession(requests.Session):
     """
-    Extension for Courtlistner to connect into the
-    Los Angeles Superior Court Media Access Portal.
-
+    A requests.Session object with special tooling to handle the Los Angeles
+    Superior Court Media Access portal.
     """
 
     def __init__(self, username=None, password=None):
         """
         Instantiate a new LASC HTTP Session with some Juriscraper defaults.
-        This method requires credentials from the media access portal to
+        This method requires credentials from the media access portal.
 
-        :param username: media access portal credentials
-        :param password: media access portal credentials
-
+        :param username: MAP username
+        :param password: MAP password
+        :return: A LASCSession object
         """
+        super(LASCSession, self).__init__()
 
         self.html = None
 
         # Los Angeles Superior Court MAP urls and paths
-        self.la_domain = 'https://media.lacourt.org'
-
-        self.la_signin_path = "/signin-oidc"
-        self.la_login_path = "/api/Account/Login"
-
-        self.la_login_url = '%s%s' % (self.la_domain, self.la_login_path)
-        self.la_signin_url = "%s%s" % (self.la_domain, self.la_signin_path)
+        la_url = 'https://media.lacourt.org'
+        self.login_url = '%s/api/Account/Login' % la_url
+        self.signin_url = "%s/signin-oidc" % la_url
 
         # Microsoft urls and paths
-        self.micro_domain = "https://login.microsoftonline.com"
-        self.api_base_path = "/calcourts02b2c.onmicrosoft.com/B2C_1_Media-LASC-SUSI/"
+        ms_base_url = "https://login.microsoftonline.com"
+        api_path1 = "/calcourts02b2c.onmicrosoft.com/B2C_1_Media-LASC-SUSI/SelfAsserted?"
+        api_path2 = "/calcourts02b2c.onmicrosoft.com/B2C_1_Media-LASC-SUSI/api/CombinedSigninAndSignup/confirmed?"
+        self.api_url1 = "%s%s" % (ms_base_url, api_path1)
+        self.api_url2 = "%s%s" % (ms_base_url, api_path2)
 
-        self.api1 = "SelfAsserted?"
-        self.api2 = "api/CombinedSigninAndSignup/confirmed?"
-
-        self.api_path1 = "%s%s" % (self.api_base_path, self.api1)
-        self.api_path2 = "%s%s" % (self.api_base_path, self.api2)
-
-        self.api_call1 = "%s%s" % (self.micro_domain, self.api_path1)
-        self.api_call2 = "%s%s" % (self.micro_domain, self.api_path2)
-
-        # Login Post Data
-        self.data = {
+        self.login_data = {
             "logonIdentifier": username,
             "password": password,
             "request_type": "RESPONSE"
         }
-
-        self.data2 = {}
-
-        self.api_params = {"p":"B2C_1_Media-LASC-SUSI", "tx":""}
-
+        self.api_params = {"p": "B2C_1_Media-LASC-SUSI", "tx": ""}
         self.headers = {
-            "Origin": self.micro_domain,
+            "Origin": ms_base_url,
             "User-Agent": "Juriscraper",
         }
 
     def get(self, url, auto_login=False, **kwargs):
         """Overrides request.Session.get with session retry logic.
 
-        Use it to import very obvious functions.
-
         :param url: url string to GET
         :param auto_login: Whether the auto-login procedure should happen.
         :return: requests.Response
         """
-
-        kwargs.setdefault('headers', self.headers)
         kwargs.setdefault('timeout', 30)
-
-        if not hasattr(self, 's'):
-            self.s = requests.session()
-            self.s.hooks['response'] = [self.log_status]
-
-        r = self.s.get(url, **kwargs)
-
-        return r
+        return super(LASCSession, self).get(url, **kwargs)
 
     def post(self, url, auto_login=False, **kwargs):
-        """Overrides request.Session.get with session retry logic.
+        """Overrides request.Session.post with session retry logic.
 
         :param url: url string to GET
         :param auto_login: Whether the auto-login procedure should happen.
         :return: requests.Response
         """
-
-        kwargs.setdefault('headers', self.headers)
         kwargs.setdefault('timeout', 30)
+        return super(LASCSession, self).post(url, **kwargs)
 
-        r = self.s.post(url, **kwargs)
-
-        return r
-
-    def parse_html_for_keys(self, *args, **kwargs):
+    @staticmethod
+    def _parse_html_for_api_parameters(r):
         """
-        This method parses the HTML after the first login page and idenitifies the two parameter values
-        Both parameters are required and both are used multiple times.
+        Parse the HTML after the first login page and identify parameter values
+        needed in subsequent requests.
 
-        :return:
-
+        :param r: A request.Response object
+        :return A dict containing parsed parameter values
+        :raises: LASCLoginException
         """
+        if r.status_code != 200:
+            raise LASCLoginException("Got bad status code when attempting "
+                                     "initial login: %s" % r.status_code)
 
-        if 200 == args[0].status_code:
-            self.csrf = args[0].text.split("csrf")[1].split("\"")[2]
-            self.transid = args[0].text.split("transId")[1].split("\"")[2]
+        csrf = r.text.split("csrf")[1].split("\"")[2]
+        transaction_id = r.text.split("transId")[1].split("\"")[2]
+        return {
+            'tx': transaction_id,
+            'csrf_token': csrf,
+            'TRANSID': transaction_id,
+        }
 
-            self.headers['X-CSRF-TOKEN'] = self.csrf
-            self.api_params['tx'] = self.transid
-            self.api_params['csrf_token'] = self.csrf
-            self.api_params['TRANSID'] = self.transid
+    @staticmethod
+    def _parse_new_html_for_keys(r):
+        """Parse the current page for new key data
 
-    def parse_new_html_for_keys(self, *args, **kwargs):
+        This method parses the HTML after the first login page and identifies
+        the parameter values required for the next step.
+
+        :param r: A request.Response object
+        :return: A dict containing the needed keys
         """
-        # Parse the current page for new key data
+        html = fromstring(r.text)
+        return {
+            'code': html.xpath('//*[@id="code"]')[0].value,
+            'id_token': html.xpath('//*[@id="id_token"]')[0].value,
+            'state': html.xpath('//*[@id="state"]')[0].value,
+        }
 
-        This method parses the HTML after the first login page and idenitifies the three parameter values required.
-        the f method comes from lxml.html import fromstring as f
+    @staticmethod
+    def check_login(r):
+        """Check that the login succeeded
 
-        :return:
+        :param r: A request.Response object
+        :return: None
+        :raises LASCLoginException
         """
-        # Parse HTML with LXML by Xpath.
-        j = f(args[0].text)
+        if r.status_code == 200:
+            return
 
-        self.data2['code'] = j.xpath('//*[@id="code"]')[0].value
-        self.data2['id_token'] = j.xpath('//*[@id="id_token"]')[0].value
-        self.data2['state'] = j.xpath('//*[@id="state"]')[0].value
-
-
-    def check_login(self, *args, **kwargs):
-        # Error handling for authentication.
-        if args[0].status_code != 200:
-            if u'Your password is incorrect' in json.loads(args[0])['message']:
-                logger.info(u'Password was incorrect')
-                raise LASCLoginException("Invalid username/password")
-
-            if u'We can\'t seem to find your account' in json.loads(args[0])['message']:
-                logger.info(u'Invalid Email Address')
-                raise LASCLoginException("Invalid Email Address")
-
-
-    def log_status(self, *args, **kwargs):
-        """
-        A simple function to provider logger info
-        coded as a hook.
-
-        """
-
-        if args[0].url == "https://media.lacourt.org/api/Account/Login":
-            logger.info(u'Logging into MAP begun')
-
-        if args[0].url == "https://media.lacourt.org/":
-            logger.info(u'Successfully Logged into MAP')
-
+        message = r.json['message']
+        if u'Your password is incorrect' in message:
+            logger.info(u'Password was incorrect')
+            raise LASCLoginException("Invalid username/password")
+        if u"We can't seem to find your account" in message:
+            logger.info(u'Invalid Email Address')
+            raise LASCLoginException("Invalid Email Address")
 
     def login(self):
-        """
-        Logging into Media Access Portal.
+        """Log into the LASC Media Access Portal
 
-        If you have media access credentials and permission two GET request and two POST requests
-        You need to collect the transID/tx cookie value and X-CSRF-TOKEN from hidden form inputs during the process
-        You also need to identify a "state, id_token and code" values as parameters during the process.
+        The process is tricky, requiring two GET requests, each of which
+        returns HTML or JSON that is parsed for values to send in a subsequent
+        POST.
 
-        If you do *not* have media access permissions you can not log into the media access portal.
+        Note that if you do not have media access permissions you can not log
+        into the media access portal.
 
-        We use a session to store cookies.
+        The individual requests are displayed below for reference.
 
-        The first request ...
-        =====================
-        First we capture the login page and capture and parse out of the TransID and Cross Site Forgery Request token
+        The first request
+        =================
+        First we capture the login page and capture and parse out of the
+        TransID and Cross Site Forgery Request token
 
         curl -X 'GET'
             -H 'Accept: */*'
@@ -184,9 +156,11 @@ class LASCSession(requests.Session):
             'https://login.microsoftonline.com/te/calcourts02b2c.onmicrosoft.com/b2c_1_media-lasc-susi/oauth2/v2.0/authorize?client_id=64f6a02f-a3d7-4871-a7b0-0883a24bdbda&redirect_uri=https%3A%2F%2Fmedia.lacourt.org%2Fsignin-oidc&response_type=code%20id_token&scope=openid%20profile%20offline_access%20https%3A%2F%2Fcalcourts02b2c.onmicrosoft.com%2Fapi%2Fread&response_mode=form_post&nonce=636989032254084125.MWY4YWQxN2UtNDM5MS00ZTc5LThkN2YtMTlhYWZmZjBhZDk5YTVlNDEyMmEtMzI5ZS00YzExLTg4YTMtYmU2MDNhZjgzMWNj&state=CfDJ8Pwr5_ZSA25Lq9tGMhyZSHpY1A9mXEeyoedJD-KgsE5SoZhpEwenEHXOe0PmMbNxFslFGyGgyA4COKS_zqKlJghkVIYO02_amjN6-JOSfcGISf9rY9uqF2yOnwhkda3D00JcU_sOPP6uKexwG87dXvMLaxN6LuCu2GqXGkkAe6fOVKBlzjL8-G_EqX3j0ok8zwUaRJW8D6I4GeCfjg2LJOk4YBmGGmRYYTdCG4usSI1FKYLjhyCVly7aXf__QfHMvCKzVO9tjmK-Sq3REFtCLdPTiBuv1YjE6DlVj6_iUJ_D&x-client-SKU=ID_NET&x-client-ver=2.1.4.0'
 
 
-        The second request ...
-        =====================
-        We make our first post request into microsofts login API and check that login credentials were correct.
+        The second request
+        ==================
+        We make our first POST request to Microsoft's login API and check that
+        login credentials were correct.
+
         curl -X 'POST'
             -H 'Accept: */*'
             -H 'Accept-Encoding: gzip, deflate'
@@ -197,10 +171,11 @@ class LASCSession(requests.Session):
             -d 'request_type=RESPONSE&password={{PASSWORD}}&logonIdentifier={{USERNAME}}'
             'https://login.microsoftonline.com/calcourts02b2c.onmicrosoft.com/B2C_1_Media-LASC-SUSI/SelfAsserted?p=B2C_1_Media-LASC-SUSI&csrf_token=MG9tTzRzUTNRWjlaTEVUOG1TT1RsN0w5dTdMdTM4RHpwUGlHcEJwTmxudkNTUXE3T3RGQzNkVEJrRHVrTzc5OEcrM2VSWnlUZ3hyNzJtQUJWYTVnN1E9PTsyMDE5LTA3LTE2VDE5OjQ5OjE4LjQyMjExOTlaO2kzMjhnTURMQ0NuYytZTGRCdDVjRWc9PTt7Ik9yY2hlc3RyYXRpb25TdGVwIjoxfQ%3D%3D&TRANSID=StateProperties%3DeyJUSUQiOiI4OWVlNGFkMy02Y2MzLTRhNzEtOWM2Ni01ZjY3MjNkOWNmYzIifQ&tx=StateProperties%3DeyJUSUQiOiI4OWVlNGFkMy02Y2MzLTRhNzEtOWM2Ni01ZjY3MjNkOWNmYzIifQ'
 
-        The third request ...
-        =====================
-        The third request handles a redirect request that is required by the system. And we generate the paramters we need in the for the final
-        request
+        The third request
+        =================
+        The third request handles a redirect request that is required by the
+        system. And we generate the parameters we need for the final request.
+
         curl -X 'GET'
             -H 'Accept: */*'
             -H 'Accept-Encoding: gzip, deflate'
@@ -212,9 +187,11 @@ class LASCSession(requests.Session):
             'https://login.microsoftonline.com/calcourts02b2c.onmicrosoft.com/B2C_1_Media-LASC-SUSI/api/CombinedSigninAndSignup/confirmed?p=B2C_1_Media-LASC-SUSI&csrf_token=QzdLais4SU11S2l0WVBKWGRHOWVyMHFyU0hOcWNlNUJJTkN6MFJ3dnBLdGY1a2VnTW4vZS9jQk9hRnJrU0NHTVZtbEVWWXoxeEZFbFpXQ1FUaXFtZ0E9PTsyMDE5LTA3LTE2VDE5OjUxOjA3LjI4Mjc2OVo7cFkwekVwN0VvMVdQazdYNTc2MkYvQT09O3siT3JjaGVzdHJhdGlvblN0ZXAiOjF9&TRANSID=StateProperties%3DeyJUSUQiOiI5ZDQ5NzI1MC03ZGYzLTRiMGEtYTY5Ny1iZWQ2MDkwMzA5OTIifQ&tx=StateProperties%3DeyJUSUQiOiI5ZDQ5NzI1MC03ZGYzLTRiMGEtYTY5Ny1iZWQ2MDkwMzA5OTIifQ'
 
 
-        The fourth request ...
-        =====================
-        The last request finishes the and navigates to the signin page.  We then use a hook to check that we are logged in.
+        The fourth request
+        ==================
+        The last request finishes the and navigates to the signin page.  We
+        submit our credentials and check that we are logged in.
+
         curl -X 'GET'
             -H 'Accept: */*'
             -H 'Accept-Encoding: gzip, deflate'
@@ -222,31 +199,33 @@ class LASCSession(requests.Session):
             -H 'Cookie: .AspNetCore.Cookies=CfDJ8Pwr5_ZSA25Lq9tGMhyZSHr8UY6g7hwjNU0XlUHTOl3E8jJL2J9zZCP_M8hPTu9jkTnz48n7Xh04NsryQVtHsekPLasvImDu9vVB9Yh5Q1ZLXcLSLyUbErgz1n6r2c7A9R4DI5BRGz3IM10QxpF9p9Pv9zzWieQUd47LHY2jKXmjdW-ygHMVrLHKVMJuiijm0JsprghvGKm1Poqj8MSOapUeyKbIVtie-2q0RpwiUA-KljZu95-k08d2G33ZHo8ntgLYLY_fgOL2Mz8n038Sa5zSCtlqgxanWmTF5OS7-ijcuYXmdbl8Ln7_uA9181-uJjaKM_qLawemVGPhzSSKG9y0txRyol0UBu6xy7zp2M5QcpJ3ZDU6nVG05RGKd3OjgU5VxXvuNI2kriXiNfzAaJzxhY_vlK7nBccPjGJrF5UlZyfdNUQS9PbDkRmITHQHGYe2HAQBqgIQ7DLq5RzQWK5201_ADnS8Ge_hJk6sb1iv_n8iPjsT28jsEtO0Wlk_Sk3kxabE1NWEOXV8aPesgWw6Yicyn6cu4mR1Bkatm3avewOCUEct7nBDGpcosrR9spOt_vXyjmqI3KbumFtmrLxnR0SjwXBNnsi1uGunjF_Lsan8aprc-CfQr7gHKUGXd3xLz3-3N66G8jbwxRnSeLOnYQ2vykVKFHM_QoA9TBVeJJ4XIHNW0blaIzkeI_X9InJy8-5ZCRJ_2ebKkvbcLC7hEnKVk8PxwyomOMW8CeF8l8MrqDgpXtwLq52L6tU2PJu4gIMSmxDymldDrBoHQDNveMX8P3i85OklGMIH9mRlXZJaJKdLcM3d08MUz7cpB3AjpBLp5LZPP2YLRkJXMF53eZwZDEGi1b1hZDmoUeKAs1ALsE2OwhPZ8YTxSsO9jlylAoZEv-52VCKI-58iq08JJNn27WbFuYumCwaUU3eElhy8BF4XcNdKmdiHHOfrv2Cg3hNqCadPsYejtYApoQ2EYX98e7lhajwrUZTY4ymsge7aq4masSkM468Huah-W4eq7fK5gbBr1vZX0ef9uj6k9RqYJXlOaIN2cl0vamNZvYLHAxWhtP1gHwC0QalRCffOod00z4t3qS3V2Xj7YBBp_uWkjlbM32JKiAWH99pc2C81Xwp2LomaF5JQweS_qawQ7z0l_yIEGAN9DStN3cvGd0lsMZRGvDSWVXQ8IJk0EHkqSmNsQoKsdaaN9FuNM_vLCgdFBKPhgkJTjjsyWAdldOhJAL57AU5x5Ay54nh886sh-94CtuJ5Jd18_yuqKqBsiTa8NV0DbRNl2uYM1MSCs1iYUxyeNY8HI7iyj-Q5kh4WpDQjHhckeT3SLX8PvCEyRctLzhAx__UhTRC-b6xZViCUMaUuLh70ioLBy7hTESQmaciIHDczeQKbfmw70USzj5hRE9oDfjAOTRAMT2FquTuRlVepC3SCQ3Vjqv5j_gjUTGlYcHazbnGY4VSacOQv4az2wE96xXnRL3NoBgN_icp4F-A8YwLmPYJRyK64ouxc2GJPbMEUbJysmCUPYTyCRdoMDszyxc47VJFu-5Zzn-yXab_y5L4Cl_W-Tj105KKgATJLAhwLqQZHBFs4cDTYZ2lhzqEQdKvNyKDrEN_zi-dDvTTF24poPQyWRAlMTgA5I9uZrBof4EXcjad0mISA--RFEseDao5kfzfzPSNHZBgwffIX1bYcxGGfxZ5wGrqg1KCudXnWwSmIuHPlM_uTEGefNBg_dRcaIyif0AnP75hKKA01rdyK48E8UnCrkITXqBdcTy0eYXIHtRCaZNF41nfUCjgIa7MBcpMnfOsHrz-fiKL05DYZmG4MBcX6434QA3srLxXY7u6gsKBj_phEaE5vKYnFt-Oal8JRjjBozrJ7haGhkPSM-of1jj-4dvGLirzObZ716Se2Z8naDL6TriQDj9218T3uRJvwwuXMkwgCSfIAYKWYcBUKKz18IJ8CaloKMPn7QnOlrtfIU0fLoyTsxpBc0T0uIIAu5RELJ6squzUwigR2VkaJ9mhAQ1CvXiR3PTkagP_xt42TifuxnOluf0WyJbXrPg61o4z64GqMSzaEdpf3efI8prfQfx_krXP4gSgKHw5qanPK0ID0-aZ4A1wv2i3gp8mRapYrcmf4Y_VlWSJqEOzm2SSSvM1WvjNevJrQQYPwwXuLIwy8Zi6l8nhd-9PnWBD02R1hjUdTv_VYxoF2nRgP-qMXPeVg99eNfOzJ1wo9pW0gxQ; .AspNetCore.Session=CfDJ8Pwr5%2FZSA25Lq9tGMhyZSHqSUZ1fdAhQfTXbZveNz5Cd4zSPbFO%2FoUukxlHK7LiHmkicHk85LyDx4gO85rjEeUYnoWRnUEeZ3T9%2BWiV7COW%2BuZnAsBHtJDMj%2BX9hzh9STn9tXURw1isdbX4hNBVziHR7695e6oxnwHSbm1tI5ip2' -H 'Origin: https://login.microsoftonline.com' -H 'User-Agent: Juriscraper' -H 'X-CSRF-TOKEN: YlREUUpJUEJMcCtac1UweVVwRDZRM1hIdjVLK1JzL1BVT3d4TFpaR0ZPVkY0dENlMDN0OVBta01IU3E4Vk5iMjhWd1JxVTk1NE4rZy9WUUFOemdXaWc9PTsyMDE5LTA3LTE2VDE5OjUyOjM4LjA5ODYxNzhaO3c2VnJrT1VYYkxkRW5RUURtYmQ5anc9PTt7Ik9yY2hlc3RyYXRpb25TdGVwIjoxfQ=='
             'https://media.lacourt.org/'
 
+        :return: None
+        :raises: LASCLoginException
         """
+        # Load the page and get the TransID and CSRF Token values from the HTML
+        logger.info(u'Logging into MAP has begun')
+        r = self.get(self.login_url)
 
+        # Set the CSRF header for subsequent requests
+        api_params = self._parse_html_for_api_parameters(r)
+        self.headers['X-CSRF-TOKEN'] = api_params['csrf_token']
 
-        # Load the page in order to get the TransID and CSFR Token values from the HTML
-        self.get(self.la_login_url, hooks={'response': [self.parse_html_for_keys, self.log_status]})
+        # Call part one of Microsoft login API
+        r = self.post(self.api_url1, params=api_params, data=self.login_data)
+        self.check_login(r)
 
-        # Call Part one of Microsoft login API
-        self.post(self.api_call1, params=self.api_params, data=self.data, hooks={'response': [self.check_login]})
+        # Call part two of Microsoft login API - Redirect
+        r = self.get(self.api_url2, params=api_params)
 
-        # Call Part two of Microsoft login API - Redirect
-        self.get(self.api_call2, params=self.api_params, hooks={'response': [self.parse_new_html_for_keys]})
-
-        # # Finalize login with post into LA MAP site
-        self.post(self.la_signin_url, data=self.data2)
-
-
-
+        # Finalize login with post into LA MAP site
+        parsed_keys = self._parse_new_html_for_keys(r)
+        self.post(self.signin_url, data=parsed_keys)
+        logger.info(u'Successfully Logged into MAP')
 
 
 class LASCLoginException(Exception):
-
     """
     Raised when the system cannot authenticate with LASC MAP
     """
-
     def __init__(self, message):
         Exception.__init__(self, message)
-
