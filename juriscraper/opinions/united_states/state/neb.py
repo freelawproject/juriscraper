a332e5c67eddad1ff47cd6a6cb4b6c93d6c782b4
Philip Ardery <arderyp@protonmail.com>
2019-09-10 21:35:13 -0700

    fix(neb): converting to Linear parsing to handle new use case of empty date tables

--- juriscraper/opinions/united_states/state/neb.py
+++ juriscraper/opinions/united_states/state/neb.py
@@ -1,39 +1,35 @@
-# Author: Michael Lissner
-# Date created: 2013-06-13
-
-from juriscraper.OpinionSite import OpinionSite
-from juriscraper.lib.string_utils import convert_date_string
+from juriscraper.OpinionSiteLinear import OpinionSiteLinear
 
 
-class Site(OpinionSite):
+class Site(OpinionSiteLinear):
     def __init__(self, *args, **kwargs):
         super(Site, self).__init__(*args, **kwargs)
         self.court_id = self.__module__
         self.url = 'https://supremecourt.nebraska.gov/courts/supreme-court/opinions'
+        self.status = 'Published'
 
-    def _get_download_urls(self):
-        return [href for href in self.html.xpath('//td[3]/a/@href')]
+    def _process_html(self):
+        path = {
+            'group': '//div[contains(@class, "view-grouping")]',
+            'date': './/span[contains(@class, "date-display-single")]',
+            'row': './/tbody/tr',
+        }
+        for group in self.html.xpath(path['group']):
+            date_element = group.xpath(path['date'])
+            if not date_element:
+                continue
+            date = date_element[0].text_content()
+            for row in group.xpath(path['row']):
+                url = row.xpath('.//td[3]//a/@href')
 
-    def _get_case_names(self):
-        return [cell.text_content() for cell in self.html.xpath('//td[3]')]
+                # skip rows without url in third cell
+                if not url:
+                    continue
 
-    def _get_case_dates(self):
-        dates = []
-        path_group = '//div[@class="view-grouping"]'
-        subpath_date = './/span[@class="date-display-single"]'
-        subpath_cases = './/tbody/tr'
-        for group in self.html.xpath(path_group):
-            date_string = group.xpath(subpath_date)[0].text_content()
-            date = convert_date_string(date_string)
-            case_count = len(group.xpath(subpath_cases))
-            dates.extend([date] * case_count)
-        return dates
-
-    def _get_precedential_statuses(self):
-        return ['Published'] * len(self.case_names)
-
-    def _get_docket_numbers(self):
-        return [cell.text_content() for cell in self.html.xpath('//td[1]')]
-
-    def _get_neutral_citations(self):
-        return [cell.text_content() for cell in self.html.xpath('//td[2]')]
+                self.cases.append({
+                    'date': date,
+                    'docket': row.xpath('.//td[1]')[0].text_content(),
+                    'name': row.xpath('.//td[3]')[0].text_content(),
+                    'neutral_citation': row.xpath('.//td[2]')[0].text_content(),
+                    'url': url[0],
+                })
