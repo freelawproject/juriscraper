f52946fdc4f8091e5feddf54817b25a52404d3c2
Philip Ardery <arderyp@protonmail.com>
2019-09-15 17:36:23 -0700

    fix(idahoctapp_u): simplifying scraper to handle new use case by being linear.  now capturing some records that we were inadvertently missing before. rebased the example test files since we are now capturing docket numbers where we can ('' where there is nonw), which invalidated all the old example files by adding a new field. bumping version minor.

--- juriscraper/opinions/united_states/state/idahoctapp_u.py
+++ juriscraper/opinions/united_states/state/idahoctapp_u.py
@@ -1,76 +1,42 @@
 # coding=utf-8
 
-import re
+from juriscraper.OpinionSiteLinear import OpinionSiteLinear
+from juriscraper.lib.exceptions import InsanityException
+from juriscraper.lib.string_utils import clean_string
 
-from juriscraper.OpinionSite import OpinionSite
-from juriscraper.lib.string_utils import convert_date_string
 
-
-class Site(OpinionSite):
-    """Unfortunately, while Idaho has updated all of its other pages to use nice
-    tables, ctapp_u continues to use a nasty ul. This scraper used to live
-    almost char for char in idaho_civil, before it was updated. Alas, now it
-    lives here.
-    """
+class Site(OpinionSiteLinear):
     def __init__(self, *args, **kwargs):
         super(Site, self).__init__(*args, **kwargs)
-        self.url = 'http://www.isc.idaho.gov/appeals-court/coaunpublished'
-        self.base_path = '//div[@id = "block-system-main"]//div[contains(concat(" ", @class, " "), " field-items ")]//li[contains(./a/@href, ".pdf")]'
-        self.sub_paths = [
-            'a[./text()][1]',
-            'span/a[./text()][1]',
-            'span/span/a[./text()][1]'
-        ]
         self.court_id = self.__module__
+        self.url = 'http://www.isc.idaho.gov/appeals-court/coaunpublished'
+        self.status = 'Unpublished'
 
-    def _get_precedential_statuses(self):
-        return ["Unpublished"] * len(self.case_names)
+    def _process_html(self):
+        for item in self.html.xpath('//li[contains(.//a/@href, ".pdf")]'):
+            text = clean_string(item.text_content())
+            text_parts = text.split('-', 1)
 
-    def tweak_response_object(self):
-        """
-        Idaho does not set their encoding properly.
-        """
-        self.request['response'].encoding = 'UTF-8'
+            if len(text_parts) != 2:
+                raise InsanityException('Unexpected text format: "%s"' % text)
 
-    def _get_case_names(self):
-        case_names = []
-        for e in self.html.xpath(self.base_path):
-            for path in self.sub_paths:
-                try:
-                    s = e.xpath(path)[0].text_content()
-                    break
-                except IndexError:
-                    continue
-            s = ' '.join(s.split())
-            regexes = [u'(.*?) [-–] (.*?)– ',
-                       u'(.*?) [-–] (.*)- ',
-                       u'(.*?) [-–] (.*)$',
-                       u'(.*[0-9]{4})(.*)']
-            for regex in regexes:
-                try:
-                    case_names.append(re.search(regex, s).group(2))
-                    # Found what we're looking for...
-                    break
-                except AttributeError:
-                    # Try the next regex...
-                    continue
-        return case_names
+            # sometimes the records include a docket number(s) as the
+            # first words in the second half of the hyphenated string,
+            # but some don't include a docket at all.  So we test to see
+            # if the first word is numeric (minus the slash characters
+            # used to conjoin multiple docket numbers).
+            docket_name = text_parts[1].split(None, 1)
+            first_word = docket_name[0].replace('/', '')
+            if first_word.isnumeric():
+                docket = docket_name[0]
+                name = docket_name[1]
+            else:
+                docket = ''
+                name = text_parts[1]
 
-    def _get_download_urls(self):
-        download_urls = []
-        for e in self.html.xpath(self.base_path):
-            for path in self.sub_paths:
-                try:
-                    sub_e = e.xpath('%s/@href' % path)[0]
-                except IndexError:
-                    continue
-            download_urls.append(sub_e)
-        return download_urls
-
-    def _get_case_dates(self):
-        case_dates = []
-        for element in self.html.xpath(self.base_path):
-            text = element.text_content()
-            date_string = text.split('-')[0].strip()
-            case_dates.append(convert_date_string(date_string))
-        return case_dates
+            self.cases.append({
+                'date': text_parts[0],
+                'docket': docket,
+                'name': name,
+                'url': item.xpath('.//a/@href')[0],
+            })
