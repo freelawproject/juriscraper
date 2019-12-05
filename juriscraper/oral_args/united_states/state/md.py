04309c837dc244d7edcc758461cfac5c5253933d
Philip Ardery <arderyp@protonmail.com>
2019-12-04 19:20:07 -0800

    fix(oral_arg/md): created new OralArgumentSiteLinear and used that to fix md oral_arg scraper, which was failing in prod. On a completely unrelated nore, I think this thing has been silently broken (skipping content) for a long time. Oddly enough, it's original html example file was missing a compare file, which is odd.  Anywho, it seems ot be working now, and is much cleaner and easier to understand.

--- juriscraper/oral_args/united_states/state/md.py
+++ juriscraper/oral_args/united_states/state/md.py
@@ -18,54 +18,25 @@ Mike
 
 CourtID: md
 Court Short Name: Md.
-Author: Brian W. Carver
-Reviewer: mlr
-History:
-  2014-10-17: Created by Brian Carver
-  2014-10-20: Some tweaks by mlr
 """
 
-from datetime import datetime
-
-from juriscraper.OralArgumentSite import OralArgumentSite
+from juriscraper.OralArgumentSiteLinear import OralArgumentSiteLinear
 
 
-class Site(OralArgumentSite):
+class Site(OralArgumentSiteLinear):
     def __init__(self, *args, **kwargs):
         super(Site, self).__init__(*args, **kwargs)
         self.court_id = self.__module__
         self.url = 'http://www.courts.state.md.us/coappeals/webcasts/webcastarchive.html'
 
-    def _get_download_urls(self):
-        path = "//table//tr/td[2]/strong/a/@href[not(contains(., 'baradmissions'))]"
-        # This works because they usually link on docket number, but there's
-        # one page where the audio must have been long and they link instead
-        # on 'Part 1' and 'Part 2'. That sort of irregularity is not handled.
-        return list(self.html.xpath(path))
-
-    def _get_case_dates(self):
-        dates = []
-        path = "//table/tr[not(contains(.//@href, 'baradmission'))]/td[1]/text()"
-        for s in self.html.xpath(path):
-            for date_format in ('%m-%d-%Y', '%m-%d-%y'):
-                try:
-                    d = datetime.strptime(s, date_format).date()
-                except ValueError:
-                    continue
-                dates.append(d)
-        return dates
-
-    def _get_case_names(self):
-        # To avoid the "Title" rows we find the siblings of the rows with links.
-        path = '//table//tr/td[2]/strong/a/../../following-sibling::td/strong/text()'
-        cases = []
-        for case in self.html.xpath(path):
-            if case.strip():
-                cases.append(case)
-        return cases
-
-    def _get_docket_numbers(self):
-        # This works except for on the page where one of the recordings was
-        # split into Part 1 and Part 2.
-        path = "//table//tr/td[2]/strong/a/text()[not(contains(., 'Admissions'))]"
-        return list(self.html.xpath(path))
+    def _process_html(self):
+        # Find rows that contain valid, non-"Bar Admissions", link
+        path = "//tr[.//a/@href][not(contains(.//@href, 'baradmission'))]"
+        for row in self.html.xpath(path):
+            cell_two = row.xpath('./td[2]')[0]
+            self.cases.append({
+                'date': row.xpath('./td[1]')[0].text_content(),
+                'name': row.xpath('./td[3]/*[self::b or self::strong]')[0].text_content(),  # sometimes they use <b> tag, other times <strong> tag
+                'docket': cell_two.text_content(),
+                'url': cell_two.xpath('.//a/@href')[0],
+            })
