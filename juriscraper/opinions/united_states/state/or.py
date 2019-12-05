62384f2c9ec8c42589fa423787217812b8487676
Philip Ardery <arderyp@protonmail.com>
2019-12-04 18:37:07 -0800

    fix(or): handle new use case where second url is missing, in which case we skip the record; adding new example file for coverage.  I Will contact the cort seperately and ask them to fix the missing url issue.

--- juriscraper/opinions/united_states/state/or.py
+++ juriscraper/opinions/united_states/state/or.py
@@ -3,51 +3,36 @@ History:
  - 2014-08-05: Adapted scraper to have year-based URLs.
 """
 
-from juriscraper.OpinionSite import OpinionSite
-from juriscraper.lib.string_utils import convert_date_string
+from juriscraper.OpinionSiteLinear import OpinionSiteLinear
 
 
-class Site(OpinionSite):
+class Site(OpinionSiteLinear):
     def __init__(self, *args, **kwargs):
         super(Site, self).__init__(*args, **kwargs)
         self.court_id = self.__module__
         self.url = 'https://www.courts.oregon.gov/publications/sc/Pages/default.aspx'
         self.cases = []
+        self.status = 'Published'
 
-    def _download(self, request_dict={}):
-        html = super(Site, self)._download(request_dict=request_dict)
-        self.extract_cases(html)
-
-    def extract_cases(self, html):
-        for header in html.xpath('//h4//a/parent::h4'):
+    def _process_html(self):
+        for header in self.html.xpath('//h4//a/parent::h4'):
             date_string = header.text_content().strip()
             if not date_string:
                 continue
-            date = convert_date_string(date_string)
             ul = header.xpath('./following-sibling::ul')[0]
             for item in ul.xpath('.//li'):
+                # make sure two links are present
+                # see or_example_2.html
+                anchors = item.xpath('.//a')
+                if not (len(anchors) > 1):
+                    continue
                 text = item.text_content().strip()
-                url = item.xpath('.//a[1]/@href')[0]
-                docket = item.xpath('.//a[2]')[0].text_content().strip()
+                url = anchors[0].xpath('./@href')[0]
+                docket = anchors[1].text_content().strip()
                 name = text.split(')', 1)[-1]
                 self.cases.append({
-                    'date': date,
+                    'date': date_string,
                     'name': name,
                     'docket': docket,
                     'url': url,
                 })
-
-    def _get_case_names(self):
-        return [case['name'] for case in self.cases]
-
-    def _get_docket_numbers(self):
-        return [case['docket'] for case in self.cases]
-
-    def _get_download_urls(self):
-        return [case['url'] for case in self.cases]
-
-    def _get_case_dates(self):
-        return [case['date'] for case in self.cases]
-
-    def _get_precedential_statuses(self):
-        return ["Published"] * len(self.cases)
