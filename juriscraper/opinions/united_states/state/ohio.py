972d9e375703cb661aa77c0fb26e8111c486dd50
vvilliam <wpalin@gmail.com>
2020-03-02 13:39:01 -0500

    refactor(ohio): Remove selenium
    Does not wrap dates across years though

--- juriscraper/opinions/united_states/state/ohio.py
+++ juriscraper/opinions/united_states/state/ohio.py
@@ -9,17 +9,13 @@ History:
  - 2015-07-31: Redone by mlr to use ghost driver. Alas, their site used to be
                great, but now it's terribly frustrating.
 """
-from datetime import date, datetime
+from datetime import date, datetime, timedelta
 from lxml import html
-from lxml.html import tostring
-
-from juriscraper.AbstractSite import logger, phantomjs_executable_path
-from juriscraper.OpinionSiteWebDriven import OpinionSiteWebDriven
-from juriscraper.lib.html_utils import fix_links_in_lxml_tree
-from juriscraper.lib.string_utils import clean_if_py3
+from juriscraper.OpinionSite import OpinionSite
+from juriscraper.lib.string_utils import convert_date_string
 
 
-class Site(OpinionSiteWebDriven):
+class Site(OpinionSite):
     def __init__(self, *args, **kwargs):
         super(Site, self).__init__(*args, **kwargs)
         # Changing the page # in the url will get additional pages
@@ -29,104 +25,103 @@ class Site(OpinionSiteWebDriven):
         # not designed to walk through multiple pages.
         self.court_index = 0
         self.year = str(date.today().year)
+        self.backwards_days = 7
         self.url = "http://www.supremecourtofohio.gov/rod/docs/"
         self.court_id = self.__module__
-        self.base_path = "id('MainContent_gvResults')//tr[position() > 1]/td[2][string-length(normalize-space(text())) > 1]"
-        self.uses_selenium = True
+        self.base_path = '//tr[contains(.//a/@href, ".pdf")]/td[2][string-length(normalize-space(text())) > 1]/./..'
+        self.end_date = date.today()
+        self.start_date = self.end_date - timedelta(days=self.backwards_days)
+
 
     def _download(self, request_dict={}):
-        """This is another of the cursed MS asp.net pages with damned POST
-          parameters like __EVENTVALIDATION. These are near impossible to
-          scrape without using Selenium.
-        """
-        if self.test_mode_enabled():
-            return super(Site, self)._download(request_dict=request_dict)
-
-        logger.info("Now downloading case page at: %s" % self.url)
-        self.initiate_webdriven_session()
-
-        # Court drop down...
-        self.webdriver.find_element_by_xpath(
-            "//select[@id='MainContent_ddlCourt']"
-            "/option[@value='{court}']".format(court=self.court_index)
-        ).click()
-
-        # Year drop down...
-        yearDropDownId = "MainContent_ddlDecidedYear"
-        yearDropDownPath = (
-            "//select[@id='{id}']/option[@value='%s']" % self.year
-        )
-        yearDropdownPaths = [
-            yearDropDownPath.format(id=yearDropDownId),  # Legacy examples
-            yearDropDownPath.format(
-                id=yearDropDownId + "Min"
-            ),  # current (2017)
+        self.set_local_variables()
+        assert self.start_date.year == self.end_date.year, "Ohio can not wrap years"
+        pg = 2
+        r = self.request["session"].get(self.url)
+        soup = html.fromstring(r.text)
+        self.data["__VIEWSTATE"] = soup.xpath('//*[@id="__VIEWSTATE"]/@value')[
+            0
         ]
-        self.webdriver.find_element_by_xpath(
-            " | ".join(yearDropdownPaths)
-        ).click()
+        self.data["__EVENTVALIDATION"] = soup.xpath(
+            '//*[@id="__EVENTVALIDATION"]/@value'
+        )[0]
 
-        # Hit submit
-        submitPath = "//input[@id='MainContent_btnSubmit']"
-        self.webdriver.find_element_by_xpath(submitPath).click()
-
-        # Selenium doesn't give us the actual code, we have to hope.
-        self.status = 200
-
-        text = self._clean_text(self.webdriver.page_source)
-        html_tree = html.fromstring(text)
-        html_tree.rewrite_links(
-            fix_links_in_lxml_tree, base_href=self.request["url"]
+        response = self.request["session"].post(
+            "http://www.supremecourtofohio.gov/rod/docs/", data=self.data
         )
-        return html_tree
+
+        soup = html.fromstring(response.text)
+        orders = soup.xpath(self.base_path)
+
+        del self.data["ctl00$MainContent$btnSubmit"]
+
+        while soup.xpath('//a[contains(.//@href, "Page$%s\'")]' % pg):
+            self.data["__EVENTTARGET"] = "ctl00$MainContent$gvResults"
+            self.data["__EVENTARGUMENT"] = "Page$%s" % pg
+            self.data["__VIEWSTATE"] = soup.xpath(
+                '//*[@id="__VIEWSTATE"]/@value'
+            )[0]
+            self.data["__EVENTVALIDATION"] = soup.xpath(
+                '//*[@id="__EVENTVALIDATION"]/@value'
+            )[0]
+            next_page = self.request["session"].post(self.url, data=self.data)
+            if convert_date_string(orders[0].xpath(".//td")[5].text_content().strip()) <= self.end_date:
+                pg = 999
+
+            soup = html.fromstring(next_page.text)
+            orders = orders + soup.xpath(self.base_path)
+            pg = pg + 1
+
+            if convert_date_string(orders[-1].xpath(".//td")[5].text_content().strip()) < self.start_date:
+                pg = 999
+
+        return [x for x in orders if self.end_date >= convert_date_string(x.xpath(".//td")[5].text_content().strip()) >= self.start_date]
 
     def _get_case_names(self):
-        path = "{base}/preceding::td[1]".format(base=self.base_path)
-        case_names = []
-        for e in self.html.xpath(path):
-            case_names.append(
-                tostring(e, method="text", encoding="unicode").strip()
-            )
-        return case_names
+        return [
+            row.xpath(".//td")[0].text_content().strip() for row in self.html
+        ]
 
     def _get_download_urls(self):
-        path = "{base}/preceding::td[1]//a[1]/@href".format(
-            base=self.base_path
-        )
-        return list(self.html.xpath(path))
+        return [row.xpath(".//a/@href")[0] for row in self.html]
 
     def _get_docket_numbers(self):
-        path = "{base}//text()".format(base=self.base_path)
-        return list(self.html.xpath(path))
+        return [
+            row.xpath(".//td")[1].text_content().strip() for row in self.html
+        ]
 
     def _get_summaries(self):
-        path = "{base}/following::td[1]//text()".format(base=self.base_path)
-        return list(self.html.xpath(path))
+        return [
+            row.xpath(".//td")[2].text_content().strip() for row in self.html
+        ]
 
     def _get_case_dates(self):
-        path = "{base}/following::td[4]//text()".format(base=self.base_path)
-        dates = []
-        for s in self.html.xpath(path):
-            dates.append(
-                datetime.strptime(clean_if_py3(s).strip(), "%m/%d/%Y").date()
-            )
-        return dates
+        return [
+            convert_date_string(row.xpath(".//td")[5].text_content().strip())
+            for row in self.html
+        ]
 
     def _get_neutral_citations(self):
-        path = "{base}/following::td[6]//text()".format(base=self.base_path)
-        return [s.replace("-", " ") for s in self.html.xpath(path)]
+        return [
+            row.xpath(".//td")[7].text_content().strip() for row in self.html
+        ]
 
     def _get_precedential_statuses(self):
         return ["Published"] * len(self.case_names)
 
     def _get_judges(self):
-        path = "{base}/following::td[2]".format(base=self.base_path)
-        return map(self._return_judge, self.html.xpath(path))
+        return [
+            row.xpath(".//td")[3].text_content().strip() for row in self.html
+        ]
 
-    @staticmethod
-    def _return_judge(e):
-        txt = e.xpath(".//text()")
-        if txt:
-            return txt[0]
-        else:
-            return ""
+    def set_local_variables(self):
+        self.start_date = self.end_date - timedelta(days=self.backwards_days)
+        self.data = {
+            "__VIEWSTATEENCRYPTED": "",
+            "ctl00$MainContent$ddlCourt": self.court_index,
+            "ctl00$MainContent$ddlCounty": "0",
+            "ctl00$MainContent$ddlDecidedYearMin": self.end_date.year,
+            "ctl00$MainContent$ddlDecidedYearMax": self.start_date.year,
+            "ctl00$MainContent$btnSubmit": "Submit",
+            "ctl00$MainContent$ddlRowsPerPage": "200",
+        }
