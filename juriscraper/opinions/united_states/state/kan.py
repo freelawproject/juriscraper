e1713982ea8d0fcdd590f31ccf10475a7849aebc
vvilliam <wpalin@gmail.com>
2020-03-02 23:35:53 -0500

    refactor(kansas): Update for clarity

--- juriscraper/opinions/united_states/state/kan.py
+++ juriscraper/opinions/united_states/state/kan.py
@@ -28,40 +28,33 @@ class Site(OpinionSite):
         self.page = 1
         self.rs = "p$lt$zonePagePlaceholder$pageplaceholder$p$lt$ctl02$OpinionFilter1$filterControl$drp"
         self.ev = "p$lt$zonePagePlaceholder$pageplaceholder$p$lt$ctl06$UniversalPager$pagerElem"
+        self.date_regex = './/td/a[@class="link-pdf"]/ancestor::tr/td[1]'
+        self.row_xp = './/td/a[@class="link-pdf"]/ancestor::tr'
         self.soup = None
         self.rows = []
 
     def _download(self, request_dict={}):
-        r = self.request["seesion"].get(self.url)
-        self.soup = fromstring(r.text)
-
-        date_regex = './/td/a[@class="link-pdf"]/ancestor::tr/td[1]'
         self.go_until_date = self.case_date - timedelta(self.backwards_days)
+        self.make_soup(self.url)
+
         while self.page:
             self.set_local_variables()
-            r = self.request["seesion"].post(self.url, data=self.data)
-            self.soup = fromstring(r.text)
-            for row in self.soup.xpath(
-                './/td/a[@class="link-pdf"]/ancestor::tr'
-            ):
+            self.make_soup(self.url)
+
+            for row in self.soup.xpath(self.row_xp):
                 self.rows.append(row)
-            last_date_str = (
-                self.soup.xpath(date_regex)[-1].text_content().strip()
-            )
-            last_date = datetime.strptime(last_date_str, "%m/%d/%Y")
-            if last_date < self.go_until_date:
+
+            lt_dt = self.soup.xpath(self.date_regex)[-1].text_content().strip()
+            if datetime.strptime(lt_dt, "%m/%d/%Y") < self.go_until_date:
                 break
             self.page = self.page + 1
 
     def _get_case_names(self):
-        return [
-            x.xpath(".//td[3]")[0].text_content().strip() for x in self.rows
-        ]
+        return [x.xpath(".//a/text()")[2] for x in self.rows]
 
     def _get_download_urls(self):
         return [
-            "%s%s"
-            % (self.base_url, x.xpath(".//td[6]/a")[0].get("href").strip())
+            "%s%s" % (self.base_url, x.xpath(".//td[6]/a/@href")[0].strip())
             for x in self.rows
         ]
 
@@ -89,3 +82,10 @@ class Site(OpinionSite):
             "%sCourt" % self.rs: self.court,
             "%sPublished" % self.rs: self.status,
         }
+
+    def make_soup(self, process_url):
+        if self.data is not None:
+            r = self.request["session"].get(process_url)
+        else:
+            r = self.request["session"].post(process_url, data=self.data)
+        self.soup = fromstring(r.text)
