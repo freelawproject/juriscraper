7b3f5cf0ce9628037c22190db54711c99c38ff13
vvilliam <wpalin@gmail.com>
2020-03-03 11:10:40 -0500

    refactor(kansas): Update Kansas pattern

--- juriscraper/opinions/united_states/state/kan.py
+++ juriscraper/opinions/united_states/state/kan.py
@@ -23,29 +23,24 @@ class Site(OpinionSite):
         self.backwards_days = 14
         self.court = "Select Court"  # Supreme Court, Court of Appeals / BOTH
         self.status = "Select Status"  # Published, Unpublished / BOTH
-
-        self.data = {}
-        self.page = 1
-        self.rs = "p$lt$zonePagePlaceholder$pageplaceholder$p$lt$ctl02$OpinionFilter1$filterControl$drp"
-        self.ev = "p$lt$zonePagePlaceholder$pageplaceholder$p$lt$ctl06$UniversalPager$pagerElem"
-        self.date_regex = './/td/a[@class="link-pdf"]/ancestor::tr/td[1]'
-        self.row_xp = './/td/a[@class="link-pdf"]/ancestor::tr'
         self.soup = None
-        self.rows = []
+        self.data = None
 
     def _download(self, request_dict={}):
-        self.go_until_date = self.case_date - timedelta(self.backwards_days)
+        # Set our local variables
+        self.set_local_variables()
+
+        # Load the homepage
         self.make_soup(self.url)
 
+        # Iterate over the pages until we reach our go until date
         while self.page:
-            self.update_data()
+            self.set_data()
             self.make_soup(self.url)
+            self.rows = self.soup.xpath(self.row_xp)
 
-            for row in self.soup.xpath(self.row_xp):
-                self.rows.append(row)
-
-            lt_dt = self.soup.xpath(self.date_regex)[-1].text_content().strip()
-            if datetime.strptime(lt_dt, "%m/%d/%Y") < self.go_until_date:
+            last_dt = self.soup.xpath(self.date_xp)[-1].text_content().strip()
+            if datetime.strptime(last_dt, "%m/%d/%Y") < self.go_until_date:
                 break
             self.page = self.page + 1
 
@@ -54,38 +49,51 @@ class Site(OpinionSite):
 
     def _get_download_urls(self):
         return [
-            "%s%s" % (self.base_url, x.xpath(".//td[6]/a/@href")[0].strip())
-            for x in self.rows
+            "%s%s" % (self.base_url, row.xpath(".//td[6]/a/@href")[0].strip())
+            for row in self.rows
         ]
 
     def _get_case_dates(self):
         return [
-            convert_date_string(x.xpath(".//td[1]")[0].text_content().strip())
-            for x in self.rows
+            convert_date_string(
+                row.xpath(".//td[1]")[0].text_content().strip()
+            )
+            for row in self.rows
         ]
 
     def _get_precedential_statuses(self):
         return [
-            x.xpath(".//td[5]")[0].text_content().strip() for x in self.rows
+            row.xpath(".//td[5]")[0].text_content().strip()
+            for row in self.rows
         ]
 
     def _get_docket_numbers(self):
         return [
-            x.xpath(".//td[2]")[0].text_content().strip() for x in self.rows
+            row.xpath(".//td[2]")[0].text_content().strip()
+            for row in self.rows
         ]
 
-    def update_data(self):
+    def set_data(self):
         self.data = {
-            "__EVENTTARGET": self.ev,
-            "__EVENTARGUMENT": str(self.page),
-            "__VIEWSTATE": self.soup.xpath('//*[@id="__VIEWSTATE"]/@value')[0],
+            "__EVENTTARGET": self.event_target_key,
+            "__EVENTARGUMENT": self.page,
+            "__VIEWSTATE": self.soup.xpath(self.view_state_xp)[0],
             "%sCourt" % self.rs: self.court,
             "%sPublished" % self.rs: self.status,
         }
 
     def make_soup(self, process_url):
-        if self.data is not None:
+        if self.data is None:
             r = self.request["session"].get(process_url)
         else:
             r = self.request["session"].post(process_url, data=self.data)
         self.soup = fromstring(r.text)
+
+    def set_local_variables(self):
+        self.go_until_date = self.case_date - timedelta(self.backwards_days)
+        self.page = 1
+        self.rs = "p$lt$zonePagePlaceholder$pageplaceholder$p$lt$ctl02$OpinionFilter1$filterControl$drp"
+        self.event_target_key = "p$lt$zonePagePlaceholder$pageplaceholder$p$lt$ctl06$UniversalPager$pagerElem"
+        self.date_xp = './/td/a[@class="link-pdf"]/ancestor::tr/td[1]'
+        self.row_xp = './/td/a[@class="link-pdf"]/ancestor::tr'
+        self.view_state_xp = '//*[@id="__VIEWSTATE"]/@value'
