0f37f85c6b1e4711b3a005bacddc6e851c5b00a2
vvilliam <wpalin@gmail.com>
2020-08-10 15:43:42 -0400

    test(AppellateAttachmentPage): Init PacerParseAppellateAttachmentPageTest
    Add PacerParseAppellateAttachmentPageTest calls

---
+++ tests/local/test_PacerParseAppellateAttachementPageTest.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+
+import os
+
+from juriscraper.pacer import AppellateAttachmentPage
+from tests import TESTS_ROOT_EXAMPLES_PACER
+from tests.local.PacerParseTestCase import PacerParseTestCase
+
+
+class PacerParseAppellateAttachmentPageTest(PacerParseTestCase):
+    def setUp(self):
+        self.maxDiff = 200000
+
+    def test_parsing_appellate_attachment_pages(self):
+        path_root = os.path.join(TESTS_ROOT_EXAMPLES_PACER,
+                                 "appellate_attachment_pages")
+
+        self.parse_files(path_root, "ca*.html", AppellateAttachmentPage)
