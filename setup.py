9c5483a0d9d12e4ebb3ca49239f51526b40e06f3
Philip Ardery <arderyp@protonmail.com>
2020-11-07 12:24:25 -0800

    bumping version minor

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import unittest
 from setuptools import setup, find_packages
 from setuptools.command.install import install
 
-VERSION = "2.1.0"
+VERSION = "2.1.1"
 AUTHOR = "Free Law Project"
 EMAIL = "info@free.law"
 HERE = os.path.abspath(os.path.dirname(__file__))
