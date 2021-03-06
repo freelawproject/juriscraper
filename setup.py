527350db9f9bfbd28ddaf3ac0210bbcb9ddffbc6
Michael Lissner <mlissner@michaeljaylissner.com>
2021-03-05 14:37:18 -1000

    Version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import unittest
 from setuptools import setup, find_packages
 from setuptools.command.install import install
 
-VERSION = "2.3.5"
+VERSION = "2.3.6"
 AUTHOR = "Free Law Project"
 EMAIL = "info@free.law"
 HERE = os.path.abspath(os.path.dirname(__file__))
