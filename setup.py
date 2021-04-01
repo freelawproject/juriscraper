132bb067534ce9bf6a173326dbd4993fa3c9cd9b
vvilliam <wpalin@gmail.com>
2021-04-01 11:04:55 -0400

    Version bump v2.3.8

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import unittest
 from setuptools import setup, find_packages
 from setuptools.command.install import install
 
-VERSION = "2.3.7"
+VERSION = "2.3.8"
 AUTHOR = "Free Law Project"
 EMAIL = "info@free.law"
 HERE = os.path.abspath(os.path.dirname(__file__))
