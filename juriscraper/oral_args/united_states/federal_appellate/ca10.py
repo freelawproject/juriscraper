1c395444c24847751c3bd5fe893561da22175aaa
Philip Ardery <arderyp@protonmail.com>
2021-09-18 12:02:25 -0700

    (fix:oralargs/ca10) handing new table layout; removing stale example files and creating new example file

--- juriscraper/oral_args/united_states/federal_appellate/ca10.py
+++ juriscraper/oral_args/united_states/federal_appellate/ca10.py
@@ -3,6 +3,7 @@ CourtID: ca10
 Author: mlr
 """
 
+import datetime
 from juriscraper.OralArgumentSite import OralArgumentSite
 from juriscraper.lib.string_utils import convert_date_string
 from juriscraper.lib.html_utils import (
@@ -15,18 +16,24 @@ class Site(OralArgumentSite):
     def __init__(self, *args, **kwargs):
         super(Site, self).__init__(*args, **kwargs)
         self.court_id = self.__module__
+        # query for results of current month
+        now = datetime.datetime.now()
+        start = "%d/01/%d" % (now.month, now.year)
+        end = "%d/%d/%d" % (now.month, now.day, now.year)
         self.url = (
-            "https://www.ca10.uscourts.gov/clerk/oral-argument-recordings"
-        )
+            "https://www.ca10.uscourts.gov/oralargument/search-results?"
+            "field_oa_hearing_date_value[min][date]=%s&"
+            "field_oa_hearing_date_value[max][date]=%s"
+        ) % (start, end)
 
     def _get_download_urls(self):
-        return get_table_column_links(self.html, 5)
+        return get_table_column_links(self.html, 6)
 
     def _get_case_names(self):
-        return get_table_column_text(self.html, 2)
+        return get_table_column_text(self.html, 3)
 
     def _get_docket_numbers(self):
-        dockets = get_table_column_text(self.html, 1)
+        dockets = get_table_column_text(self.html, 2)
         # Handle case where "no results" message is put in table cell, see: ca10_example_2.html
         return (
             []
@@ -37,5 +44,5 @@ class Site(OralArgumentSite):
     def _get_case_dates(self):
         return [
             convert_date_string(ds)
-            for ds in get_table_column_text(self.html, 3)
+            for ds in get_table_column_text(self.html, 4)
         ]
