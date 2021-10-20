8c225b944ade6dc8c3496c08e878221bb16f2b73
Grecia Cortes <grecia@freelawproject.org>
2021-10-20 12:27:28 -0500

    Changed court URL to RSS feed

--- juriscraper/opinions/united_states/state/pasuperct.py
+++ juriscraper/opinions/united_states/state/pasuperct.py
@@ -1,19 +1,20 @@
-#  Scraper for Pennsylvania Supreme Court
-# CourtID: pasup
-# Court Short Name: pasup
-# Author: Andrei Chelaru
-# Reviewer: mlr
-# Date created: 21 July 2014
-
+# -*- coding: utf-8 -*-
+"""
+Scraper for Pennsylvania Superior Court
+CourtID: pasup
+Court Short Name: pasup
+Author: Andrei Chelaru
+Reviewer: mlr
+Date created: 21 July 2014
+"""
 from juriscraper.opinions.united_states.state import pa
-import re
 
 
 class Site(pa.Site):
     def __init__(self, *args, **kwargs):
         super(Site, self).__init__(*args, **kwargs)
         self.court_id = self.__module__
-        self.url = "http://www.pacourts.us/assets/rss/SuperiorOpinionsRss.ashx"
+        self.url = "https://www.pacourts.us/Rss/Opinions/Superior/"
 
     def _get_judges(self):
         # Judges for this feed are provided as obscure numbers.
