22d009d63facc778f80560ace151d35209f8ece5
Grecia Cortes <grecia@freelawproject.org>
2021-10-26 11:43:54 -0500

    Added fixed opinion scraper and example for Illinois Appellate Courts correction for Illinois Supreme Court and example

--- juriscraper/opinions/united_states/state/ill.py
+++ juriscraper/opinions/united_states/state/ill.py
@@ -9,6 +9,7 @@ Contact: webmaster@illinoiscourts.gov, 217-558-4490, 312-793-3250
 """
 
 from juriscraper.OpinionSiteLinear import OpinionSiteLinear
+from juriscraper.AbstractSite import logger
 from juriscraper.lib.html_utils import (
     get_row_column_links,
     get_row_column_text,
@@ -29,14 +30,20 @@ class Site(OpinionSiteLinear):
         for row in self.html.xpath("//table[@id='ctl04_gvDecisions']//tr"):
             cells = row.xpath(".//td")
             if len(cells) == 7:
-                status = get_row_column_text(row, 6)
-                if status == "NRel":
-                    status = "Unpublished"
-                else:
-                    status = "Published"
-                name = get_row_column_text(row, 1)
-                citation = get_row_column_text(row, 2)
-                redirect_url = get_row_column_links(row, 1)
+                try:
+                    status = get_row_column_text(row, 6)
+                    # NRel = Not Published
+                    if status == "NRel":
+                        status = "Unpublished"
+                    else:
+                        status = "Published"
+                    name = get_row_column_text(row, 1)
+                    citation = get_row_column_text(row, 2)
+                    redirect_url = get_row_column_links(row, 1)
+                except IndexError:
+                    # If the opinion file's information is missing, skip record
+                    continue
+
                 url = self.set_file_url(redirect_url, name, citation)
                 docket = self.extract_docket(citation)
                 self.cases.append(
@@ -53,11 +60,18 @@ class Site(OpinionSiteLinear):
     def set_file_url(self, raw_url, case_name, case_citation):
         # Opinions' URL redirect to the real PDF files' URL, this redirection
         # might cause problems when downloading files in CourtListener
-        case_uuid = raw_url.lstrip(
-            "https://www.illinoiscourts.gov/resources/"
-        ).rstrip("/file")
-        file_name = quote(f"{case_name}, {case_citation}.pdf")
-        return f"https://ilcourtsaudio.blob.core.windows.net/antilles-resources/resources/{case_uuid}/{file_name}"
+        search = re.search(
+            r"(?:.*\/resources\/)([0-9A-Za-z]{8}-[0-9A-Za-z]{4}-4[0-9A-Za-z]{3}-[89ABab][0-9A-Za-z]{3}-[0-9A-Za-z]{12})(?:\/file)",
+            raw_url,
+        )
+        if search:
+            case_uuid = search.group(1)
+            file_name = quote(f"{case_name}, {case_citation}.pdf")
+            url = f"https://ilcourtsaudio.blob.core.windows.net/antilles-resources/resources/{case_uuid}/{file_name}"
+        else:
+            url = ""
+            logger.warning(f"Case UUID not found: {raw_url}")
+        return url
 
     def extract_docket(self, case_citation):
         # RegEx: "{YYYY: year_4_digit} IL {docket_number}"
@@ -66,4 +80,5 @@ class Site(OpinionSiteLinear):
             docket = search.group(1)
         else:
             docket = ""
+            logger.warning(f"Docket not found: {case_citation}")
         return docket
--- juriscraper/opinions/united_states/state/illappct.py
+++ juriscraper/opinions/united_states/state/illappct.py
@@ -1,12 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
 # Author: Krist Jin
 # 2013-08-18: Created.
 # 2014-07-17: Updated by mlr to remedy InsanityException.
+"""
 
-from lxml import html
-
-from juriscraper.OpinionSite import OpinionSite
-from juriscraper.lib.string_utils import convert_date_string
 from juriscraper.opinions.united_states.state import ill
+from juriscraper.AbstractSite import logger
+import re
 
 
 class Site(ill.Site):
@@ -14,5 +15,18 @@ class Site(ill.Site):
         super(Site, self).__init__(*args, **kwargs)
         self.court_id = self.__module__
         self.url = (
-            "http://www.illinoiscourts.gov/Opinions/recent_appellate.asp"
+            "https://www.illinoiscourts.gov/top-level-opinions?type=appellate"
         )
+
+    def extract_docket(self, case_citation):
+        # RegEx: "[{YYYY: year_4_digit}|] IL [App|] ({d: appellate district}[st|d|th]) {docket_number}{-U: opcional}"
+        search = re.search(r"(?:.+\()(\d+)(?:.+\)\s+)(\d+)", case_citation)
+        if search:
+            district = search.group(1)
+            docket = search.group(2)
+            docket = f"{district}-{docket[0:2]}-{docket[2:]}"
+        else:
+            district = ""
+            docket = ""
+            logger.warning(f"Docket not found: {case_citation}")
+        return docket
