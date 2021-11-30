19059462b8dbb474f14fb99d7b7dea53cb7558d9
William Palin <bill@free.law>
2021-11-29 20:07:06 -0500

    refactor(cafc): Minor changes to CAFC for clarity.
    Update doc strings
    Check for PDF urls and simplify xpath
    Remove unused imports

--- juriscraper/opinions/united_states/federal_appellate/cafc.py
+++ juriscraper/opinions/united_states/federal_appellate/cafc.py
@@ -1,7 +1,4 @@
-from urllib.parse import urlparse
-
-from juriscraper.AbstractSite import logger
-from juriscraper.lib.string_utils import convert_date_string, titlecase
+from juriscraper.lib.string_utils import titlecase
 from juriscraper.OpinionSiteLinear import OpinionSiteLinear
 
 
@@ -11,34 +8,28 @@ class Site(OpinionSiteLinear):
         self.url = "https://cafc.uscourts.gov/category/opinion-order/feed/"
         self.court_id = self.__module__
 
-    def _process_html(self):
-        """Process HTML
-        Iterate over each item in the RSS feed
+    def _process_html(self) -> None:
+        """Process the RSS feed.
 
+        Iterate over each item in the RSS feed to extract out
+        the date, case name, docket number, and status and pdf URL.
         Return: None
         """
         for item in self.html.xpath("//item"):
-            # Expected <title> content:
-            # <title>21-1358: LEVARIO v. MCDONOUGH [OPINION], Nonprecedential</title>
             title = item.xpath("./title/text()")[0]
-            # Expected <content:encoded> content:
-            # <content:encoded><![CDATA[<p>Opinions/Orders posted: </p><p><a href="/opinions-orders/21-1358.opinion.10-12-2021_1847104.pdf">LEVARIO v. MCDONOUGH [OPINION](pdf)</a> <br />Appeal Number: 21-1358 <br />Origin: CAVC <br />Nonprecedential </p><p>To see more opinions and orders, follow this link: <a href="/home/case-information/opinions-orders/">Opinions and Orders</a>.</p>]]></content:encoded>
             p_element = item.xpath(
                 "./encoded",
                 namespaces={
                     "content": "http://purl.org/rss/1.0/modules/content/"
                 },
             )[0]
-            urls_raw = p_element.xpath("./p/a/@href")
-            for url_raw in urls_raw:
-                parsed = urlparse(url_raw)
-                if parsed.path.lower().endswith(".pdf"):
-                    url = url_raw
-                    break
-            # Expected <pubDate> content:
-            # <pubDate>Tue, 12 Oct 2021 15:00:23 +0000</pubDate>
-            pubdate = item.xpath("./pubdate/text()")[0]
 
+            # Check for the existence of a link to the PDF, otherwise skip.
+            urls = p_element.xpath("//a[contains(@href, '.pdf')]/@href")
+            if not urls:
+                continue
+
+            pubdate = item.xpath("./pubdate/text()")[0]
             docket = title.split(":")[0].strip()
             title = title.split(":")[1].strip()
             name = titlecase(title.split("[")[0].strip())
@@ -55,7 +46,7 @@ class Site(OpinionSiteLinear):
                 {
                     "date": pubdate,
                     "docket": docket,
-                    "url": url,
+                    "url": urls[0],
                     "name": name,
                     "status": status,
                 }
