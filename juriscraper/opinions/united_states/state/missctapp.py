8913fd0a89bd7450551f6fc753752b1382e79581
Grecia Cortes <grecia@freelawproject.org>
2021-12-16 15:09:59 -0600

    Updated courts param name to get published opinions dates

--- juriscraper/opinions/united_states/state/missctapp.py
+++ juriscraper/opinions/united_states/state/missctapp.py
@@ -1,11 +1,16 @@
 """Scraper for Mississippi Court of Appeals
 CourtID: missctapp
 Court Short Name: Miss. Ct. App.
+Author: Jon Andersen
+History:
+    2014-06-21: Created by Jon Andersen
+    2021-12-16: Court param name changed
 """
 
 from juriscraper.opinions.united_states.state import miss
 
 
+# Landing page: https://courts.ms.gov/appellatecourts/coa/coadecisions.php
 class Site(miss.Site):
-    def get_court_parameter(self):
+    def get_court_parameter(self) -> str:
         return "COA"
