09a5a2c8efc4304309eee0dd18cf273caea08984
William Palin <bill@free.law>
2021-12-19 09:56:45 -0500

    refactor(doj_bia): Updated for Opinion site linear

--- juriscraper/opinions/united_states/administrative_agency/doj_bia.py
+++ juriscraper/opinions/united_states/administrative_agency/doj_bia.py
@@ -5,17 +5,15 @@ Author: William Palin
 Reviewer:
 Type:
 History:
-    2021-12-18: Created by William Palin, Esq.
+    2021-12-18: Created by William E. Palin
 """
 
 import re
-from datetime import datetime
-from typing import List
 
-from juriscraper.OpinionSite import OpinionSite
+from juriscraper.OpinionSiteLinear import OpinionSiteLinear
 
 
-class Site(OpinionSite):
+class Site(OpinionSiteLinear):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.court_id = self.__module__
@@ -27,47 +25,29 @@ class Site(OpinionSite):
         self.url = html.xpath(".//table//tbody/tr/td/a/@href")[0]
         return self._get_html_tree_by_url(self.url)
 
-    def _get_case_dates(self):
-        """Get the case dates for the current page"""
-        dates = []
-        cites = self.html.xpath("//article")[0].xpath(
-            ".//strong[1]/./../text() | .//b[1]/./../text()"
+    def _process_html(self):
+        tables = self.html.xpath(
+            ".//table/following-sibling::p/preceding-sibling::table"
         )
-        cites = [c.strip(" ,") for c in cites if c.strip(" ,")]
-        for cite in cites:
+        for table in tables:
+            bold_text = table.xpath(".//strong | .//b")
+            if not bold_text:
+                continue
+            bold_text = bold_text[0]
+            cite = " ".join(
+                bold_text.xpath(".//following-sibling::text()")
+            ).strip(" ,")
             year = re.findall("\d{4}", cite)[0]
-            dates.append(datetime.strptime(f"{year}-07-01", "%Y-%m-%d").date())
-        return dates
-
-    def _get_case_names(self):
-        strong = self.html.xpath("//article")[0].xpath(
-            ".//strong[1]/text() | .//b[1]/text()"
-        )
-        return strong
-
-    def _get_docket_numbers(self) -> List[str]:
-        """"""
-        return self.html.xpath("//article")[0].xpath(
-            ".//td[@class='rteright']/a[1]/text()"
-        )
-
-    def _get_precedential_statuses(self):
-        """"""
-        return ["Unpublished"] * len(self.download_urls)
-
-    def _get_download_urls(self):
-        """"""
-        return self.html.xpath("//article")[0].xpath(
-            ".//td[@class='rteright']/a[1]/@href"
-        )
-
-    def _get_west_citations(self):
-        """"""
-        cites = self.html.xpath("//article")[0].xpath(
-            ".//strong[1]/./../text() | .//b[1]/./../text()"
-        )
-        cites = [c.strip(" ,") for c in cites if c.strip(" ,")]
-        return cites
-
-    def _get_date_filed_is_approximate(self):
-        return [True] * len(self.download_urls)
+            docket = table.xpath(".//a")[0]
+            name = bold_text.xpath(".//text()")[0]
+            self.cases.append(
+                {
+                    "name": name,
+                    "cite": cite,
+                    "year": year,
+                    "docket": docket.text_content(),
+                    "url": docket.get("href"),
+                    "status": "Unpublished",
+                    "date": f"{year}-07-01",
+                }
+            )
