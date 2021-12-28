9d057286708396009143d56658fb8c91491262dc
William Palin <bill@free.law>
2021-12-28 13:58:58 -0500

    tests(ohio): Fix Ohio tests
    The tests were stripping out the hyphen in
    XXXX-OHIO-XXXX which is the proper format.  That has been fixed
    
    Also, the tests were not working for ohio app ct 1-12.

--- juriscraper/opinions/united_states/state/ohioctapp_6.py
+++ juriscraper/opinions/united_states/state/ohioctapp_6.py
@@ -1,6 +1,6 @@
-"""Scraper for the 1st District Court of Appeals
-CourtID: ohio
-Court Short Name: Ohio
+"""Scraper for the 6th District Court of Appeals
+CourtID: ohioctap_6
+Court Short Name: Ohio Dist Ct App 6
 Author: Andrei Chelaru
 """
 
