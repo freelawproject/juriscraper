e28a0fcd73ee7f933991715967a67677ec61f620
William Palin <bill@free.law>
2021-12-28 09:19:34 -0500

    refactor(Ga): Refactor georgia
    Drop old tests that are no longer applicable
    Drop duplicated GA HTML tests
    Update scraper to parse names better
    Sped up scraper to scrape only the last month

--- juriscraper/opinions/united_states/state/ga.py
+++ juriscraper/opinions/united_states/state/ga.py
@@ -3,10 +3,9 @@
 # Court Short Name: ga
 
 import re
-from datetime import date
+from datetime import date, datetime, timedelta
 
 from juriscraper.AbstractSite import logger
-from juriscraper.lib.exceptions import InsanityException
 from juriscraper.lib.string_utils import titlecase
 from juriscraper.OpinionSiteLinear import OpinionSiteLinear
 
@@ -15,69 +14,59 @@ class Site(OpinionSiteLinear):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.court_id = self.__module__
-        self.url = self._get_url(date.today().year)
+        today = date.today()
+        self.last_month = today - timedelta(days=31)
+        self.url = self._get_url(today.year)
         self.status = "Published"
         self.back_scrape_iterable = range(2016, 2022)
-        self.regex_docket = re.compile(r"(S?[0-9]{2}[A-Z]{1}[0-9]{4})")
 
     def _get_url(self, year: int) -> str:
-        return "https://www.gasupreme.us/opinions/%d-opinions/" % year
+        """Generate the GA URL for a given year.
 
-    def _process_html(self):
-        paths = "//p/strong | //p/b | //p/font/strong | //p/font/b"
-        for date_element in self.html.xpath(paths):
-            string = date_element.xpath("./text()")
-            try:
-                string = string[0]
-                # handle examples where time but no date (ga_example_3.html)
-                if ":" in string and ("AM" in string or "PM" in string):
-                    continue
-                # handle legacy example (ga_example.html)
-                string = string.split("SUMMARIES")[0]
-                date_string = re.sub(r"\W+", " ", string)
-                # handle legacy example (ga_example.html)
-                if len(date_string.split()) != 3:
-                    continue
-            except:
+        :param year: Year to scrape
+        :return: URL for the given year
+        """
+        return f"https://www.gasupreme.us/opinions/{year}-opinions/"
+
+    def _process_html(self) -> None:
+        for link in self.html.xpath("//li/a[contains(@href, '.pdf')]"):
+            url = link.get("href")
+            title = link.text_content()
+            if not title:
                 continue
-            parent = date_element.xpath("./..")[0]
-            # handle legacy example (ga_example.html)
-            while parent.tag != "p":
-                parent = parent.xpath("./..")[0]
-            for item in parent.getnext().xpath("./li"):
-                text = item.text_content()
-                if text:
-                    # Extract Docket numbers
-                    dockets = re.findall(self.regex_docket, text)
-                    if not dockets:
-                        if (
-                            text
-                            == "IN RE: MOTION TO AMEND 2021-3 (Bar Rule 1.2)"
-                        ):
-                            continue
-                        else:
-                            raise InsanityException(
-                                f"Could not find docket numbers in: '{text}'"
-                            )
+            dockets = re.findall(r"S?\d{2}\w\d{4}", title)
+            if not dockets:
+                # Skip empty links in the HTML
+                continue
+            docket = ", ".join(dockets)
+            name = title.split(dockets[-1])[-1].strip("., ")
+            summary = link.xpath(
+                ".//parent::li/parent::ul/preceding-sibling::p[1]"
+            )[0].text_content()
+            # We've got en/em dash and hyphens.
+            date_str = (
+                summary.split("–")[0].split("—")[0].split("-")[0].strip()
+            )
+            date_summary = datetime.strptime(date_str, "%B %d, %Y")
+            if self.test_mode_enabled():
+                self.last_month = date(year=2019, month=11, day=1)
 
-                    # Extract name substring; I am sure this could
-                    # be done with a more slick regex, but its not
-                    # my forte...
-                    name = text
-                    for docket in dockets:
-                        name = name.replace(docket, "")
-                    name = name.lstrip(" .,")
+            if date_summary.date() < self.last_month:
+                # GA lists all opinions from the previous year on the same page
+                # but we don't need to scrape them every time.  So cancel out
+                # after 31 days.
+                # This should resolve the speed warning we receive on GA.
+                break
+            self.cases.append(
+                {
+                    "date": date_str,
+                    "docket": docket,
+                    "name": titlecase(name),
+                    "url": url,
+                }
+            )
 
-                    self.cases.append(
-                        {
-                            "date": date_string,
-                            "docket": ", ".join(dockets),
-                            "name": titlecase(name.lstrip(" .,")),
-                            "url": item.xpath(".//a[1]/@href")[0],
-                        }
-                    )
-
-    def _download_backwards(self, year):
+    def _download_backwards(self, year) -> None:
         self.url = self._get_url(year)
         logger.info(f"Backscraping for year {year}: {self.url}")
         self.html = self._download()
