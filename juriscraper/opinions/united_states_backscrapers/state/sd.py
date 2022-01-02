7c44433d319ec470f97a100bc13f482ed8d4f7bb
William Palin <bill@free.law>
2022-01-01 20:28:14 -0500

    fix(sd): Make backscraper awesome for SD

--- juriscraper/opinions/united_states_backscrapers/state/sd.py
+++ juriscraper/opinions/united_states_backscrapers/state/sd.py
@@ -1,9 +1,16 @@
 # Author: Michael Lissner
-# Date created: 2013-06-11
+# History:
+# - 2013-06-11: Birth.
+# - 2013-08-06: Revised by Brian Carver
+# - 2014-08-05: Updated URL by mlr
+# - 2022-01-01: Updated by flooie
 
-from lxml.html import fromstring
+import datetime
+import re
+from typing import Any, Dict
 
 from juriscraper.lib.html_utils import get_row_column_text
+from juriscraper.lib.string_utils import titlecase
 from juriscraper.OpinionSiteLinear import OpinionSiteLinear
 
 
@@ -13,48 +20,108 @@ class Site(OpinionSiteLinear):
         self.court_id = self.__module__
         self.status = "Published"
         self.url = "https://ujs.sd.gov/Supreme_Court/opinions.aspx"
+        self.years = range(2010, datetime.date.today().year)
 
-    def _set_parameters(self):
+    def _set_page_year(
+        self,
+        year: int,
+        view_state: str,
+        event_validation: str,
+    ) -> None:
+        a = self.html.xpath(
+            f".//a[contains(@id,'ContentPlaceHolder1_ChildContent1_Repeater_OpinionsYear_LinkButton1')][contains(text(), '{year}')]"
+        )
+        link = a[0].get("href").split("'")[1]
+        data = {
+            "__VIEWSTATE": view_state,
+            "__EVENTVALIDATION": event_validation,
+            "__EVENTTARGET": link,
+        }
+        self.parameters = data
         self.method = "POST"
-        self.parameters = {
+
+    def _set_page_next(
+        self,
+        view_state: str,
+        event_validation: str,
+    ) -> None:
+        data = {
             "ctl00$ctl00$ScriptManager1": "ctl00$ctl00$ContentPlaceHolder1$ChildContent1$UpdatePanel_Opinions|ctl00$ctl00$ContentPlaceHolder1$ChildContent1$LinkButton_Next",
             "__EVENTTARGET": "ctl00$ctl00$ContentPlaceHolder1$ChildContent1$LinkButton_Next",
+            "__VIEWSTATE": view_state,
+            "__EVENTVALIDATION": event_validation,
         }
-        self.parameters["__VIEWSTATE"] = (
-            self.html.xpath("//*[@id='__VIEWSTATE']/@value")[0],
-        )
-        self.parameters["__EVENTVALIDATION"] = (
-            self.html.xpath("//*[@id='__EVENTVALIDATION']/@value")[0],
-        )
-
-    def _download(self):
-        response = self.request["session"].post(self.url, data=self.parameters)
-        return fromstring(response.text)
+        self.parameters = data
+        self.method = "POST"
 
     def _process_html(self):
-        self._set_parameters()
-        rows = self.html.xpath(
-            "//div[@id='ContentPlaceHolder1_ChildContent1_UpdatePanel_Opinions']//tbody/tr"
-        )
-        for row in rows:
-            self.cases.append(
-                {
-                    "date": get_row_column_text(row, 1),
-                    "name": get_row_column_text(row, 2).rsplit(",", 1)[0],
-                    "citation": get_row_column_text(row, 2).rsplit(
-                        ",", 1
-                    )[1],
-                    "url": row.xpath(".//td[2]/a/@href")[0],
-                    "docket": row.xpath(".//td[2]/a/@href")[0].split("/")[-1][
-                        :5
-                    ],
-                }
+        for year in self.years:
+            view_state = self.html.xpath("//input[@id='__VIEWSTATE']")[0].get(
+                "value"
             )
+            event_validation = self.html.xpath(
+                "//input[@id='__EVENTVALIDATION']"
+            )[0].get("value")
 
-        # Continue the loop
-        if get_row_column_text(rows[-1], 3) != "1":
-            try:
+            self._set_page_year(year, view_state, event_validation)
+            self.html = super()._download()
+            while True:
+
+                rows = self.html.xpath(
+                    "//div[@id='ContentPlaceHolder1_ChildContent1_UpdatePanel_Opinions']//tbody/tr"
+                )
+                for row in rows:
+                    cite = re.findall(
+                        r"\d{4} S\.D\. \d+", get_row_column_text(row, 2)
+                    )
+                    if not cite:
+                        continue
+
+                    # https://ujs.sd.gov/uploads/sc/opinions/2928369ef9a6.pdf
+                    # We abstract out the first part of the docket number here
+                    # And process the full docket number in the `extract_from_text` method
+                    # Called after the file has been downloaded.
+                    docket = row.xpath(".//td[2]/a/@href")[0].split("/")[-1][
+                        :5
+                    ]
+                    self.cases.append(
+                        {
+                            "date": get_row_column_text(row, 1),
+                            "name": titlecase(
+                                get_row_column_text(row, 2).rsplit(",", 1)[0]
+                            ),
+                            "neutral_citation": cite[0],
+                            "url": row.xpath(".//td[2]/a/@href")[0],
+                            "docket": docket,
+                        }
+                    )
+                page_count = self.html.xpath(
+                    "//span[@id='ContentPlaceHolder1_ChildContent1_Label_Page']"
+                )[0].text_content()
+                page_of = re.findall("Page (\d+) of (\d+)", page_count)
+                if len(set(page_of[0])) == 1:
+                    break
+
+                vs = self.html.xpath("//input[@id='__VIEWSTATE']")[0].get(
+                    "value"
+                )
+                ev = self.html.xpath("//input[@id='__EVENTVALIDATION']")[
+                    0
+                ].get("value")
+
+                self._set_page_next(view_state=vs, event_validation=ev)
                 self.html = super()._download()
-                self._process_html()
-            except Exception as e:
-                pass
+
+    def extract_from_text(self, scraped_text: str) -> Dict[str, Any]:
+        """Can we extract the date filed from the text?
+
+        :param scraped_text: The content of the document downloaded
+        :return: Metadata to be added to the case
+        """
+        docket = re.findall(r"#\d+.*-.-\w{3}", scraped_text)[0][:20]
+        metadata = {
+            "Docket": {
+                "docket_number": docket,
+            },
+        }
+        return metadata
