235ea28f1820e1e86c5e9e008b2c56d84a57660c
William Palin <bill@free.law>
2022-01-06 09:30:48 -0500

    refactor(kan_p): Unwind failed deferring list.

--- juriscraper/opinions/united_states/state/kan_p.py
+++ juriscraper/opinions/united_states/state/kan_p.py
@@ -1,8 +1,7 @@
-#  Scraper for Kansas Supreme Court
+# Scraper for Kansas Supreme Court
 # CourtID: kan_p
-from typing import List
 
-from juriscraper.DeferringList import DeferringList
+
 from juriscraper.lib.html_utils import get_row_column_text
 from juriscraper.OpinionSiteLinear import OpinionSiteLinear
 
@@ -36,21 +35,18 @@ class Site(OpinionSiteLinear):
     def _process_html(self):
         if self.test_mode_enabled():
             self.pages_to_process = 1
+
         for page in range(1, self.pages_to_process + 1):
             if not self.test_mode_enabled():
-                pass
+                viewstate = self.html.xpath("//input[@id='__VIEWSTATE']")[
+                    0
+                ].get("value")
+                self._set_parameters(page, viewstate)
+                self.html = self._download()
 
-    def _get_case_names(self) -> List:
-        def paginate(page):
-            viewstate = self.html.xpath("//input[@id='__VIEWSTATE']")[0].get(
-                "value"
-            )
-            self._set_parameters(page, viewstate)
-            self.html = self._download()
             rows = self.html.xpath("//table[@class='info-table']/tbody/div/tr")
-            cases = []
             for row in rows[1:]:
-                cases.append(
+                self.cases.append(
                     {
                         "date": row.xpath(f".//td[1]")[0].text_content(),
                         "docket": get_row_column_text(row, 2),
@@ -60,9 +56,3 @@ class Site(OpinionSiteLinear):
                         ),
                     }
                 )
-            return cases
-
-        seeds = list(range(1, self.pages_to_process + 1))
-        for def_list in DeferringList(seed=seeds, fetcher=paginate):
-            self.cases.extend(def_list)
-        return [case["name"] for case in self.cases]
