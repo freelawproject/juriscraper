4bd8204bd6823dda8fa4be1fb2f647843aec4c85
William Palin <bill@free.law>
2022-01-27 16:55:40 -0500

    refactor(mich): Update michigan scraper
    Update tests as well.

--- juriscraper/opinions/united_states/state/mich.py
+++ juriscraper/opinions/united_states/state/mich.py
@@ -8,10 +8,9 @@ History:
     - 2022-01-28: Updated for new web site, @satsuki-chan.
 """
 import re
-import time
 from typing import List
+from urllib.parse import urlencode
 
-from juriscraper.AbstractSite import logger
 from juriscraper.lib.string_utils import titlecase
 from juriscraper.OpinionSiteLinear import OpinionSiteLinear
 
@@ -20,58 +19,93 @@ class Site(OpinionSiteLinear):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.court_id = self.__module__
-        self.url = "https://www.courts.michigan.gov/api/CaseSearch/SearchCaseOpinions?resource=supreme-court-opinion&aAppellateCourt=Supreme%20Court&searchQuery=&sortOrder=Newest&page=1&pageSize=100"
-        self.back_scrape_iterable = list(range(1, 99))
-        self.request["headers"] = {"User-Agent": ""}
-        self.title_re = (
-            r"(MSC|COA) (?P<docket>\d+)\s+(?P<name>.+)\s+(Opinion|Order)"
+        self.filters = (
+            ("releaseDate", "Past Year"),
+            ("page", "1"),
+            ("sortOrder", "Newest"),
+            ("searchQuery", ""),
+            ("resultType", "opinions"),
+            ("pageSize", "100"),
         )
+        self.court = "Supreme Court"
+        params = self.filters + (
+            ("resource", "supreme-court-opinion"),
+            ("aAppellateCourt", self.court),
+        )
+        self.url = f"https://www.courts.michigan.gov/api/CaseSearch/SearchCaseOpinions?{urlencode(params)}"
+        # Currently blocked from Michigan so we drop the user agent for now
+        self.request["headers"] = {"User-Agent": ""}
+        self.title_re = r"(MSC|COA) (?P<docket>\d+)\s+(?P<name>.+)\s+Opinion"
         self.status = "Published"
         self.cases = []
 
     def _process_html(self) -> None:
+        """Process the html and extract out the opinions
+
+        :return: None
+        """
         for item in self.html["searchItems"]:
             match = re.search(self.title_re, item["title"])
-            if not match:
-                logger.warning(
-                    f"Opinion '{item['title']}' has no docket and case name."
-                )
-                continue
-            docket = match.group("docket")
-            name = self.get_name(match.group("name"))
+            if match:
+                docket = match.group("docket")
+                name = self.cleanup_case_name(match.group("name"))
+            else:
+                # In some cases the case name is missing. Grab the docket from the title in a different manner
+                docket = item["title"].split("_")[0]
+                name = ""
             url = (
                 f"https://www.courts.michigan.gov{item['documentUrl'].strip()}"
             )
-            date = item["displayDate"]
-            lower_courts = self.get_lower_courts(item["courts"])
-
             self.cases.append(
                 {
-                    "date": date,
+                    "date": item["displayDate"],
                     "docket": docket,
                     "name": name,
                     "url": url,
-                    "lower_courts": lower_courts,
+                    "lower_courts": self.get_lower_courts(item["courts"]),
+                    "title": item["title"],
                 }
             )
 
-    def get_name(self, name_raw: str) -> str:
+    def _get_case_names(self) -> List[str]:
+        """Get case names if missing
+
+        In some cases the case name is missing. This method will query for it.
+
+        :return: List of case names
+        """
+        for case in self.cases:
+            if case["name"] != "":
+                continue
+            if not self.test_mode_enabled:
+                self.url = f"https://www.courts.michigan.gov/api/CaseSearch/SearchCaseSearchContent/?searchQuery={case['title']}"
+                self.html = self._download()
+                case["name"] = self.html["caseDetailResults"]["searchItems"][
+                    0
+                ]["title"].title()
+            else:
+                case["name"] = "Test Name"
+        return [case["name"] for case in self.cases]
+
+    def cleanup_case_name(self, name_raw: str) -> str:
+        """Clean up case name in Michigan
+
+        :param name_raw: Raw title string
+        :return: Cleaned name
+        """
         name = titlecase(name_raw)
         if "People of Mi " in name:
             name = name.replace("People of Mi ", "People of Michigan ")
         return name
 
     def get_lower_courts(self, courts: List[str]) -> str:
+        """Get the lower courts
+
+        :param courts: Court names as an array
+        :return: The lower courts combined into a string
+        """
         if courts:
             lower_courts = ", ".join(courts).lstrip(", ")
         else:
             lower_courts = ""
         return titlecase(lower_courts)
-
-    def _get_lower_courts(self) -> List[str]:
-        return self._get_optional_field_by_id("lower_courts")
-
-    def _download_backwards(self, page: int) -> None:
-        self.url = f"https://www.courts.michigan.gov/api/CaseSearch/SearchCaseOpinions?resource=supreme-court-opinion&aAppellateCourt=Supreme%20Court&searchQuery=&sortOrder=Newest&page={page}&pageSize=100"
-        time.sleep(7)  # This site throttles if faster than 2 hits / 5s.
-        self.html = self._download()
--- juriscraper/opinions/united_states/state/michctapp.py
+++ juriscraper/opinions/united_states/state/michctapp.py
@@ -7,12 +7,9 @@ History:
     - 2014-09-19: Created by Jon Andersen
     - 2022-01-28: Updated for new web site, @satsuki-chan.
 """
-import re
-import time
 from typing import List
+from urllib.parse import urlencode
 
-from juriscraper.AbstractSite import logger
-from juriscraper.lib.string_utils import titlecase
 from juriscraper.opinions.united_states.state import mich
 
 
@@ -20,46 +17,29 @@ class Site(mich.Site):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.court_id = self.__module__
-        self.url = "https://www.courts.michigan.gov/api/CaseSearch/SearchCaseOpinions?resource=opinion&aAppellateCourt=Court%20Of%20Appeals&searchQuery=&sortOrder=Newest&page=1&pageSize=100"
-        self.back_scrape_iterable = list(range(1, 999))
+        self.court = "Court Of Appeals"
+        params = self.filters + (("aAppellateCourt", self.court),)
+        self.url = f"https://www.courts.michigan.gov/api/CaseSearch/SearchCaseOpinions?{urlencode(params)}"
 
-    def _process_html(self) -> None:
-        for item in self.html["searchItems"]:
-            title = item["title"]
-            match = re.search(self.title_re, title)
-            if not match:
-                logger.warning(
-                    f"Opinion '{title}' has no docket and case name."
-                )
-                continue
-            docket = match.group("docket")
-            name = self.get_name(match.group("name"))
-            url = (
-                f"https://www.courts.michigan.gov{item['documentUrl'].strip()}"
-            )
-            date = item["displayDate"]
-            lower_courts = self.get_lower_courts(item["courts"])
-            status = self.get_status(title)
+    def _get_precedential_statuses(self) -> List[str]:
+        """Find Precedential Status
 
-            self.cases.append(
-                {
-                    "date": date,
-                    "docket": docket,
-                    "name": name,
-                    "url": url,
-                    "lower_courts": lower_courts,
-                    "status": status,
-                }
-            )
+        If the case is published they note Published in the title string.
+
+        :return: Precedential statuses
+        """
+        for case in self.cases:
+            case["precedential_status"] = self.get_status(case["title"])
+        return [case["precedential_status"] for case in self.cases]
 
     def get_status(self, title: str) -> str:
+        """Get the status of a case
+
+        :param title: The JSON API title string
+        :return: The status of the case
+        """
         if " Published" in title:
             status = "Published"
         else:
             status = "Unpublished"
         return status
-
-    def _download_backwards(self, page: int) -> None:
-        self.url = f"https://www.courts.michigan.gov/api/CaseSearch/SearchCaseOpinions?resource=opinion&aAppellateCourt=Court%20Of%20Appeals&searchQuery=&sortOrder=Newest&page={page}&pageSize=100"
-        time.sleep(7)  # This site throttles if faster than 2 hits / 5s.
-        self.html = self._download()
