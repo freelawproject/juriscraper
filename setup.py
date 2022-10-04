2ea3062a89cfed814f23cc45b5772a148756287f
Alberto Islas <albertisfu@gmail.com>
2022-10-04 17:50:54 -0500

    version bump v2.5.19

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import unittest
 from setuptools import find_packages, setup
 from setuptools.command.install import install
 
-VERSION = "2.5.18"
+VERSION = "2.5.19"
 AUTHOR = "Free Law Project"
 EMAIL = "info@free.law"
 HERE = os.path.abspath(os.path.dirname(__file__))
