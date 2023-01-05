e989f2dbc23f15565789378f6a2982f4bebf051c
William Palin <bill@free.law>
2023-01-05 16:00:02 -0500

    fix(colo): Update colorado scraper
    Add tests and update for
    Colo
    Colo Ct App

--- juriscraper/opinions/united_states/state/coloctapp.py
+++ juriscraper/opinions/united_states/state/coloctapp.py
@@ -4,9 +4,15 @@ Court Short Name: Colo. Ct. App.
 
 History:
     - 2022-01-31: Updated by William E. Palin
+    - 2023-01-05: Updated by William E. Palin
 """
 
-from juriscraper.lib.string_utils import convert_date_string
+
+import datetime
+import re
+
+from lxml.html import fromstring
+
 from juriscraper.opinions.united_states.state import colo
 
 
@@ -14,39 +20,101 @@ class Site(colo.Site):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.court_id = self.__module__
-        self.url = "https://www.courts.state.co.us/Courts/Court_of_Appeals/Case_Announcements/"
+        self.month = datetime.date.today().month
+        self.year = datetime.date.today().year
+        self.url = f"https://www.courts.state.co.us/Courts/Court_of_Appeals/Case_Announcements/Index.cfm?year={self.year}&month={self.month}&Submit=Go"
 
-    def _process_html(self) -> None:
-        """Process the HTML
+    def _process_html(self):
+        if not self.test_mode_enabled():
+            self.initiate_webdriven_session()
 
-        This is an odd little site, where they just post unpublished and published opinions without links.
-        Fortunately, its easy enough to crawl and the pattern for the URLs is pretty consistent. (i think)
+        if not self.test_mode_enabled():
+            links = self.html.xpath(".//a[@class='touchable-link']")
+        else:
+            links = ["fake_link"]
 
-        The URL looks like this https://www.courts.state.co.us/Courts/Court_of_Appeals/Opinion/[YEAR]/[DOCKET]-[PD].pdf
+        for link in links:
+            if self.test_mode_enabled():
+                super()._download()
+                date_filed = "December 29, 2022"
+            else:
+                date_filed = link.text_content()
+                self.webdriver.get(link.get("href"))
+                # Wait for pdf to render
+                self.webdriver.implicitly_wait(10000)
+                # And click next twice to make sure we fully render the PDF content
+                self.find_element_by_id("next").click()
+                self.find_element_by_id("next").click()
+                self.html = fromstring(self.webdriver.page_source)
 
-        I think PD means Published Decision, but I'm not sure.  If it is then the URLs should work everytime.
-        The caveat is that the Supreme Court seems to have some lag between switching to 2022 from 2021 bucket but
-        the URLS are given there.
+            pattern = r"(2022COA\d+)\n(.*?)\n(Division \w+)"
+            urls_to_add = []
+            for pg in self.html.xpath(".//div[@class='page']"):
+                if not pg.xpath(".//span[@role='link']/span/@aria-owns"):
+                    continue
+                anno_ids = pg.xpath(".//span[@role='link']/span/@aria-owns")
+                for anno_id in anno_ids:
+                    urls = pg.xpath(
+                        f"//a[@id='pdfjs_internal_id_{anno_id}']/@href"
+                    )
+                    if urls:
+                        url = urls[0]
+                        if url not in urls_to_add:
+                            urls_to_add.append(url)
+                pg_content = []
+                for element in pg.xpath(".//span"):
+                    if element.get("class") != "markedContent":
+                        continue
+                    pg_content.append(element.text_content())
+                pg_text = "\n".join(pg_content)
+                for match in re.finditer(pattern, pg_text, re.DOTALL):
+                    citation = match.groups()[0]
+                    case_name = self.get_case_name(match.groups()[1])
+                    url = urls_to_add.pop(0)
+                    docket = url.split("/")[-1][:-4]
+                    self.cases.append(
+                        {
+                            "date": date_filed,
+                            "name": case_name,
+                            "docket": docket,
+                            "status": "Published",
+                            "url": url,
+                            "citation": citation,
+                        }
+                    )
 
-        :return: None
+        if not self.test_mode_enabled():
+            self.webdriver.quit()
+
+    def get_case_name(self, page_content) -> str:
+        """Get case name from page content
+
+        :param pg: List of text
+        :return: Cleaned case name
         """
-        rows = self.html.xpath("//p")
-        date = rows[0].text_content()
-        date_year = convert_date_string(date)
-
-        if "P U B L I S H E D  O P I N I O N S" != rows[1].text_content():
-            return {}
-        for row in rows[2:]:
-            if row.text_content() == "U N P U B L I S H E D  O P I N I O N S":
-                break
-            row_text = row.text_content().strip()
-            docket, name = row_text.split(" ", 1)
-            self.cases.append(
-                {
-                    "date": date,
-                    "docket": docket,
-                    "name": name,
-                    "url": f"https://www.courts.state.co.us/Courts/Court_of_Appeals/Opinion/{date_year.year}/{docket}-PD.pdf",
-                    "status": "Published",
-                }
-            )
+        start = False
+        content = []
+        for row in page_content.split("\n"):
+            if not row.strip():
+                continue
+            no_go_words = [
+                "Plaintiff",
+                "Appellee",
+                "Defendant",
+                "Intervenor",
+                "Appellant",
+            ]
+            if any(word in row for word in no_go_words):
+                continue
+            m = re.findall(r"Honorable .*, Judge", row)
+            if m:
+                start = True
+                continue
+            m = re.findall(r"[A-Z ,]{5,}", row)
+            if m and m[0] == row:
+                start = False
+            if not start:
+                continue
+            content.append(row.strip(","))
+        case_name = " ".join(content)
+        return case_name
