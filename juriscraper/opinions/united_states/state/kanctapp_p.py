8777dc277ca42232b3999bf5de96dcc5259305e3
William Palin <bill@free.law>
2023-01-13 09:07:29 -0500

    fix(kansas): Kansas updated website
    Update the four kansas scrapers and tests
    
    They have improved the site and it no longer
    requires selenium. Hooray

--- juriscraper/opinions/united_states/state/kanctapp_p.py
+++ juriscraper/opinions/united_states/state/kanctapp_p.py
@@ -1,13 +1,42 @@
-#  Scraper for Kansas Appeals Court
+# Scraper for Kansas Appeals Court
 # CourtID: kanctapp
 
+from juriscraper.OpinionSiteLinear import OpinionSiteLinear
 
-from juriscraper.opinions.united_states.state import kan_p
 
-
-class Site(kan_p.Site):
+class Site(OpinionSiteLinear):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.court_id = self.__module__
-        self.court = "Court of Appeals"
-        self.status = "Published"
+        self.url = "https://www.kscourts.org/cases-opinions/opinions.html"
+
+    def _process_html(self):
+        url = None
+        cap_column = self.html.xpath('//h2[@id="kc-cap"]')[0].getparent()
+        for element in cap_column.xpath(".//*"):
+            if element.tag == "h2":
+                if element.attrib["id"] == "kc-cap":
+                    status = "Published"
+                elif element.attrib["id"] == "kc-cau":
+                    status = "Unpublished"
+                    break
+            elif element.tag == "h3":
+                date = element.text_content()[6:]
+            elif element.tag == "p":
+                case_content = element.text_content()
+                components = case_content.split(" - ")
+                if "None Released" in case_content:
+                    continue
+                url = element.xpath(".//a/@href")[0]
+            if url == None:
+                continue
+            self.cases.append(
+                {
+                    "status": status,
+                    "date": date,
+                    "docket": components[0],
+                    "name": components[1],
+                    "url": url,
+                }
+            )
+            url = None
