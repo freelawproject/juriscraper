6fb07102b04a9a1e178e2380c45d765a8dde4c19
William Palin <bill@free.law>
2023-01-23 13:56:14 -0500

    fix(cavc): Fix CAVC scraper

--- juriscraper/opinions/united_states/federal_special/cavc.py
+++ juriscraper/opinions/united_states/federal_special/cavc.py
@@ -1,80 +1,72 @@
 """Scraper for the United States Court of Appeals for Veterans Claims
 CourtID: cavc
-Court Short Name: Vet.App.
+Court Short Name: Vet. App.
 History:
  - 2012-06-07: Created by Brian Carver
  - 2014-08-06: Updated by mlr.
+ - 2023-01-23: Update by William Palin
 """
 
 import datetime
-import time
 from datetime import date
+from typing import Optional
 
-from juriscraper.OpinionSite import OpinionSite
+from juriscraper.DeferringList import DeferringList
+from juriscraper.OpinionSiteLinear import OpinionSiteLinear
 
 
-class Site(OpinionSite):
+class Site(OpinionSiteLinear):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.url = "http://www.uscourts.cavc.gov/opinions.php"
         self.court_id = self.__module__
+        self.base = (
+            "https://efiling.uscourts.cavc.gov/cmecf/servlet/TransportRoom"
+        )
+        self.seeds = []
+        self.last_month = date.today() - datetime.timedelta(days=31)
+        self.status = "Published"
 
-    def _get_docket_numbers(self):
-        docket_numbers = []
-        for e in self.html.xpath("//*[@id='oasteps_boxes']//td[2]/a"):
-            s = ", ".join([t.strip() for t in e.xpath("text()") if t.strip()])
-            docket_numbers.append(s)
-        return docket_numbers
+    def _process_html(self):
+        """Process the CAVC website and collect new opinions
 
-    def _get_download_urls(self):
-        return list(self.html.xpath("//*[@id='oasteps_boxes']//td[2]//@href"))
+        :return: None
+        """
+        cases = self.html.xpath(".//tbody/tr/td/a/parent::td/parent::tr")
+        for case in cases:
+            try:
+                name, date = case.xpath(".//td/text()")
+                docket = case.xpath(".//a/text()")[0]
+                document_url = case.xpath(".//a/@href")[0]
+                clean_date = datetime.datetime.strptime(date, "%d%b%y")
+                if self.last_month > clean_date.date():
+                    break
+                clean_date_string = clean_date.strftime("%m/%d/%Y")
+                url = f"{self.base}?servlet=CaseSummary.jsp&caseNum={docket}&incOrigDkt=Y&incDktEntries=Y"
+                self.seeds.append(url)
+                self.cases.append(
+                    {
+                        "url": document_url,
+                        "date": clean_date_string,
+                        "docket": docket,
+                    }
+                )
+            except ValueError:
+                # An opinion was malformed. I'd rather skip it for now
+                continue
 
-    def _get_case_dates(self):
-        dates = []
-        for txt in self.html.xpath("//*[@id='oasteps_boxes']//td[3]//text()"):
-            dates.append(
-                date.fromtimestamp(time.mktime(time.strptime(txt, "%d%b%y")))
-            )
-        return dates
+    def _get_case_names(self) -> DeferringList:
+        """Get case names using a deferring list."""
 
-    def _get_case_names(self):
-        case_names = []
-        dates = self._get_case_dates()
-        appellants = []
-        for e in self.html.xpath("//*[@id='oasteps_boxes']//td[1]"):
-            app = ", ".join(
-                [t.strip() for t in e.xpath("text()") if t.strip()]
-            )
-            appellants.append(app)
-        for t, d in zip(appellants, dates):
-            # See: http://en.wikipedia.org/wiki/United_States_Secretary_of_Veterans_Affairs
-            if d > datetime.date(2014, 7, 30):
-                case_names.append(f"{t}v. McDonald")
-            elif d > datetime.date(2014, 5, 30):
-                case_names.append(f"{t} v. Gibson")
-            elif d > datetime.date(2009, 1, 20):
-                case_names.append(f"{t} v. Shinseki")
-            elif d > datetime.date(2007, 12, 20):
-                case_names.append(f"{t} v. Peake")
-            elif d > datetime.date(2007, 10, 1):
-                case_names.append(f"{t} v. Mansfield")
-            elif d > datetime.date(2005, 1, 26):
-                case_names.append(f"{t} v. Nicholson")
-            elif d > datetime.date(2001, 1, 23):
-                case_names.append(f"{t} v. Principi")
-            elif d > datetime.date(2000, 7, 25):
-                case_names.append(f"{t} v. Gober")
-            elif d > datetime.date(1998, 1, 2):
-                case_names.append(f"{t} v. West")
-            elif d > datetime.date(1997, 7, 1):
-                case_names.append(f"{t} v. Gober")
-            elif d > datetime.date(1993, 1, 22):
-                case_names.append(f"{t} v. Brown")
-            elif d > datetime.date(1992, 9, 26):
-                case_names.append(f"{t} v. Principi")
-            else:
-                case_names.append(f"{t} v. Derwinski")
-        return case_names
+        def get_name(url) -> Optional[str]:
+            # """Abstract out the case name from the case page."""
+            if self.test_mode_enabled():
+                return "No case names fetched during tests."
+            self.url = url
+            self.html = self._download()
+            case_name = self.html.xpath('.//tr[contains(., " v. ")]')[
+                -1
+            ].text_content()
+            return case_name
 
-    def _get_precedential_statuses(self):
-        return ["Published"] * len(self.case_names)
+        return DeferringList(seed=self.seeds, fetcher=get_name)
