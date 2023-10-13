48ad1600f7af220904dbe2a00d282414b6436630
William Palin <bill@free.law>
2023-10-13 13:21:59 -0400

    fix(kan/kanctapp): Fix four kansas scrapers

--- juriscraper/opinions/united_states/state/kan_p.py
+++ juriscraper/opinions/united_states/state/kan_p.py
@@ -1,4 +1,4 @@
-# Scraper for Kansas Appeals Court
+# Scraper for Kansas Supreme Court (published)
 # CourtID: kan_p
 
 from juriscraper.OpinionSiteLinear import OpinionSiteLinear
@@ -8,26 +8,63 @@ class Site(OpinionSiteLinear):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.court_id = self.__module__
-        self.url = "https://www.kscourts.org/cases-opinions/opinions.html"
-        self.status = "Published" if "_p" in self.court_id else "Unpublished"
-        self.xpath = "kc-scu" if "kan_" in self.court_id else "kc-cau"
-        self.rows = "preceding" if "_p" in self.court_id else "following"
+        self.url = "https://www.kscourts.org/Cases-Decisions/Decisions"
+        self.request["verify"] = False
+        self.status = "Published"
+        self.court = "Supreme Court"
+
+    def _update_parameters(self):
+        """"""
+        view_state = self.html.xpath("//input[@id='__VIEWSTATE']")[0].get(
+            "value"
+        )
+        VIEWSTATEGENERATOR = self.html.xpath(
+            "//input[@id='__VIEWSTATEGENERATOR']"
+        )[0].get("value")
+        CMSCsrfToken = self.html.xpath("//input[@id='__CMSCsrfToken']")[0].get(
+            "value"
+        )
+
+        data = {
+            "__CMSCsrfToken": CMSCsrfToken,
+            "__EVENTTARGET": "",
+            "__EVENTARGUMENT": "",
+            "lng": "en-US",
+            "__VIEWSTATEGENERATOR": VIEWSTATEGENERATOR,
+            "__SCROLLPOSITIONX": "0",
+            "__SCROLLPOSITIONY": "239",
+            "p$lt$ctl01$SmartSearchBox3$txtWord_exWatermark_ClientState": "",
+            "p$lt$ctl01$SmartSearchBox3$txtWord": "",
+            "p$lt$zonePagePlaceholder$pageplaceholder$p$lt$ctl02$OpinionFilter1$filterControl$txtSearch": "",
+            "p$lt$zonePagePlaceholder$pageplaceholder$p$lt$ctl02$OpinionFilter1$filterControl$drpPublished": self.status,
+            "p$lt$zonePagePlaceholder$pageplaceholder$p$lt$ctl02$OpinionFilter1$filterControl$drpCourt": self.court,
+            "p$lt$zonePagePlaceholder$pageplaceholder$p$lt$ctl02$OpinionFilter1$filterControl$drpSortBy": "Sort By",
+            "p$lt$zonePagePlaceholder$pageplaceholder$p$lt$ctl02$OpinionFilter1$filterControl$btnFilter": "Apply Filters",
+            "p$lt$zonePagePlaceholder$pageplaceholder$p$lt$ctl03$AccordionLayout1$acc_AccordionExtender_ClientState": "-1",
+            "p$lt$zonePagePlaceholder$pageplaceholder$p$lt$ctl07$AccordionLayout$acc_AccordionExtender_ClientState": "-1",
+            "p$lt$ctl09$SmartSearchBox1$txtWord_exWatermark_ClientState": "",
+            "p$lt$ctl09$SmartSearchBox1$txtWord": "",
+            "__VIEWSTATE": view_state,
+        }
+        self.parameters = data
+        self.method = "POST"
+        self.request["verify"] = False
 
     def _process_html(self):
-        h2_element = self.html.xpath(f'//h2[@id="{self.xpath}"]')[0]
-        ul_elements = h2_element.xpath(f"{self.rows}-sibling::ul")
-        for ul in ul_elements:
-            if "None Released" in ul.text_content():
-                continue
-            date = ul.getprevious().text_content()[6:]
-            for item in ul.xpath(".//p"):
-                components = item.text_content().split(" - ")
-                self.cases.append(
-                    {
-                        "status": self.status,
-                        "date": date,
-                        "docket": components[0],
-                        "name": components[1],
-                        "url": item.xpath(".//a/@href")[0],
-                    }
-                )
+        self.method = "POST"
+        self._update_parameters()
+        self.html = super()._download()
+        for row in self.html.xpath(".//tr"):
+            date_filed, docket_number, case_name, court, status = row.xpath(
+                ".//td/a/text()"
+            )
+            url = row.xpath(".//td/a")[0].get("href")
+            self.cases.append(
+                {
+                    "status": status,
+                    "date": date_filed,
+                    "docket": docket_number,
+                    "name": case_name,
+                    "url": url,
+                }
+            )
--- juriscraper/opinions/united_states/state/kanctapp_u.py
+++ juriscraper/opinions/united_states/state/kanctapp_u.py
@@ -1,4 +1,4 @@
-# Scraper for Kansas Appeals Court
+# Scraper for Kansas Appeals Court (unpublished)
 # CourtID: kanctapp_u
 
 from juriscraper.opinions.united_states.state import kan_p
@@ -8,3 +8,5 @@ class Site(kan_p.Site):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.court_id = self.__module__
+        self.status = "Unpublished"
+        self.court = "Court of Appeals"
