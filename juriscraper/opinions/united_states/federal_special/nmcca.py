c23d020fb7ecf69a543fb96b4fc722f530bd460f
William Palin <bill@free.law>
2023-11-14 15:22:06 -0500

    fix(nmcca): Fix NMCCA
    They added a nice django API we can use

--- juriscraper/opinions/united_states/federal_special/nmcca.py
+++ juriscraper/opinions/united_states/federal_special/nmcca.py
@@ -6,50 +6,32 @@ History:
     15 Sep 2014: Created by Jon Andersen
 """
 
-from juriscraper.lib.string_utils import convert_date_string, titlecase
-from juriscraper.OpinionSite import OpinionSite
+from juriscraper.OpinionSiteLinear import OpinionSiteLinear
 
 
-class Site(OpinionSite):
+class Site(OpinionSiteLinear):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.url = "http://www.jag.navy.mil/courts/opinion_archive.htm"
-        self.court_id = self.__module__
-        self.back_scrape_iterable = list(range(2013, 2004 - 1, -1))
-
-    def _get_case_dates(self):
-        # this court makes a lot of typos apparently
-        typos = {
-            "6/13/30/13": "6/13/13",
-            "6/11/30/13": "6/11/13",
-            "02/12/09 & 12/04/08": "02/12/09",
-            "006/08/2020": "06/08/2020",
-        }
-
-        dates = []
-        path = "//table/tbody/tr/td[3]//text()"
-        for ds in self.html.xpath(path):
-            ds = typos[ds] if ds in typos else ds
-            dates.append(convert_date_string(ds))
-        return dates
-
-    def _get_case_names(self):
-        path = "//table/tbody/tr/td[1]/text()"
-        return [titlecase(text) for text in self.html.xpath(path)]
-
-    def _get_download_urls(self):
-        path = "//table/tbody/tr/td[4]/a[1]/@href"
-        return [e for e in self.html.xpath(path)]
-
-    def _get_docket_numbers(self):
-        path = "//table/tbody/tr/td[2]//text()"
-        return [docket_number for docket_number in self.html.xpath(path)]
-
-    def _get_precedential_statuses(self):
-        return ["Published"] * len(self.case_dates)
-
-    def _download_backwards(self, year):
         self.url = (
-            "http://www.jag.navy.mil/courts/opinion_archive_%d.htm" % year
+            "https://www.jag.navy.mil/api/tables/decisions-opinions/data/"
         )
-        self.html = self._download()
+        self.court_id = self.__module__
+        self.request["verify"] = False
+
+    def _process_html(self):
+        for row in self.html["results"]:
+            date, docket, notes, name = list(row["data"].values())
+            url = row["documents"][0]["document"]["download_url"]
+            if notes == "Unpublished":
+                status = "Unpublished"
+            else:
+                status = "Published"
+            self.cases.append(
+                {
+                    "date": date,
+                    "name": name,
+                    "url": url,
+                    "status": status,
+                    "docket": docket,
+                }
+            )
