cf65945dbf3ac2549cb3e04ff304f12f24c6aece
William Palin <bill@free.law>
2023-11-20 15:01:12 -0500

    fix(va): Make Virginia faster

--- juriscraper/opinions/united_states/state/va.py
+++ juriscraper/opinions/united_states/state/va.py
@@ -1,3 +1,5 @@
+from datetime import date, datetime, timedelta
+
 from juriscraper.OpinionSiteLinear import OpinionSiteLinear
 
 
@@ -7,6 +9,10 @@ class Site(OpinionSiteLinear):
         self.court_id = self.__module__
         self.url = "http://www.courts.state.va.us/scndex.htm"
         self.status = "Published"
+        today = date.today()
+        if self.test_mode_enabled():
+            today = datetime.strptime("11/20/2023", "%m/%d/%Y").date()
+        self.two_months_ago = today - timedelta(days=60)
 
     def _process_html(self):
         for row in self.html.xpath("//p"):
@@ -17,7 +23,12 @@ class Site(OpinionSiteLinear):
             summary = row.text_content().split(name)[1].strip()
             date = summary.split("\n")[0].strip()
             if "Revised" in date:
-                date = date.split("Revised")[1].strip()
+                date = date.split("Revised")[1].strip().strip(")")
+
+            date_object = datetime.strptime(date, "%m/%d/%Y").date()
+            if date_object < self.two_months_ago:
+                continue
+
             self.cases.append(
                 {
                     "name": row.xpath(".//b/text()")[0].strip(),
