3451c9bf1bb6c3e6c5e64801563e3249ec55f837
Kevin Ramirez <kvnzavalza@hotmail.com>
2023-12-12 11:15:29 -0600

    fix(ca2): Fix oral arguments ca2

--- juriscraper/oral_args/united_states/federal_appellate/ca2.py
+++ juriscraper/oral_args/united_states/federal_appellate/ca2.py
@@ -11,21 +11,18 @@ History:
   2023-11-21: Fixed by flooie
   2023-12-11: Fixed by quevon24
 """
-from datetime import date, timedelta
 
-from dateutil.rrule import DAILY, rrule
 from lxml import html
 
-from juriscraper.lib.string_utils import convert_date_string
 from juriscraper.OralArgumentSiteLinear import OralArgumentSiteLinear
 
 
 class Site(OralArgumentSiteLinear):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.interval = 30
         self.court_id = self.__module__
-        self.url = "https://ww3.ca2.uscourts.gov/decisions"
+        self.base_url = "http://www.ca2.uscourts.gov"
+        self.url = f"{self.base_url}/decisions"
         self.method = "POST"
         self.base_xpath = '//tr[contains(.//a/@href, "mp3")]'
         self.parameters = {
@@ -37,86 +34,43 @@ class Site(OralArgumentSiteLinear):
             "IW_DATABASE": "Oral Args",
             "opinion": "*",
         }
-        self.back_scrape_iterable = [
-            i.date()
-            for i in rrule(
-                DAILY,
-                interval=self.interval,
-                dtstart=date(2023, 6, 14),
-                until=date(2023, 12, 11),
-            )
-        ]
+        self.backscraper = False
+        self.back_scrape_iterable = ["placeholder"]
 
     def _process_html(self):
-        self._extract_cases_from_html(self.html)
+        for row in self.html.xpath("//table[@border='1']"):
+            link = row.xpath(".//a")[0].get("href")
+            if ".mp3" not in link:
+                continue  # skip bad data
+            docket = row.xpath(".//a/nobr/text()")[0]
+            name, date = row.xpath(".//td/text()")
+            url = self._build_url(link)
+            if docket == "GMT20230614-175136_Recording":
+                continue  # skip bad data
+            self.cases.append(
+                {"docket": docket, "url": url, "name": name, "date": date}
+            )
 
     def _download(self, request_dict={}):
         if self.test_mode_enabled():
             self.html = super()._download()
             return self.html
+        if self.backscraper:
+            return super()._download()
         r = self.request["session"].post(self.url, params=self.parameters)
         return html.fromstring(r.content)
 
-    def _extract_cases_from_html(self, html):
-        for row in html.xpath("//table[@border='1']"):
-            link = row.xpath(".//a")[0].get("href")
-            docket = (
-                row.xpath(".//a/nobr/text()")[0]
-                if row.xpath(".//a/nobr/text()")
-                else None
-            )
-            if row.xpath(".//td/text()"):
-                name, date = row.xpath(".//td/text()")
-            else:
-                continue
-            if docket and link and name and date:
-                # empty row in date ranges between 10-12-2023 and 11-11-2023
-                self.cases.append(
-                    {"docket": docket, "url": link, "name": name, "date": date}
-                )
-
-    def extract_next_page_url(self, html):
-        """Return the href url from "Next" pagination element
-        if it exists, otherwise return False.
-        """
-        path = '//table//tr/td[2]/font/a[contains(., "Next")]/@href'
-        elements = html.xpath(path)
-        return (
-            f"https://ww3.ca2.uscourts.gov{elements[0]}" if elements else False
-        )
-
-    def _get_case_dates(self):
-        dates = []
-        for case in self.cases:
-            if case["date"] == "22-2649":
-                # Incorrect data in column range between 06-14-2023 and 06-30-2023
-                case["date"] = "6-14-23"
-                case["docket"] = "22-2649"
-            dates.append(convert_date_string(case["date"]))
-        return dates
+    def _build_url(self, link):
+        if self.backscraper:
+            url = link
+        else:
+            url = f"{self.base_url}{link}"
+        return url
 
     def _download_backwards(self, d):
-        # Date format: %Y%m%d = 20231111
-        self.parameters.update(
-            {
-                "IW_FILTER_DATE_BEFORE": (
-                    d + timedelta(self.interval)
-                ).strftime("%Y%m%d"),
-                "IW_FILTER_DATE_AFTER": d.strftime("%Y%m%d"),
-            }
-        )
-        self.html = self._download()
-
-        proceed = True
-        while proceed:
-            next_page_url = self.extract_next_page_url(self.html)
-            if next_page_url:
-                self.html = self._get_html_tree_by_url(next_page_url)
-                self._extract_cases_from_html(self.html)
-            else:
-                proceed = False
-
-        if self.html is not None:
-            # Setting status is important because it prevents the download
-            # function from being run a second time by the parse method.
-            self.status = 200
+        self.backscraper = True
+        self.method = "GET"
+        for i in range(1, 33):
+            self.url = f"{self.base_url}/decisions/isysquery/19342efa-bb49-4684-a054-875324b58eb9/{(i*10)-9}-{i*10}/list/"
+            self.html = self._download()
+            self._process_html()
