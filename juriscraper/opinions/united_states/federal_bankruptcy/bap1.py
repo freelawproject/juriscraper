5ade74c97eb643033da55461aef3d3c6834f8e79
Gianfranco Rossi <gianfranco.rossi.r@gmail.com>
2023-12-15 23:07:27 -0500

    Updated example files for main scraper; refactored code according to code review

--- juriscraper/opinions/united_states/federal_bankruptcy/bap1.py
+++ juriscraper/opinions/united_states/federal_bankruptcy/bap1.py
@@ -9,18 +9,70 @@ from datetime import datetime, timedelta
 from lxml.html import HtmlElement
 
 from juriscraper.OpinionSiteLinear import OpinionSiteLinear
-from juriscraper.AbstractSite import logger
 
 
 class Site(OpinionSiteLinear):
+    # See issue #828 on Github for extraction reference
+    lower_court_to_abbreviation = {
+        "USBC - District of New Hampshire": "NH",
+        "USBC - District of Massachusetts (Worcester)": "MW",
+        "USBC - District of Puerto Rico": "PR",
+        "USBC - District of Massachusetts (Boston)": "MB",
+        "USBC - District of Maine (Portland)": "EP",
+        "USBC - District of Rhode Island": "RI",
+        "Bankrupcty Court of ME - Bangor": "EB",
+        "Bankruptcy Court of MA - Boston": "MB",
+        "Bankruptcy Court of MA - Springfield": "MS",
+        "Bankruptcy Court of ME - Portland": "EP",
+        "Bankruptcy Court - Rhode Island": "RI",
+        "Bankruptcy Court - San Juan Puerto Rico": "PR",
+        "Bankruptcy Court of MA - Worcester": "MW",
+        "Bankruptcy Court - Ponce Puerto Rico": "PR",
+        "Bankruptcy Court of NH, Concord": "NH",
+    }
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.url = "https://www.bap1.uscourts.gov/bapopn"
+        self.base_url = self.url = "https://www.bap1.uscourts.gov/bapopn"
         self.court_id = self.__module__
 
-        self.method = "GET"
+        self.set_date_filter_parameters()
 
-        # Search the last month
+        # There are 29 pages as of development in Dec 2023 (source indexes from 0)
+        self.back_scrape_iterable = range(29)
+
+    def _download(self, request_dict: Dict = {}) -> HtmlElement:
+        return super()._download(self.parameters)
+
+    def _process_html(self) -> None:
+        for row in self.html.xpath("//tr[td]"):
+            opinion_name = row.xpath("td[1]")[0].text_content().strip()
+            status = self.get_status_from_opinion_name(opinion_name)
+
+            lower_court = row.xpath("td[4]/span")[0].text_content().strip()
+            docket_number = row.xpath("td[2]")[0].text_content().strip()
+
+            case = {
+                "status": status,
+                "url": row.xpath("td[1]/a/@href")[0],  # opinion url
+                "docket": self.build_full_docket_number(
+                    docket_number, lower_court
+                ),
+                # Pub Date
+                "date": row.xpath("td[3]")[0].text_content().strip(),
+                # short title
+                "name": row.xpath("td[4]")[0].text.strip(),
+                # district
+                "lower_court": lower_court,
+            }
+
+            self.cases.append(case)
+
+    def set_date_filter_parameters(self):
+        """
+        Limit search to get fresh data and avoid overloading the court server
+        Default: last month
+        """
         date_filter_end = datetime.today()
         date_filter_start = date_filter_end - timedelta(30)
         self.parameters = {
@@ -36,57 +88,29 @@ class Site(OpinionSiteLinear):
             }
         }
 
-        self.back_scrape_iterable = ["placeholder"]
-
-    def _download(self, request_dict: Dict = {}) -> HtmlElement:
-        return super()._download(self.parameters)
-
-    def _process_html(self) -> None:
-        for row in self.html.xpath("//tr[td]"):
-            opinion_name = row.xpath("td[1]")[0].text_content().strip()
-            status = self.get_status_from_opinion_name(opinion_name)
-
-            case = {
-                "status": status,
-                "url": row.xpath("td[1]/a/@href")[0],  # opinion url
-                "docket": row.xpath("td[2]")[0].text_content().strip(),
-                # Pub Date
-                "date": row.xpath("td[3]")[0].text_content().strip(),
-                # short title
-                "name": row.xpath("td[4]")[0].text.strip(),
-                # district
-                "lower_court": row.xpath("td[4]/span")[0]
-                .text_content()
-                .strip(),
-            }
-
-            self.cases.append(case)
-
-    def _download_backwards(self, _) -> None:
-        self.backscraper = True
+    def _download_backwards(self, page_number: int) -> None:
         self.parameters = {}  # delete date filters used in normal scraper
-
+        self.url = (
+            f"{self.base_url}?page={page_number}"
+            if page_number > 0
+            else self.base_url
+        )
         self.html = self._download()
         self._process_html()
 
-        next_page_exists = True
+    def build_full_docket_number(
+        self, docket_number: str, lower_court: str
+    ) -> str:
+        """
+        Full docket number has the lower court abbreviation in it
+        For each unique lower court in the opinions available on the source,
+        the linked opinion PDF was opened and the abbrevation extracted
+        """
+        lower_court_abbreviation = self.lower_court_to_abbreviation.get(
+            lower_court, ""
+        )
 
-        while next_page_exists:
-            next_page_url = self.extract_next_page_url()
-
-            if next_page_url:
-                logger.info(f"Scraping next page: {next_page_url}")
-                self.html = self._get_html_tree_by_url(next_page_url)
-            else:
-                next_page_exists = False
-
-            self._process_html()
-
-    def extract_next_page_url(self) -> str:
-        next_page_link_xpath = "//a[@title='Go to next page']/@href"
-        elements = self.html.xpath(next_page_link_xpath)
-
-        return elements[0] if elements else ""
+        return f"BAP No. {lower_court_abbreviation} {docket_number}"
 
     @staticmethod
     def get_status_from_opinion_name(opinion_name: str) -> str:
