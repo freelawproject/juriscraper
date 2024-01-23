bd46c28263c9884f3f51b073706bdf7dc599fe1d
William Palin <bill@free.law>
2024-01-23 17:03:24 -0500

    feat(conn): Update conn
    Add SSL adapter
    Fix Scraper for 2024

--- juriscraper/opinions/united_states/state/conn.py
+++ juriscraper/opinions/united_states/state/conn.py
@@ -16,6 +16,7 @@ History:
 import re
 from datetime import date
 
+from juriscraper.lib.network_utils import SSLAdapter
 from juriscraper.OpinionSiteLinear import OpinionSiteLinear
 
 
@@ -26,27 +27,75 @@ class Site(OpinionSiteLinear):
         self.year = date.today().strftime("%y")
         self.url = f"http://www.jud.ct.gov/external/supapp/archiveAROsup{self.year}.htm"
         self.status = "Published"
+        self._mount_ssl_adapter()
+
+    def _mount_ssl_adapter(self):
+        """Configures and mounts an SSL adapter to a given session
+
+        :return: None
+        """
+        self.request["session"].mount(
+            "https://", SSLAdapter(ciphers="AES256-SHA256")
+        )
+
+    def fetch_docket_numbers(self, text):
+        """Find docket numbers in row
+
+        :param text: the row text
+        :return: return a list of docket numbers matches
+        """
+        matches = re.findall(r"SC\d+", text)
+        return matches
+
+    def make_case_name(self, cleaned_text, url):
+        """Generate the case name identifying opinion type
+
+        :param cleaned_text: the row text without excess whitespace
+        :param url: the url of the document
+        :return: Update case name
+        """
+        name = cleaned_text.split("-", 1)[1].strip()
+        if url[-5].upper() == "A":
+            name = f"{name} (Concurrence)"
+        elif url[-5].upper() == "B":
+            name = f"{name} (Second Concurrence)"
+        elif url[-5].upper() == "E":
+            name = f"{name} (Dissent)"
+        return name.strip()
 
     def _process_html(self) -> None:
         """Process the html and extract out the opinions
 
         :return: None
         """
-        for row in self.html.xpath("//ul/preceding-sibling::p"):
-            if "Published in the Law Journal" not in row.text_content():
+        begin = False
+        date = None
+        for row in self.html.xpath(
+            "//table[@id='AutoNumber1']//*[text() and not(./*[text()])]"
+        ):
+            if "Published" in row.text_content():
+                begin = True
+            if not begin:
                 continue
-            date = row.text_content().split(" ")[-1][:-1]
-            for document in row.xpath("following-sibling::ul[1]/li"):
-                output_string = re.sub(
-                    r"\s{2,}", " ", document.text_content()
-                ).strip()
-                name = output_string.split("-")[-1].strip()
-                docket = document.xpath(".//a")[0].text_content().split()[0]
+            if "Published" in row.text_content():
+                date = row.text_content().split("of")[-1][:-1].strip()
+                continue
+            if row.text_content().strip() == "Return to Top":
+                return
+            if row.tag == "a":
+                url = row.get("href")
+                text = row.getparent().text_content().strip()
+                cleaned_text = re.sub(r"\s+", " ", text)
+                name = self.make_case_name(cleaned_text, url)
+                docket = self.fetch_docket_numbers(cleaned_text)
+
+                if not date:
+                    continue
                 self.cases.append(
                     {
                         "date": date,
-                        "url": document.xpath(".//a")[0].get("href"),
-                        "docket": docket,
+                        "url": url,
+                        "docket": ",".join(docket),
                         "name": name,
                     }
                 )
