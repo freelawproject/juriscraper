708dd14f9348bc26b25392e230371b9016b871c9
William Palin <bill@free.law>
2024-01-26 14:31:51 -0500

    fix(conn): Update connecticut

--- juriscraper/opinions/united_states/state/conn.py
+++ juriscraper/opinions/united_states/state/conn.py
@@ -23,30 +23,69 @@ class Site(OpinionSiteLinear):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.court_id = self.__module__
-        self.year = date.today().strftime("%y")
-        self.url = f"http://www.jud.ct.gov/external/supapp/archiveAROsup{self.year}.htm"
+        year = date.today().strftime("%y")
+        self.url = (
+            f"http://www.jud.ct.gov/external/supapp/archiveAROsup{year}.htm"
+        )
         self.status = "Published"
+        self.cipher = "AES256-SHA256"
+        self.set_custom_adapter(self.cipher)
+
+    @staticmethod
+    def find_docket_numbers(text: str) -> str:
+        """Find docket numbers in row
+
+        :param text: the row text
+        :return: return docket numbers
+        """
+        matches = re.findall(r"SC\d+", text)
+        return ", ".join(matches)
+
+    @staticmethod
+    def remove_excess_whitespace(text: str) -> str:
+        return re.sub(r"\s+", " ", text)
+
+    @staticmethod
+    def find_published_date(row):
+        closest_published = row.xpath(
+            "preceding::*[contains(., 'Published')][1]"
+        )
+        date_str = closest_published[0].text_content()
+        m = re.search(
+            r"(\b\d{1,2}/\d{1,2}/\d{4}\b)|(\b(?:January|February|March|April|May|June|July|August|September|October|November|December) \d{1,2}, \d{4}\b)",
+            date_str,
+        )
+        return m.groups()[0] if m.groups()[0] else m.groups()[1]
 
     def _process_html(self) -> None:
         """Process the html and extract out the opinions
 
         :return: None
         """
-        for row in self.html.xpath("//ul/preceding-sibling::p"):
-            if "Published in the Law Journal" not in row.text_content():
-                continue
-            date = row.text_content().split(" ")[-1][:-1]
-            for document in row.xpath("following-sibling::ul[1]/li"):
-                output_string = re.sub(
-                    r"\s{2,}", " ", document.text_content()
-                ).strip()
-                name = output_string.split("-")[-1].strip()
-                docket = document.xpath(".//a")[0].text_content().split()[0]
-                self.cases.append(
-                    {
-                        "date": date,
-                        "url": document.xpath(".//a")[0].get("href"),
-                        "docket": docket,
-                        "name": name,
-                    }
+        pdf_xpath = "//table[@id='AutoNumber1']//a[contains(@href, '.pdf')]"
+        for row in self.html.xpath(pdf_xpath):
+            url = row.get("href")
+            link_text = row.text_content().strip()
+            full_row_text = row.getparent().text_content().strip()
+            if link_text == full_row_text:
+                # Fix for atypical bug found in an older year
+                full_row_text = (
+                    row.getparent().getparent().text_content().strip()
                 )
+            docket_with_type_opt = self.remove_excess_whitespace(link_text)
+            name = (
+                self.remove_excess_whitespace(full_row_text)
+                .replace(docket_with_type_opt, "")
+                .strip("- ")
+            )
+            date = self.find_published_date(row)
+            dockets = self.find_docket_numbers(docket_with_type_opt)
+
+            self.cases.append(
+                {
+                    "date": date,
+                    "url": url,
+                    "docket": dockets,
+                    "name": name,
+                }
+            )
