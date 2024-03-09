ae96a6b14d28a864e07d0aa8a8df2a2ffc9c742c
Gianfranco Rossi <gianfranco.rossi.r@gmail.com>
2024-03-08 22:29:57 -0500

    feat(texapp_1, texapp_2): update to new base class
    Also, update test files

--- juriscraper/opinions/united_states/state/texapp_1.py
+++ juriscraper/opinions/united_states/state/texapp_1.py
@@ -5,6 +5,7 @@
 # Reviewer:
 # Date: 2014-07-10
 
+from typing import Dict, List
 
 from juriscraper.opinions.united_states.state import tex
 
@@ -16,13 +17,32 @@ class Site(tex.Site):
         self.court_name = "capp_1"
         self.checkbox = 2
 
-    def get_opinions(self, html):
-        """
-        Cluster with 2 opinions (Court of Appeals)
-        https://search.txcourts.gov/Case.aspx?cn=02-22-00347-CV&coa=coa02
-        Counter Examples:
+    def get_opinions(self, html) -> List[Dict]:
+        """Override from tex.py. See docstring there for more info
 
-        'opinion' in text, but it is actually a letter
-        https://search.txcourts.gov/Case.aspx?cn=01-23-00777-CR&coa=coa01
+        :param html: page's HTML object
+        :return List of opinions
         """
-        raise NotImplementedError("...")
+        opinions = []
+        opinion_xpath = "//div[div[contains(text(), 'Case Events')]]//tr[td[contains(text(), 'pinion issued')]]"
+        link_xpath = ".//tr[td[1]/a and td[2][contains(text(), 'pinion')]]"
+        for opinion in html.xpath(opinion_xpath):
+            op = {}
+            link = opinion.xpath(link_xpath)
+            if not link:
+                continue
+
+            op["disposition"] = opinion.xpath(".//td[3]/text()")[0]
+            op["download_url"] = link[0].xpath("td/a/@href")[0]
+
+            op_type = link[0].xpath("td[2]/text()")[0].strip().lower()
+            if "concur" in op_type:
+                op["type"] = "030concurrence"
+            elif "diss" in op_type:
+                op["type"] = "040dissent"
+            else:
+                op["type"] = "010combined"
+
+            opinions.append(op)
+
+        return opinions
