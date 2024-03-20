1985b911f6db039f505d67dbf6ce1547c49c0774
Gianfranco Rossi <gianfranco.rossi.r@gmail.com>
2024-03-20 12:04:12 -0500

    feat(fla): support dynamic backscraper
    Needed to fill `fla` gap #960
    Also, update fla scraper to OpinionSiteLinear

--- juriscraper/opinions/united_states/state/fla.py
+++ juriscraper/opinions/united_states/state/fla.py
@@ -2,62 +2,94 @@
 # CourtID: fla
 # Court Short Name: fla
 
-from datetime import datetime
+from datetime import date, datetime, timedelta
+from typing import Dict, Tuple
 
-from juriscraper.lib.string_utils import convert_date_string
-from juriscraper.OpinionSite import OpinionSite
+from juriscraper.AbstractSite import logger
+from juriscraper.lib.date_utils import make_date_range_tuples
+from juriscraper.OpinionSiteLinear import OpinionSiteLinear
 
 
-class Site(OpinionSite):
+class Site(OpinionSiteLinear):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.court_id = self.__module__
-        today = datetime.today().strftime("%m/%d/%Y")
-        this_year = datetime.today().year
-        last_year = this_year - 1
-        today_last_year = today.replace(str(this_year), str(last_year))
+        self.status = "Published"
+
+        today = datetime.today()
+        today_last_year = today - timedelta(days=365)
+        fmt = self.url_date_format = "%m/%d/%Y"
+
         # Get 50 most recent opinions from past year--even though you can
         # put whatever number you want as limit, 50 seems to be max
-        self.url = (
-            "https://www.floridasupremecourt.org/search/?searchtype=opinions&limit=50&startdate=%s&enddate=%s"
-            % (today_last_year, today)
+        self.base_url = "https://www.floridasupremecourt.org/search/?searchtype=opinions&limit=50&startdate={}&enddate={}"
+        self.url = self.base_url.format(
+            today_last_year.strftime(fmt), today.strftime(fmt)
         )
-        self.path_cell = '//div[@class="search-results"]//tr/td[%d]'
-        self.cases = []
 
-    def _download(self, request_dict={}):
-        html = super()._download(request_dict)
-        self._extract_cases_from_html(html)
-        return html
+        # make a backscrape request every `days_interval` range, to avoid pagination
+        self.days_interval = 60
+        self.first_opinion_date = datetime(1999, 9, 23)
+        self.make_backscrape_iterable(kwargs)
 
-    def _extract_cases_from_html(self, html):
+    def _process_html(self) -> None:
+        """Parses HTML into case dictionaries
+
+        :return: None
+        """
         path = '//div[@class="search-results"]//tbody/tr'
-        for row in html.xpath(path):
-            cells = row.xpath("./td")
-            url = cells[4].xpath("./a/@href")
+        for row in self.html.xpath(path):
+            cells = row.xpath("td")
+            url = cells[4].xpath("a/@href")
+
             if not url:
                 # Skip rows without PDFs
                 continue
+
             self.cases.append(
                 {
                     "url": url[0],
-                    "name": cells[2].text_content(),
-                    "docket": cells[1].text_content(),
-                    "date": convert_date_string(cells[0].text_content()),
+                    "docket": cells[1].text_content().strip(),
+                    "name": cells[2].text_content().strip(),
+                    "date": cells[0].text_content().strip(),
+                    "status": self.status,
                 }
             )
 
-    def _get_case_names(self):
-        return [case["name"] for case in self.cases]
+    def _download_backwards(self, dates: Tuple[date]) -> None:
+        """Overrides scraper URL using date inputs
 
-    def _get_download_urls(self):
-        return [case["url"] for case in self.cases]
+        :param dates: (start_date, end_date) tuple
+        :return None
+        """
+        start, end = dates
+        self.url = self.base_url.format(
+            start.strftime(self.url_date_format),
+            end.strftime(self.url_date_format),
+        )
+        logger.info("Backscraping for range %s %s", start, end)
 
-    def _get_case_dates(self):
-        return [case["date"] for case in self.cases]
+    def make_backscrape_iterable(self, kwargs: Dict) -> None:
+        """Checks if backscrape start and end arguments have been passed
+        by caller, and parses them accordingly
 
-    def _get_docket_numbers(self):
-        return [case["docket"] for case in self.cases]
+        :param kwargs: passed when initializing the scraper, may or
+        may not contain backscrape controlling arguments
 
-    def _get_precedential_statuses(self):
-        return ["Published"] * len(self.cases)
+        :return None
+        """
+        start = kwargs.get("backscrape_start")
+        end = kwargs.get("backscrape_end")
+
+        if start:
+            start = datetime.strptime(start, "%m/%d/%Y")
+        else:
+            start = self.first_opinion_date
+        if end:
+            end = datetime.strptime(end, "%m/%d/%Y")
+        else:
+            end = datetime.now()
+
+        self.back_scrape_iterable = make_date_range_tuples(
+            start, end, self.days_interval
+        )
