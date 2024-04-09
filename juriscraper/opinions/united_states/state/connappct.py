463002588a0bbd13206cd879c5427818f0f63b19
Gianfranco Rossi <gianfranco.rossi.r@gmail.com>
2024-04-09 11:03:56 -0500

    feat(conn): implement dynamic backscraper
    Solves #987

--- juriscraper/opinions/united_states/state/connappct.py
+++ juriscraper/opinions/united_states/state/connappct.py
@@ -12,7 +12,8 @@ from juriscraper.opinions.united_states.state import conn
 
 
 class Site(conn.Site):
+    court_abbv = "ap"
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.court_id = self.__module__
-        self.url = f"http://www.jud.ct.gov/external/supapp/archiveAROap{self.year}.htm"
