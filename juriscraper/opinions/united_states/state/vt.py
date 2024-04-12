ba56cc21e64f73bee9912a935d4dc995443e8161
Gianfranco Rossi <gianfranco.rossi.r@gmail.com>
2024-04-12 10:04:21 -0500

    feat(vt): dynamic backscraper
    Helps solve #996

--- juriscraper/opinions/united_states/state/vt.py
+++ juriscraper/opinions/united_states/state/vt.py
@@ -11,23 +11,42 @@ If there are errors with the site, you can contact:
 She's very responsive.
 """
 
+from datetime import date, datetime
+from typing import Optional, Tuple
+from urllib.parse import urlencode
+
+from juriscraper.AbstractSite import logger
+from juriscraper.lib.date_utils import make_date_range_tuples
 from juriscraper.OpinionSiteLinear import OpinionSiteLinear
 
 
 class Site(OpinionSiteLinear):
+    base_url = "https://www.vermontjudiciary.org/opinions-decisions"
+    days_interval = 30
+    first_opinion_date = datetime(2000, 1, 1)
+    division = 7
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.court_id = self.__module__
-        self.division = 7
+        if self.division == 7:
+            self.first_opinion_date = datetime(1999, 5, 26)
         self.status = "Published"
-        self.url = f"https://www.vermontjudiciary.org/opinions-decisions?f%5B0%5D=court_division_opinions_library_%3A{self.division}"  # supreme
+        self.set_url()
+        self.make_backscrape_iterable(kwargs)
 
-    def _process_html(self):
+    def _process_html(self) -> None:
+        """Process HTML into case dictionaries
+
+        Source's page size is 25 rows
+
+        :return None
+        """
         for case in self.html.xpath(".//article"):
             name_url_span = case.xpath(
                 ".//div[contains(@class, 'views-field-name')]"
             )[0]
-            date = (
+            date_filed = (
                 case.xpath(
                     ".//div[contains(@class, 'views-field-field-document-expiration')]"
                 )[0]
@@ -45,7 +64,71 @@ class Site(OpinionSiteLinear):
                 {
                     "url": name_url_span.xpath(".//a/@href")[0],
                     "name": name_url_span.text_content(),
-                    "date": date,
+                    "date": date_filed,
                     "docket": docket,
                 }
             )
+
+    def _download_backwards(self, dates: Tuple[date]) -> None:
+        """Make custom date range request
+        :param dates: (start_date, end_date) tuple
+        :return None
+        """
+        logger.info("Backscraping for range %s %s", *dates)
+        self.set_url(*dates)
+        self.html = self._download()
+        self._process_html()
+
+    def make_backscrape_iterable(self, kwargs: dict) -> None:
+        """Checks if backscrape start and end arguments have been passed
+        by caller, and parses them accordingly
+
+        :param kwargs: passed when initializing the scraper, may or
+            may not contain backscrape controlling arguments
+        :return None
+        """
+        start = kwargs.get("backscrape_start")
+        end = kwargs.get("backscrape_end")
+
+        if start:
+            start = datetime.strptime(start, "%m/%d/%Y")
+        else:
+            start = self.first_opinion_date
+        if end:
+            end = datetime.strptime(end, "%m/%d/%Y")
+        else:
+            end = datetime.now()
+
+        self.back_scrape_iterable = make_date_range_tuples(
+            start, end, self.days_interval
+        )
+
+    def set_url(
+        self, start: Optional[date] = None, end: Optional[date] = None
+    ) -> None:
+        """Formats and sets `self.url` with date inputs
+        If no start or end dates are given, scrape without date filter values
+
+        There is a Document Type filter available, with an 'Opinion' value, that
+        can be added to params like this:
+            {"f[1]": "document_type:94"}a
+        However, we are not using it since some documents marked as 'Decisions'
+        also contain opinions. For example:
+        https://www.vermontjudiciary.org/sites/default/files/documents/2020-10-13-57.pdf
+        titled "Opinion and Order on Cross-Motions for Summary Judgment"
+        has 8 pages, most of which are argumentation
+
+        :param start: start date
+        :param end: end date
+        :return None
+        """
+        params = {
+            "facet_from_date": "",
+            "facet_to_date": "",
+            "f[0]": f"court_division_opinions_library_:{self.division}",  # filter by court
+        }
+        if start:
+            params["facet_from_date"] = start.strftime("%m/%d/%Y")
+            params["facet_to_date"] = end.strftime("%m/%d/%Y")
+
+        self.url = f"{self.base_url}?{urlencode(params)}"
