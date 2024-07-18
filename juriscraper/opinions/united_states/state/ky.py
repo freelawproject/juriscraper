6abe13b38ead8417c5884c6c9079d3b8544d5bd9
Gianfranco Rossi <gianfranco.rossi.r@gmail.com>
2024-07-17 22:41:28 -0500

    fix(ky, kyctapp): use less requests
    Also, update example files

--- juriscraper/opinions/united_states/state/ky.py
+++ juriscraper/opinions/united_states/state/ky.py
@@ -15,25 +15,29 @@ History:
     2024-04-08: Updated to use new opinion search portal, by grossir
 """
 
+import re
 from datetime import date, datetime, timedelta
 from typing import Dict, Optional, Tuple
 from urllib.parse import urlencode
 
 from juriscraper.AbstractSite import logger
+from juriscraper.lib.string_utils import titlecase
 from juriscraper.OpinionSiteLinear import OpinionSiteLinear
 
 
 class Site(OpinionSiteLinear):
+    # Home: https://appellatepublic.kycourts.net/login
     api_court = "Kentucky Supreme Court"
     api_url = (
         "https://appellatepublic.kycourts.net/api/api/v1/opinions/search?"
     )
-    api_docket_entry_url = "https://appellatepublic.kycourts.net/api/api/v1/publicaccessdocuments?filter=parentCategory%3Ddocketentries%2CparentID%3D{}"
-    api_document_url = (
-        "https://appellatepublic.kycourts.net/documents/{}/download"
-    )
-    api_case_url = "https://appellatepublic.kycourts.net/api/api/v1/cases/{}"
-    first_opinion_date = ""
+    docket_entry_url = "https://appellatepublic.kycourts.net/api/api/v1/publicaccessdocuments?filter=parentCategory%3Ddocketentries%2CparentID%3D{}"
+    pdf_url = "https://appellatepublic.kycourts.net/api/api/v1/publicaccessdocuments/{}/download"
+    first_opinion_date = datetime(1982, 2, 18)
+
+    # Examples: "BY JUDGE EASTON", "BY CHIEF JUSTICE VANMETER AFFIRMING",
+    # "BY JUDGE A. JONES"
+    judge_regex = r"BY(\sCHIEF)?\s(JUDGE|JUSTICE)\s(?P<judge>(\w\.\s)?\w+)"
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -69,68 +73,49 @@ class Site(OpinionSiteLinear):
         self.url = f"{self.api_url}{urlencode(params)}"
 
     def _process_html(self) -> None:
-        """Parse HTMl into case dicts
+        """Parse API JSON response into case dictionaries
 
         :return None
         """
-        json = self.html
+        result_json = self.html
 
-        for result in json["resultItems"]:
-            result = result["rowMap"]
-            docket_number = result["caseHeader.caseNumber"]
-            disposition = result["docketEntryDescription"]
-            status = self.get_status(result["docketEntrySubtype"])
-            date_filed = result["filedDate"]
+        for result in result_json["resultItems"]:
+            row = result["rowMap"]
+            docket_number = row["caseHeader.caseNumber"]
+            date_filed = row["filedDate"]
+            disposition = row["docketEntryDescription"]
 
-            # Doing 2 extra requests. However we could extract
-            # the case name from the opinion's text, which is sent
-            # already extracted from the server when querying the docket
-            # entry documents
+            judge = ""
+            if judge_match := re.search(self.judge_regex, disposition):
+                judge = judge_match.group("judge")
+
             if self.test_mode_enabled():
-                url = "placeholder"
-                case_name = "placeholder"
+                # detail request has been manually nested in the test file
+                doc_json = result["detailJson"]
             else:
-                url = self.get_opinion_url(result["docketEntryID"])
-                case_name = self.get_case_name(result["caseHeader.caseID"])
+                detail_url = self.docket_entry_url.format(row["docketEntryID"])
+                doc_json = self.get_json(detail_url)
+
+            case_name = doc_json[0]["caseHeader"].get("shortTitle")
+            doc_id = doc_json[0].get("documentID")
+            doc_text = doc_json[0]["documentText"][0]
+
+            if "\r\nTO BE PUBLISHED \r\n" in doc_text:
+                status = "Published"
+            else:
+                status = "Unpublished"
 
             self.cases.append(
                 {
-                    "url": url,
-                    "name": case_name,
+                    "url": self.pdf_url.format(doc_id),
+                    "name": titlecase(case_name),
                     "disposition": disposition,
-                    "status": status,
                     "docket": docket_number,
                     "date": date_filed,
+                    "status": status,
+                    "judge": judge,
                 }
             )
-
-    def get_opinion_url(self, docket_entry_id: str) -> str:
-        """Builds opinion URL using document id from docket entry detail page
-
-        :param docket_entry_id:
-
-        :return: opinion url
-        """
-        docket_entry_url = self.api_docket_entry_url.format(docket_entry_id)
-        doc_json = self.get_json(docket_entry_url)
-
-        if doc_json and doc_json[0].get("documentID"):
-            url = self.api_document_url.format(doc_json[0]["documentID"])
-        else:
-            url = ""
-            logger.info("No docket entry documents found %s", docket_entry_url)
-
-        return url
-
-    def get_case_name(self, case_id: str) -> str:
-        """Get case name from case page
-
-        :param case_id: case id
-        :return: case name
-        """
-        case_url = self.api_case_url.format(case_id)
-        case_json = self.get_json(case_url)
-        return case_json["shortTitle"]
 
     def get_json(self, url: str) -> Dict:
         """Get JSON from the API
@@ -141,17 +126,6 @@ class Site(OpinionSiteLinear):
         self._request_url_get(url)
         self._post_process_response()
         return self._return_response_text_object()
-
-    def get_status(self, entry_subtype: str) -> str:
-        """Get status from entry_subtype.
-
-        This function is overriden in inheriting classes
-        Returning 'Unknown' is kept from legacy scraper
-
-        :param entry_subtype: used in subclasses
-        :return: status string
-        """
-        return "Unknown"
 
     def make_backscrape_iterable(self, kwargs: dict) -> None:
         """Checks if backscrape start and end arguments have been passed
