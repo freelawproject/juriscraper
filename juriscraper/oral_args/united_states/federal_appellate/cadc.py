e67b80eb9b0a6a4a05853cca98157c2d9b9d600c
William Palin <bill@free.law>
2024-10-10 17:09:45 -0400

    feat(cadc): Fix OA cadc
    Update tests

--- juriscraper/oral_args/united_states/federal_appellate/cadc.py
+++ juriscraper/oral_args/united_states/federal_appellate/cadc.py
@@ -8,7 +8,6 @@ Date created: 18 July 2014
 Updated: 2024-10-10
 """
 
-import re
 from datetime import date
 
 from juriscraper.AbstractSite import logger
@@ -26,7 +25,7 @@ class Site(OralArgumentSiteLinear):
             yearmo=self.d.strftime("%Y/%m")
         )
         self.parameters = {"verify": False}
-        self.make_backscrape_iterable(kwargs)
+        # self.make_backscrape_iterable(kwargs)
 
     def _process_html(self):
         """"""
@@ -38,14 +37,15 @@ class Site(OralArgumentSiteLinear):
                 logger.info("Audio likely missing")
                 continue
             cells = row.xpath(".//div[@class='col-sm-9 ml-3 ml-sm-0']/div")
+
             self.cases.append(
                 {
                     "url": url,
                     "docket": docket,
                     "name": cells[0].text_content(),
                     "date": cells[3].text_content(),
-                    "attorney": cells[2].text_content(),
-                    "judges": cells[1].text_content(),
+                    # "attorney": cells[2].text_content(), #not yet implemented
+                    # "panel": cells[1].text_content(), #need to implement
                 }
             )
 
