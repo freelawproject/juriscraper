d1a47570065f2d71d6f2f27a88d29f1077f68b10
Gianfranco Rossi <gianfranco.rossi.r@gmail.com>
2024-11-11 20:35:36 -0500

    fix(kan, kanctapp): scraper now apply filters
    Solves #1222
    
    - scraper was not applying filters; wrong data must be cleaned on the DB level

--- juriscraper/opinions/united_states/state/kanctapp_u.py
+++ juriscraper/opinions/united_states/state/kanctapp_u.py
@@ -5,8 +5,5 @@ from juriscraper.opinions.united_states.state import kan_p
 
 
 class Site(kan_p.Site):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.court_id = self.__module__
-        self.status = "Unpublished"
-        self.court = "Court of Appeals"
+    court_filter = "Court of Appeals"
+    status_filter = "Unpublished"
