b8b031142bc2786c138d0524485dfb0d1f724359
Gianfranco Rossi <gianfranco.rossi.r@gmail.com>
2024-12-31 10:10:00 -0500

    fix(cadc): implement new site
    Fixes #1280
    
    Site had changed for cadc, cadc_pi, cadc_u

--- juriscraper/opinions/united_states/federal_appellate/cadc_pi.py
+++ juriscraper/opinions/united_states/federal_appellate/cadc_pi.py
@@ -7,36 +7,11 @@ History:
   2023-01-12: Fixed requests.exceptions.InvalidURL error by grossir
 """
 
-from urllib.parse import urljoin
-
-from juriscraper.OpinionSiteLinear import OpinionSiteLinear
+from juriscraper.opinions.united_states.federal_appellate import cadc
 
 
-class Site(OpinionSiteLinear):
+class Site(cadc.Site):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.url = "https://www.cadc.uscourts.gov/internet/orders.nsf"
-        self.base = "https://www.cadc.uscourts.gov"
-        self.court_id = self.__module__
-
-    def _process_html(self) -> None:
-        """Iterate over the public interest cases.
-
-        :return: None
-        """
-        for row in self.html.xpath(".//div[@class='row-entry']"):
-            url = row.xpath(".//a/@href")[0]
-            docket = row.xpath(".//a/span/text()")[0]
-            name = row.xpath(".//div[@class='column-two']/div[1]/text()")[
-                0
-            ].strip()
-            date = row.xpath(".//date/text()")[0]
-            self.cases.append(
-                {
-                    "date": date,
-                    "url": urljoin("https:", url),
-                    "docket": docket,
-                    "name": name,
-                    "status": "Published",
-                }
-            )
+        # https://media.cadc.uscourts.gov/orders/
+        self.url = "https://media.cadc.uscourts.gov/orders/bydate/recent"
