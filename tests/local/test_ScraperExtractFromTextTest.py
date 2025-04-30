e1fdcd0b10bb763eb1226c307c661cef6d8234f7
“Kevin <kvnzavalza@hotmail.com>
2025-04-30 10:32:24 -0600

    fix(bia): update bia scraper add test cases update CHANGES.md

--- tests/local/test_ScraperExtractFromTextTest.py
+++ tests/local/test_ScraperExtractFromTextTest.py
@@ -31,6 +31,24 @@ class ScraperExtractFromText(unittest.TestCase):
                     }
                 },
             ),
+            (
+                "Matter of O-A-R-G-, et al., Respondents\nDecided as amended April 16, 2025 1\n",
+                {
+                    "OpinionCluster": {
+                        "date_filed": "2025-04-16",
+                        "date_filed_is_approximate": False,
+                    }
+                },
+            ),
+            (
+                "Matter of Leobardo DE JESUS-PLATON, Respondent\nDecided by Board January 17, 2025 1\n",
+                {
+                    "OpinionCluster": {
+                        "date_filed": "2025-01-17",
+                        "date_filed_is_approximate": False,
+                    }
+                },
+            ),
         ],
         "juriscraper.opinions.united_states.state.nm": [
             (
