80c4e16c66b50825268310a7f2685cf52fd5eb65
William Palin <bill@free.law>
2025-05-21 16:25:43 -0400

    feat(prapp): Remove locale
    Locale was causing issues locally
    on other tests so i removed it
    just to be safe

--- juriscraper/opinions/united_states/territories/prapp.py
+++ juriscraper/opinions/united_states/territories/prapp.py
@@ -6,7 +6,6 @@ CourtID: prapp
 Court Short Name: Puerto Rico Court of Apelaciones
 """
 
-import locale
 from datetime import date, datetime
 
 from dateparser import parse
@@ -21,20 +20,22 @@ class Site(OpinionSiteLinear):
     first_opinion_date = "2015/01/01"
     today = today_str = datetime.now().strftime("%Y/%m/%d")
     base_url = "https://poderjudicial.pr/tribunal-apelaciones/decisiones-finales-del-tribunal-de-apelaciones"
+    is_backscrape = False
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.court_id = self.__module__
         self.year = date.today().year
-        locale.setlocale(locale.LC_TIME, "es_ES.UTF-8")
-        self.current_month = datetime.now().strftime("%B")
+        self.current_month = self.format_spanish_month_year(datetime.now())
 
         self.url = self.base_url
         self.status = "Published"
         self.make_backscrape_iterable(kwargs)
 
     def _download(self):
-        """Download websites
+        """Download case pages
+
+        If a backscrape ignore the need to find the correct page
 
         :param request_dict: Empty dict
         :return: HTML object
@@ -43,15 +44,17 @@ class Site(OpinionSiteLinear):
             return super()._download()
         if not self.html:
             self.html = super()._download()
-        latest_url = self.html.xpath(
-            f"(//a[contains(@href,'{self.year}') and contains(@href,'decisiones-del-tribunal-de-apelaciones')])[last()]/@href"
-        )
-        if not latest_url:
-            logger.debug(f"No opinions posted for {self.year}")
-            self.html = None
-            return
 
-        self.url = latest_url[0]
+        if not self.is_backscrape:
+            latest_url = self.html.xpath(
+                f"(//a[contains(@href,'{self.year}') and contains(@href,'decisiones-del-tribunal-de-apelaciones')])[last()]/@href"
+            )
+            if not latest_url:
+                logger.debug(f"No opinions posted for {self.year}")
+                self.html = None
+                return
+
+            self.url = latest_url[0]
         if self.current_month not in self.url:
             logger.debug(
                 f"No opinions yet posted for current month {self.current_month}, moving to most recent month."
@@ -60,7 +63,7 @@ class Site(OpinionSiteLinear):
         return super()._download()
 
     def _process_html(self):
-        """Process html
+        """Process the html
 
         Long delays between posting necessitate a few extra warnings around
         Years and months missing
@@ -103,7 +106,7 @@ class Site(OpinionSiteLinear):
 
         months = []
         while current <= end_date.replace(day=1):
-            months.append(current.strftime("%B-%Y"))
+            months.append(self.format_spanish_month_year(current))
             current += relativedelta(months=1)
 
         self.back_scrape_iterable = months
@@ -119,3 +122,29 @@ class Site(OpinionSiteLinear):
         self.url = f"{self.base_url}/decisiones-del-tribunal-de-apelaciones-{year_month}/"
         self.html = self._download()
         self._process_html()
+
+    def format_spanish_month_year(self, datetime_obj: datetime) -> str:
+        """Generate month year slug in spanish
+
+        Use this method to avoid issues with changing locale to spanish
+
+        :param datetime_obj: Datetime obj
+        :return: spanish month year slug
+        """
+        SPANISH_MONTHS = [
+            None,
+            "enero",
+            "febrero",
+            "marzo",
+            "abril",
+            "mayo",
+            "junio",
+            "julio",
+            "agosto",
+            "septiembre",
+            "octubre",
+            "noviembre",
+            "diciembre",
+        ]
+
+        return f"{SPANISH_MONTHS[datetime_obj.month]}-{datetime_obj.year}"
