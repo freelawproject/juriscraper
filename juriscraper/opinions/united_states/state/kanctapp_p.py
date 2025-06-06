60239f0dbda588e2fcc1960b72de242fcc24857a
“Kevin <kvnzavalza@hotmail.com>
2025-06-05 20:11:02 -0600

    feat(kan*): update kan and kanctapp scrapers update examples

--- juriscraper/opinions/united_states/state/kanctapp_p.py
+++ juriscraper/opinions/united_states/state/kanctapp_p.py
@@ -1,9 +1,15 @@
-# Scraper for Kansas Appeals Court (published)
-# CourtID: kanctapp_p
+"""
+CourtID: kanctapp_p
+Court Short Name: Kansas Appeals Court (published)
+History:
+    2025-06-05, quevon24: Implemented new site
+"""
+from datetime import datetime
 
 from juriscraper.opinions.united_states.state import kan_p
 
 
 class Site(kan_p.Site):
-    court_filter = "Court of Appeals"
-    status_filter = "Published"
+    court_filter = 20 # Court of Appeals
+    status_filter = 1  # Published
+    first_opinion_date = datetime(1986, 2, 27)
--- juriscraper/opinions/united_states/state/kanctapp_u.py
+++ juriscraper/opinions/united_states/state/kanctapp_u.py
@@ -1,9 +1,15 @@
-# Scraper for Kansas Appeals Court (unpublished)
-# CourtID: kanctapp_u
+"""
+CourtID: kanctapp_u
+Court Short Name: Kansas Appeals Court (unpublished)
+History:
+    2025-06-05, quevon24: Implemented new site
+"""
+from datetime import datetime
 
 from juriscraper.opinions.united_states.state import kan_p
 
 
 class Site(kan_p.Site):
-    court_filter = "Court of Appeals"
-    status_filter = "Unpublished"
+    court_filter = 20  # Court of Appeals
+    status_filter = 0  # Published
+    first_opinion_date = datetime(2015, 8, 1)
