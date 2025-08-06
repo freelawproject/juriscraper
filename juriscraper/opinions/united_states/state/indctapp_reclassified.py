7876ae92752a4c56349ca415a702631d988f82ca
luism <luismanzur91@outlook.com>
2025-08-06 15:12:12 -0400

    fix(indctapp): improve handling of links and enhance case data structure

--- juriscraper/opinions/united_states/state/indctapp_reclassified.py
+++ juriscraper/opinions/united_states/state/indctapp_reclassified.py
@@ -30,37 +30,37 @@ class Site(OpinionSiteLinear):
         for row in rows:
             cells = row.xpath(".//td")
             link = cells[2].xpath(".//a")
-            if link:
-                name_parts = link[0].xpath(".//p")
-                if name_parts:
-                    name = titlecase(
-                        " ".join(p.text.strip() for p in name_parts)
-                    )
-                else:
-                    name = titlecase(link[0].text.strip())
-                url = link[0].get("href")
+            if not link:
+                continue
 
-                lower_court_parts = cells[3].xpath(".//p")
-                lower_court = (
-                    ", ".join(p.text.strip() for p in lower_court_parts)
-                    if lower_court_parts
-                    else cells[3].text.strip()
-                )
+            name_parts = link[0].xpath(".//p")
+            if name_parts:
+                name = titlecase(" ".join(p.text.strip() for p in name_parts))
+            else:
+                name = titlecase(link[0].text.strip())
+            url = link[0].get("href")
 
-                docket_parts = cells[4].xpath(".//p")
-                docket = (
-                    docket_parts[0].text.strip()
-                    if docket_parts
-                    else cells[4].text.strip()
-                )
+            lower_court_parts = cells[3].xpath(".//p")
+            lower_court = (
+                ", ".join(p.text.strip() for p in lower_court_parts)
+                if lower_court_parts
+                else cells[3].text.strip()
+            )
 
-                self.cases.append(
-                    {
-                        "date": cells[0].text.strip(),
-                        "other_date": cells[1].text.strip(),
-                        "name": name,
-                        "url": url,
-                        "lower_court": lower_court,
-                        "docket": docket,
-                    }
-                )
+            docket_parts = cells[4].xpath(".//p")
+            docket = (
+                docket_parts[0].text.strip()
+                if docket_parts
+                else cells[4].text.strip()
+            )
+
+            self.cases.append(
+                {
+                    "date": cells[0].text.strip(),
+                    "other_date": f"Date opinion was reclassified as published: {cells[1].text.strip()}",
+                    "name": name,
+                    "url": url,
+                    "lower_court_number": lower_court,
+                    "docket": docket,
+                }
+            )
