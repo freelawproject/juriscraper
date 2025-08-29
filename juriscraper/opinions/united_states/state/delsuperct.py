6eb4e1405b018eb66e9a93c408ec36bfa694200a
luism <luismanzur91@outlook.com>
2025-08-29 16:12:13 -0400

    feat(dc): implement lower court extraction in Delaware scraper

--- juriscraper/opinions/united_states/state/delsuperct.py
+++ juriscraper/opinions/united_states/state/delsuperct.py
@@ -6,9 +6,12 @@
 # Date created: 31 July 2014
 
 from juriscraper.opinions.united_states.state import delaware
+from juriscraper.OpinionSite import OpinionSite
 
 
 class Site(delaware.Site):
+    extract_from_text = OpinionSite.extract_from_text
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.url = (
