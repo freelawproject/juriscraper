655dfac36e1246cae8d726ba5418fcaba709849f
luism <luismanzur91@outlook.com>
2025-09-10 16:39:43 +0100

    feat(utahctapp):  use extract_from_text base method

--- juriscraper/opinions/united_states/state/utahctapp.py
+++ juriscraper/opinions/united_states/state/utahctapp.py
@@ -1,7 +1,10 @@
+from juriscraper.OpinionSite import OpinionSite
 from juriscraper.opinions.united_states.state import utah
 
 
 class Site(utah.Site):
+    extract_from_text = OpinionSite.extract_from_text
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.url = "https://legacy.utcourts.gov/opinions/appopin/"
