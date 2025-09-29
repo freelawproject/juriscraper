34fa6a64860073b9c947f1853a8b7b114f9a3d17
luism <luismanzur91@outlook.com>
2025-09-29 15:14:20 -0400

    feat(proxy): add DECODO proxy environment variable checks in request handler

--- juriscraper/OpinionSiteLinearProxyRequestHandler.py
+++ juriscraper/OpinionSiteLinearProxyRequestHandler.py
@@ -1,9 +1,20 @@
+import os
+
 from juriscraper.OpinionSiteLinear import OpinionSiteLinear
-from juriscraper.ProxyRequestHandler import ProxyRequestHandler
 
 
 class OpinionSiteLinearProxyRequestHandler(
-    OpinionSiteLinear, ProxyRequestHandler
+    OpinionSiteLinear
 ):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
+    def _download(self, request_dict=None):
+        USERNAME = os.environ.get("DECODO_USER")
+        PASSWORD = os.environ.get("DECODO_PASSWORD")
+        PROXY_ADDRESS = os.environ.get("DECODO_PROXY")
+        PORT = os.environ.get("DECODO_PORT")
+        proxy_values = [USERNAME, PASSWORD, PROXY_ADDRESS, PORT]
+        if not all(proxy_values) and not self.test_mode_enabled():
+            raise ValueError("Missing DECODO proxy environment variables")
+        return super()._download(request_dict)
