ceb0fd3d907fbca21e1cf4cc5154ec579f00d138
Morgan Bennet <98787614+MorganBennetDev@users.noreply.github.com>
2025-11-14 15:24:30 -0600

    refactor(scotus): move validity checks to data method
    Moves validity checks out of each individual parse method and into the data method. Adds additional checks to `_parse_text` before setting `is_valid` to true

--- juriscraper/scotus/scotus_email.py
+++ juriscraper/scotus/scotus_email.py
@@ -1,8 +1,8 @@
 import email
-import os.path
 import re
 from datetime import datetime
 from email.message import EmailMessage
+from pathlib import Path
 from typing import Optional
 from urllib.parse import parse_qs, urlparse
 
@@ -30,24 +30,27 @@ class SCOTUSEmail:
         """Extract all the data in the email into the following format:
 
         {
-            "filing_name": "Amicus brief of Corey J. Biazzo, Esq. submitted.",
-            "case_name": "Donald J. Trump, President of the United States, et al., Petitioners v. V.O.S. Selections, Inc., et al.",
-            "case_link": "https://www.supremecourt.gov/search.aspx?filename=/docket/DocketFiles/html/Public/25-250.html",
             "docket_number": "25-250",
-            "notification_datetime": "2025-10-09 02:04:05+00:00"
+            "notification_datetime": "2025-10-09 02:04:05+00:00",
+            "case_name": "Donald J. Trump, President of the United States v. V.O.S. Selections, Inc.",
+            "case_url": "https://www.supremecourt.gov/search.aspx?filename=/docket/DocketFiles/html/Public/25-250.html",
+            "entry_description": "Amicus brief of Corey J. Biazzo, Esq. submitted."
         }
 
-        :return: `dict` of data extracted from the email. A field will be set
-        to `None` if extraction of that specific field from the email failed or
-        if there was an error while parsing the email.
+        :return: `dict` of data extracted from the email. Will return `{}` if
+        email parsing has not occurred or has failed. A field will be set to
+        `None` if extraction of that specific field from the email fails.
         """
-        return {
-            "docket_number": self._parse_docket_number(),
-            "notification_datetime": self._parse_datetime(),
-            "case_name": self._parse_case_name(),
-            "case_url": self._parse_case_link(),
-            "entry_description": self._parse_filing_name(),
-        }
+        if self.is_valid:
+            return {
+                "docket_number": self._parse_docket_number(),
+                "notification_datetime": self._parse_datetime(),
+                "case_name": self._parse_case_name(),
+                "case_url": self._parse_case_link(),
+                "entry_description": self._parse_filing_name(),
+            }
+        else:
+            return {}
 
     def _parse_text(self, text: str) -> None:
         """Extract and store the first part of the email with the "Content-Type"
@@ -61,6 +64,10 @@ class SCOTUSEmail:
         """
         self.message = email.message_from_string(text)
 
+        if self.message.get("Date") is None:
+            logger.error("Unable to find 'Date' header in email")
+            return
+
         content_part = next(
             (
                 part
@@ -70,6 +77,7 @@ class SCOTUSEmail:
             ),
             None,
         )
+
         if content_part is None:
             logger.error(
                 "Unable to find non-multipart content part with 'text/html' content type in email"
@@ -88,6 +96,22 @@ class SCOTUSEmail:
             return
 
         self.tree = html.fromstring(clean_html(body))
+
+        n_anchors = sum(1 for _ in self.tree.iterfind(".//a"))
+
+        if n_anchors < 2:
+            logger.error(
+                "Unable to find at least two links in email body (should be case link and unsubscribe link)"
+            )
+            return
+
+        text = self.tree.text_content()
+        if self.TITLE_REGEX.match(text) is None:
+            logger.error(
+                "Unable to find match for docket entry title regex in email"
+            )
+            return
+
         self.is_valid = True
 
     def _parse_datetime(self) -> Optional[datetime]:
@@ -95,82 +119,55 @@ class SCOTUSEmail:
         `datetime`, returning `None` if the header is absent or parsing fails
         and logging an appropriate error.
 
-        :return: `datetime` or `None` if unable to extract or parse the "Date" header.
+        :return: `datetime` or `None` if unable to parse the "Date" header.
         """
-        if self.is_valid:
-            message_date = self.message.get("Date")
+        message_date = self.message.get("Date")
 
-            if message_date:
-                try:
-                    return datetime.strptime(
-                        message_date, "%a, %d %b %Y %H:%M:%S %z"
-                    )
-                except ValueError:
-                    logger.error(
-                        "Unable to parse 'Date' header of email (value is '%s')",
-                        message_date,
-                    )
-                    return None
-            else:
-                logger.error("Unable to find 'Date' header of email")
-                return None
-        else:
+        try:
+            return datetime.strptime(message_date, "%a, %d %b %Y %H:%M:%S %z")
+        except ValueError:
+            logger.error(
+                "Unable to parse 'Date' header of email (value is '%s')",
+                message_date,
+            )
             return None
 
-    def _parse_filing_name(self) -> Optional[str]:
+    def _parse_filing_name(self) -> str:
         """Extract the docket entry title from the email body using a regex
         and clean it with the `clean_string` method. Return `None` and log
         an error if the regex fails to match the email.
 
-        :return: Clean docket entry title or `None` if unable to extract the title.
+        :return: Clean docket entry title.
         """
-        if self.is_valid:
-            text = self.tree.text_content()
-            match = self.TITLE_REGEX.match(text)
+        text = self.tree.text_content()
+        match = self.TITLE_REGEX.match(text)
 
-            if match is None:
-                logger.error(
-                    "Unable to find match for docket entry title regex in email"
-                )
-                return None
-            else:
-                return clean_string(match.group(1))
-        else:
-            return None
+        return clean_string(match.group(1))
 
-    def _parse_case_name(self) -> Optional[str]:
+    def _parse_case_name(self) -> str:
         """Extract the case name from the first `<a>` tag in the email body
         and uses the `harmonize` method to clean it.
 
-        :return: Cleaned case name or `None` if unable to extract the case name.
+        :return: Cleaned case name.
         """
-        if self.is_valid:
-            return harmonize(self.tree.findtext(".//a"))
-        else:
-            return None
+        return harmonize(self.tree.findtext(".//a"))
 
-    def _parse_case_link(self) -> Optional[str]:
+    def _parse_case_link(self) -> str:
         """Extract the `href` attribute from the first `<a>` tag in the email
         body, which should be the link to the case.
 
-        :return: Link to the case or `None` if unable to extract the link.
+        :return: Link to the case.
         """
-        if self.is_valid:
-            return self.tree.find(".//a").get("href")
-        else:
-            return None
+        return self.tree.find(".//a").get("href")
 
-    def _parse_docket_number(self) -> Optional[str]:
+    def _parse_docket_number(self) -> str:
         """Extract the docket number using the `href` attribute from the first
         `<a>` tag in the email body.
 
-        :return: Docket number or `None` if unable to extract the docket number.
+        :return: Docket number.
         """
-        if self.is_valid:
-            file = parse_qs(
-                urlparse(self.tree.find(".//a").get("href")).query
-            )["filename"][0]
-            (docket_number, _) = os.path.splitext(os.path.basename(file))
-            return clean_string(docket_number)
-        else:
-            return None
+        file = parse_qs(urlparse(self.tree.find(".//a").get("href")).query)[
+            "filename"
+        ][0]
+        docket_number = Path(file).stem
+        return clean_string(docket_number)
