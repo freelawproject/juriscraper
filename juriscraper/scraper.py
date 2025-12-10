2267268bd095df4e32b2e6907bf4ebfbd2cf528d
Morgan Bennet <98787614+MorganBennetDev@users.noreply.github.com>
2025-12-10 09:32:54 -0700

    docs(scraper): add docstrings for Scraper ABC
    Add docstrings explaining basic usage of the Scraper Abstract Base Class

--- juriscraper/scraper.py
+++ juriscraper/scraper.py
@@ -5,15 +5,18 @@ T = TypeVar("T")
 
 
 class Scraper(Generic[T], ABC):
+    """Abstract base class, which all scrapers should inherit from."""
     @abstractmethod
     def __init__(self, court_id: str):
         self.court_id: str = court_id
 
     @abstractmethod
     def _parse_text(self, text: str) -> None:
+        """Ingest some arbitrary text and prepare it to be parsed by the data() method."""
         pass
 
     @property
     @abstractmethod
     def data(self) -> T:
+        """Parse the last text ingested by _parse_text()."""
         pass
