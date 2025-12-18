6cef23e4c2040cd2256372ea5969ac3c8d1c9886
luism <luismanzur91@outlook.com>
2025-12-18 17:01:03 -0400

    fix(mass): update scraper to use new HTML endpoint after URL change

--- juriscraper/opinions/united_states/state/mass.py
+++ juriscraper/opinions/united_states/state/mass.py
@@ -13,14 +13,16 @@ History:
  - 2017-11-29, arderyp: Moved from RSS source to HTML
     parsing due to website redesign
  - 2023-01-28, William Palin: Updated scraper
+ - 2025-12-18, Luis Manzur: Updated scraper
 """
 
 import re
-from datetime import date, datetime
-from urllib.parse import urljoin
+from datetime import date, datetime, timedelta
 
+from dateutil.relativedelta import relativedelta
 from lxml import etree, html
 
+from juriscraper.AbstractSite import logger
 from juriscraper.lib.date_utils import unique_year_month
 from juriscraper.lib.html_utils import strip_bad_html_tags_insecure
 from juriscraper.lib.string_utils import titlecase
@@ -29,18 +31,15 @@ from juriscraper.OpinionSiteLinear import OpinionSiteLinear
 
 class Site(OpinionSiteLinear):
     court_name = "Supreme Judicial Court"
-    first_opinion_date = datetime(2017, 6, 20)
+    first_opinion_date = datetime.now() - relativedelta(months=3) #the site only have last 3 month available
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.url = "https://www.socialaw.com/customapi/slips/getopinions"
-        self.court_id = self.__module__
-        self.search_date = datetime.today()
-        self.parameters = {
-            "SectionName": self.court_name,
-            "ArchiveDate": self.search_date.strftime("%B %Y"),
+        self.url = f"https://www.socialaw.com/services/slip-opinions/"
+        self.request["parameters"]["params"] = {
+            "Court": self.court_name,
         }
-        self.method = "POST"
+        self.court_id = self.__module__
         self.status = "Published"
         self.expected_content_types = ["text/html"]
         self.days_interval = 30
@@ -51,33 +50,24 @@ class Site(OpinionSiteLinear):
 
         :return: None
         """
-        for row in self.html:
-            url = urljoin(
-                "https://www.socialaw.com/services/slip-opinions/",
-                row["UrlName"],
-            )
-            details = row["Details"]
-            caption = titlecase(row.get("Parties"))
-            caption = re.sub(r"(\[\d{1,2}\])", "", caption)
+        rows = self.html.xpath("//div[@class='accordion-item']")
 
-            judge_str = details.get("Present", "")
-            judge_str = re.sub(r"(\[\d{1,2}\])", "", judge_str)
-            judge_str = re.sub(r"\, JJ\.", "", judge_str)
-            judge_str = re.sub(
-                r"(Associate\s+)?Justice*|of the Superior Court", "", judge_str
-            )
+        for row in rows[2::]:
+            name = row.xpath(".//strong[@class='title sh2']")[0].text.strip()
 
-            # Clear judge_str if it matches a date like 'July 16, 2024'
-            if re.match(r"^[A-Za-z]+\s+\d{1,2},\s+\d{4}$", judge_str.strip()):
-                judge_str = ""
+            #clean name participants enum
+            name = titlecase(re.sub(r"\[\d\]", "", name))
+
+            date = row.xpath(".//span[@class='date sh3']")[0].text.strip()
+            docket = row.xpath(".//div[@class='section-header']/div[@class='rich-text rich-text-sm']")[0].text.strip()
+            url = row.xpath(".//a")[0].get("href")
 
             self.cases.append(
                 {
-                    "name": caption,
-                    "judge": judge_str,
-                    "date": row["Date"],
+                    "name": name,
+                    "date": date,
+                    "docket": docket,
                     "url": url,
-                    "docket": details["Docket"],
                 }
             )
 
@@ -107,10 +97,11 @@ class Site(OpinionSiteLinear):
         :return None; sets the target date, downloads the corresponding HTML
         and processes the HTML to extract case details.
         """
-        self.search_date = search_date
-        self.parameters = {
-            "SectionName": self.court_name,
-            "ArchiveDate": self.search_date.strftime("%B %Y"),
+
+        logger.info("Backscraping for %s", search_date.strftime("%B %Y"))
+        self.request["parameters"]["params"] = {
+            "Court": self.court_name,
+            "Month": search_date.strftime("%B %Y"),
         }
         self.html = self._download()
         self._process_html()
@@ -121,6 +112,7 @@ class Site(OpinionSiteLinear):
         :param kwargs: the back scraping params
         :return: None
         """
+
         super().make_backscrape_iterable(kwargs)
         self.back_scrape_iterable = unique_year_month(
             self.back_scrape_iterable
