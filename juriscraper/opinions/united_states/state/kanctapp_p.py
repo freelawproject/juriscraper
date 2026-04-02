2071e7dffa118d7fe407dc08fb93f444bc3dd1ec
grossir@bigbuba <gianfranco.rossi.r@gmail.com>
2026-04-02 13:00:29 -0500

    refactor(kan,kanctapp): separate scrapers by status
    Will help back scrape kanctapp_u #1262

--- juriscraper/opinions/united_states/state/kanctapp_p.py
+++ juriscraper/opinions/united_states/state/kanctapp_p.py
@@ -1,18 +1,19 @@
 """
-Scraper for Kansas Court of Appeals
-CourtID: kanctapp
+Scraper for Kansas Court of Appeals - Published
+CourtID: kanctapp_p
 Court Short Name: Kansas Ct. App.
 Author: William Palin
 Court Contact:
 History:
  - 2023-01-04: Created.
  - 2026-03-19: Refactored from WebDriven to OpinionSiteLinear. Added backscraper. grossir
+ - 2026-04-02: Split into kanctapp_p and kanctapp_u by status. grossir
 """
 
-from juriscraper.opinions.united_states.state import kan
+from juriscraper.opinions.united_states.state import kan_p
 
 
-class Site(kan.Site):
+class Site(kan_p.Site):
     court_string = "Court of Appeals"
     court_filter = "20"
     days_interval = 15
