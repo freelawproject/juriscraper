f50cdae9be984d0a731c7f5bfdfd291e66e49d14
Brennan Chesley <brennan@free.law>
2026-08-18 11:54:23 -0500

    feat(nycoa) add enums from NYCoA

---
+++ juriscraper/state/new_york/nycourts_gov/__init__.py
@@ -0,0 +1,15 @@
+"""
+New York Court of Appeals Court-PASS vocabularies.
+"""
+
+from .vocabularies import (
+    COURT_GENERATED_DOCTYPES,
+    DOCTYPE_LABELS,
+    CourtVocabulary,
+)
+
+__all__ = [
+    COURT_GENERATED_DOCTYPES,
+    DOCTYPE_LABELS,
+    CourtVocabulary,
+]
