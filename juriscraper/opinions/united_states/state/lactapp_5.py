9d477216468d7a515f548b7cec08302ab0c5787e
renatodvc <40128530+renatodvc@users.noreply.github.com>
2026-09-15 19:10:00 -0300

    Refactor lactapp_5 to scrape from latest decisions window

--- juriscraper/opinions/united_states/state/lactapp_5.py
+++ juriscraper/opinions/united_states/state/lactapp_5.py
@@ -1,8 +1,7 @@
 import re
-from datetime import date, datetime
 
 from juriscraper.AbstractSite import logger
-from juriscraper.lib.date_utils import unique_year_month
+from juriscraper.lib.exceptions import BotChallengeError, ParsingException
 from juriscraper.lib.string_utils import titlecase
 from juriscraper.OpinionSiteLinear import OpinionSiteLinear
 
@@ -16,51 +15,46 @@ class Site(OpinionSiteLinear):
         "lblLowerCourt": "lower_court",
         "lblAttorney": "attorney",
     }
-    first_opinion_date = datetime(1992, 1, 1)
-    days_interval = 28  # ensure a tick for each month
     date_regex = re.compile(r"\d{2}/\d{2}/\d{4}")
+    panel_prefix = "cntBody_ctlOpinions"
+    count_xpath = f"//span[@id='{panel_prefix}_lblRecordCnt']"
+    row_xpath = (
+        "//tr[.//span[starts-with(@id, "
+        f"'{panel_prefix}_rptCaseSearch_lblCaseNum')]]"
+    )
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.court_id = self.__module__
-        self.url = "https://www.fifthcircuit.org/searchopinions.aspx"
-        self.search_is_configured = False
-        self.parameters = {
-            "ctl00$cntBody$ctlOpinionSearch_Toggle$ddlSearchOptions": "2",
-        }
-        self.target_date = datetime.today()
-        self.make_backscrape_iterable(kwargs)
+        self.url = "https://www.fifthcircuit.org/"
         self.status = "Unknown"
 
+        # Kwargs from backscrape callers
+        if {"backscrape_start", "backscrape_end", "days_interval"} & set(
+            kwargs
+        ):
+            raise NotImplementedError(
+                f"{self.court_id} has no backscraper. Only the reCaptcha "
+                "protected search form can filter opinions by date"
+            )
+
     async def _process_html(self):
-        # We need to do a plain GET to get hidden inputs
-        # Then we can do our filtered request
-        if not self.test_mode_enabled():
-            self.method = "POST"
+        self.check_panel_is_present()
 
-            # We need to set the proper search filter the first time
-            if not self.search_is_configured:
-                self.update_hidden_inputs()
-                self.parameters["__EVENTTARGET"] = (
-                    "ctl00$cntBody$ctlOpinionSearch_Toggle$ddlSearchOptions"
-                )
-                self.html = await self._download()
-                self.search_is_configured = True
-
-            # Set the proper filters to get the actual data we want
-            self.update_date_filters()
-            self.update_hidden_inputs()
-            self.html = await self._download()
-
-        count_xpath = "//*[@id='cntBody_ctlOpinionSearch_Toggle_lblRecordCnt']"
-        logger.info(self.html.xpath(count_xpath)[0].text_content().strip())
-
-        for row in self.html.xpath("//tr[.//a[contains(@id, 'HyperLink_')]]"):
+        for row in self.html.xpath(self.row_xpath):
             fixed_values = {}
             for id_part, key in self.id_to_case_mapper.items():
                 element = row.xpath(f".//*[contains(@id, '{id_part}')]")
                 if element:
                     fixed_values[key] = element[0].text_content().strip()
+
+            if not fixed_values.get("name"):
+                logger.warning(
+                    "%s: no case title for docket %s; skipping row",
+                    self.court_id,
+                    fixed_values.get("docket"),
+                )
+                continue
 
             fixed_values["name"] = titlecase(fixed_values["name"])
             if fixed_values.get("judge"):
@@ -68,13 +62,10 @@ class Site(OpinionSiteLinear):
                     r"Hon\.[\s\n]+", "", fixed_values["judge"]
                 )
 
-            # Some cases have more than 1 opinion document (check example 2)
-            # Some cases have no links, they will be ignored by this loop
-            for anchor in row.xpath(".//a"):
-                # The opinion date is sometimes in the disposition text
+            # Cases can have more than 1 opinion document
+            for anchor in row.xpath(".//a[contains(@id, 'HyperLink_')]"):
                 disposition = ""
-                case_date = f"{self.target_date.year}/07/01"
-                date_filed_is_approximate = True
+                case_date = ""
                 if disp_container := anchor.xpath("following-sibling::text()"):
                     disposition = disp_container[0].strip()
 
@@ -83,51 +74,53 @@ class Site(OpinionSiteLinear):
                         disposition = disposition.rsplit(" on ", 1)[0].strip(
                             " '"
                         )
-                        date_filed_is_approximate = False
+
+                if not case_date:
+                    logger.error(
+                        "%s: no date in disposition %r for docket %s; "
+                        "skipping document %s",
+                        self.court_id,
+                        disposition,
+                        fixed_values.get("docket"),
+                        anchor.get("href"),
+                    )
+                    continue
 
                 case = {
                     "url": anchor.get("href"),
                     "disposition": disposition,
                     "date": case_date,
-                    "date_filed_is_approximate": date_filed_is_approximate,
+                    "date_filed_is_approximate": False,
                     **fixed_values,
                 }
 
                 self.cases.append(case)
 
-    def update_hidden_inputs(self) -> None:
-        """Parse form values characteristic of aspx sites,
-        and put then on self.parameters for POST use
+    def check_panel_is_present(self) -> None:
+        """Check that the "Latest Decisions" panel returned decisions.
+
+        :raises BotChallengeError: when the page serves a captcha challenge
+        :raises ParsingException: when the panel is missing or empty
         """
-        for input in self.html.xpath('//input[@type="hidden"]'):
-            self.parameters[input.get("name")] = input.get("value", "")
+        if self.html.xpath(
+            "//*[contains(@class, 'h-captcha')]"
+        ) or self.html.xpath("//title[contains(., 'Bot Manager Captcha')]"):
+            raise BotChallengeError(
+                f"{self.court_id}: served a captcha challenge instead of the homepage",
+                fingerprint=[f"{self.court_id}-bot-challenge"],
+            )
 
-    def update_date_filters(self) -> None:
-        """Set year and month values from `self.target_date`
-        into self.parameters for POST use
-        """
-        logger.info(
-            "Scraping for year: %s - month: %s",
-            self.target_date.year,
-            self.target_date.month,
-        )
-        self.parameters = {
-            "ctl00$cntBody$ctlOpinionSearch_Toggle$ddlOpnMonth": str(
-                self.target_date.month
-            ),
-            "ctl00$cntBody$ctlOpinionSearch_Toggle$ddlOpnYear": str(
-                self.target_date.year
-            ),
-            "ctl00$cntBody$ctlOpinionSearch_Toggle$btnSearch": "Search",
-        }
+        count = self.html.xpath(self.count_xpath)
+        if not count:
+            raise ParsingException(
+                f"{self.court_id}: no 'Latest Decisions' record count on "
+                f"{self.url}; the page layout may have changed"
+            )
 
-    async def _download_backwards(self, target_date: date) -> None:
-        self.target_date = target_date
-        self.html = await self._download()
-        await self._process_html()
+        logger.info(count[0].text_content().strip())
 
-    def make_backscrape_iterable(self, kwargs):
-        super().make_backscrape_iterable(kwargs)
-        self.back_scrape_iterable = unique_year_month(
-            self.back_scrape_iterable
-        )
+        if not self.html.xpath(self.row_xpath):
+            raise ParsingException(
+                f"{self.court_id}: the 'Latest Decisions' panel on {self.url} "
+                "has no rows"
+            )
