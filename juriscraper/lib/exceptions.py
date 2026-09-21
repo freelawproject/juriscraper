83dd09b8ae180dda44c81c3d1fde80d3ce52d83a
Chad Weider <cweider@oofn.net>
2026-09-21 14:56:57 -0700

    chore(types): remove empty `__init__()` definitions in `exceptions`
    Rather than adding annotations, drop the implementation, which is
    unnecessary (super's implementation will be sufficient).
    
    ```
    ERROR juriscraper/lib/exceptions.py:29:24-31: `__init__` is missing an annotation for parameter `message` [implicit-any-parameter]
    ERROR juriscraper/lib/exceptions.py:36:24-31: `__init__` is missing an annotation for parameter `message` [implicit-any-parameter]
    ERROR juriscraper/lib/exceptions.py:43:24-31: `__init__` is missing an annotation for parameter `message` [implicit-any-parameter]
    ERROR juriscraper/lib/exceptions.py:50:24-31: `__init__` is missing an annotation for parameter `message` [implicit-any-parameter]
    ```

--- juriscraper/lib/exceptions.py
+++ juriscraper/lib/exceptions.py
@@ -26,29 +26,17 @@ class SkipRowError(JuriscraperException):
 class SlownessException(Exception):
     """Raised when things are too slow."""
 
-    def __init__(self, message):
-        Exception.__init__(self, message)
-
 
 class ParsingException(Exception):
     """Raised when parsing fails."""
-
-    def __init__(self, message):
-        Exception.__init__(self, message)
 
 
 class InsanityException(Exception):
     """Raised when data validation fails."""
 
-    def __init__(self, message):
-        Exception.__init__(self, message)
-
 
 class PacerLoginException(Exception):
     """Raised when the system cannot authenticate with PACER"""
-
-    def __init__(self, message):
-        Exception.__init__(self, message)
 
 
 class InvalidDocumentError(Exception):
