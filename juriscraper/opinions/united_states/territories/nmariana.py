1ec6716b6010a8989d7b7682b43e0ed73626f503
renatodvc <40128530+renatodvc@users.noreply.github.com>
2026-09-24 16:01:13 -0300

    fix:(nmariana) Update parsing patterns for cnmilaw.gov

--- juriscraper/opinions/united_states/territories/nmariana.py
+++ juriscraper/opinions/united_states/territories/nmariana.py
@@ -4,6 +4,7 @@ Court Short Name: NMI
 Author: William Edward Palin
 History:
   2023-01-21: Created by William Palin
+  2026-09-24: Site moved to cnmilaw.gov
 """
 
 import re
@@ -18,8 +19,8 @@ class Site(OpinionSiteLinear):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.court_id = self.__module__
-        year = date.today().strftime("%Y")[-2:]
-        self.url = f"https://www.cnmilaw.org/spm{year}.php#gsc.tab=0"
+        year = date.today().year
+        self.url = f"https://cnmilaw.gov/documents?type=case&court=Supreme&year={year}"
         self.status = "Published"
 
     def _cleanup_judge_names(self, judges: str) -> list[str]:
@@ -52,7 +53,7 @@ class Site(OpinionSiteLinear):
         return author
 
     def _process_html(self):
-        for s in self.html.xpath(".//td/a/@href[contains(., 'pdf')]/../../.."):
+        for s in self.html.xpath(".//a[@class='pdf-link']/ancestor::tr"):
             cells = s.xpath(".//td")
             judge_text = cells[3].text_content()
             author = self._fetch_author(judge_text)
@@ -66,7 +67,7 @@ class Site(OpinionSiteLinear):
                     "judge": ", ".join(self._cleanup_judge_names(judge_text)),
                     "author": author,
                     "per_curiam": not author,
-                    "url": s.xpath(".//td/a/@href")[0],
+                    "url": s.xpath(".//a[@class='pdf-link']/@href")[0],
                     "docket": "",
                 }
             )
